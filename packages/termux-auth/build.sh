74f8952044068e12d9624d8044284c7e1b2ea476
araczkowski <araczkowski@gmail.com>
2019-03-20 11:39:03 +0100

    dnsmasq and rclone

--- packages/termux-auth/build.sh
+++ packages/termux-auth/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-auth
 TERMUX_PKG_DESCRIPTION="Password authentication library and utility for Termux"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
-TERMUX_PKG_VERSION=1.1
-TERMUX_PKG_SRCURL=https://github.com/termux/termux-auth/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=fd6fab1808b1e98dc865c47db8b4719f887273f95fe0f4dad26af016c28fa915
+TERMUX_PKG_VERSION=1.2
+TERMUX_PKG_SRCURL=https://github.com/araczkowski/termux-auth/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=29253c2932425995a9ca416702389cd6f185bac7f56c7738091086b7c235be1e
 TERMUX_PKG_DEPENDS="openssl"
