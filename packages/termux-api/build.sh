ce4ebd2d0709405555c66a36e2998b7564c910a2
araczkowski <araczkowski@gmail.com>
2019-10-28 17:33:02 +0100

    termux-api

--- packages/termux-api/build.sh
+++ packages/termux-api/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.termux.com/wiki/Termux:API
 TERMUX_PKG_DESCRIPTION="Termux API commands (install also the Termux:API app)"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.47
-TERMUX_PKG_SHA256=f0ce46c3f39d609d64e38d1ad292f233251760032bdf313ce8547c2f551f43c3
-TERMUX_PKG_SRCURL=https://github.com/termux/termux-api-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_VERSION=0.48
+TERMUX_PKG_SHA256=6d7fc7eeeb55fe7a2709d7c8679dac9b4e9f17dd7a106b16c9bbcc568df65e2a
+TERMUX_PKG_SRCURL=https://github.com/araczkowski/termux-api-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="bash"
