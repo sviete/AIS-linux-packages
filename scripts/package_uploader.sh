71c244548287f60f5a94518abc6e05a3eecba767
rysiekstation <rysiekstation@gmail.com>
2020-05-30 09:44:23 +0200

    package uploader

--- scripts/package_uploader.sh
+++ scripts/package_uploader.sh
@@ -70,8 +70,10 @@ SCRIPT_EMERG_EXIT=false
 SCRIPT_ERROR_EXIT=false
 
 # Bintray-specific configuration.
-BINTRAY_REPO_NAME="termux-packages-24"
-BINTRAY_REPO_GITHUB="termux/termux-packages"
+# BINTRAY_REPO_NAME="termux-packages-24"
+BINTRAY_REPO_NAME="ais"
+# BINTRAY_REPO_GITHUB="termux/termux-packages"
+BINTRAY_REPO_GITHUB="sviete/AIS-linux-packages"
 BINTRAY_REPO_DISTRIBUTION="stable"
 BINTRAY_REPO_COMPONENT="main"
 
