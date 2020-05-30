24d9dd465e77fc78fa92b219f2cff6daf3d58747
rysiekstation <rysiekstation@gmail.com>
2020-05-30 09:12:00 +0200

    fix 2 scripts

--- scripts/repology/generate-repology-metadata.sh
+++ scripts/repology/generate-repology-metadata.sh
@@ -22,7 +22,7 @@ set -e
 
 BASEDIR=$(dirname "$(realpath "$0")")
 export TERMUX_ARCH=aarch64
-export TERMUX_PREFIX=/data/data/com.termux/files/usr
+export TERMUX_PREFIX=/data/data/pl.sviete.dom/files/usr
 
 check_package() { # path
 	# Avoid ending on errors such as $(which prog)
