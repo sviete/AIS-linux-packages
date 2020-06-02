58a0fcee17fe2090fba3544d74e64dc1dab20369
rysiekstation <rysiekstation@gmail.com>
2020-06-02 19:25:01 +0200

    fix sha256

--- packages/timewarrior/build.sh
+++ packages/timewarrior/build.sh
@@ -3,10 +3,9 @@ TERMUX_PKG_DESCRIPTION="Command-line time tracker"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=1.3.0
 TERMUX_PKG_SRCURL=https://github.com/GothenburgBitFactory/timewarrior/releases/download/v$TERMUX_PKG_VERSION/timew-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=1f3b9166a96637d3c098a7cfcff74ca61c41f13e2ca21f6c7ad6dd54cc74ac70
+TERMUX_PKG_SHA256=c04c3ebefddd485323784409987ae66c11c3ce4c882a290c10934336a196f3e8
 TERMUX_PKG_DEPENDS="libandroid-glob, libc++"
 
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
 }
-
