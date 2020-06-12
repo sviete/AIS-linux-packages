ca9a5a931016a1f3028cae28a7161341df98ef5d
tomek@sviete.pl <tomek@sviete.pl>
2020-06-12 15:55:34 +0200

    fix fossil url

--- packages/fossil/build.sh
+++ packages/fossil/build.sh
@@ -2,8 +2,8 @@ TERMUX_PKG_HOMEPAGE=https://www.fossil-scm.org
 TERMUX_PKG_DESCRIPTION="DSCM with built-in wiki, http interface and server, tickets database"
 TERMUX_PKG_LICENSE="BSD 2-Clause"
 TERMUX_PKG_VERSION=2.11
-TERMUX_PKG_SRCURL=https://www.fossil-scm.org/index.html/uv/fossil-src-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3d5a04bbe0075edd7f04c64ffc8f3af71d16565bf606b753f5d5752e48ff3631
+TERMUX_PKG_SRCURL=https://www.fossil-scm.org/home/uv/fossil-src-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
 TERMUX_PKG_DEPENDS="libsqlite, openssl, zlib"
 
 termux_step_pre_configure() {
