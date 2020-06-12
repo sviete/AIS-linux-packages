2b559dea4c1bc1447dea61ffcb2c76c2fb5fb0f0
tomek@sviete.pl <tomek@sviete.pl>
2020-06-12 15:25:15 +0200

    remove frida-server

--- packages/frida-server/frida-python.subpackage.sh
+++
@@ -1,7 +0,0 @@
-TERMUX_SUBPKG_DESCRIPTION="Python bindings for Frida"
-TERMUX_SUBPKG_INCLUDE="lib/python*"
-
-termux_step_create_subpkg_debscripts() {
-	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
-	echo "pip3.8 install wcwidth colorama pygments" >> postinst
-}
