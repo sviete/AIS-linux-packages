f90fca61e407861af9b08aa85841dc27a322da86
rysiekstation <rysiekstation@gmail.com>
2020-06-21 11:47:33 +0200

    dnsmasq

---
+++ packages/dnsmasq/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://www.thekelleys.org.uk/dnsmasq/doc.html
+TERMUX_PKG_DESCRIPTION="Dnsmasq provides network infrastructure for small networks"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=2.80
+TERMUX_PKG_SRCURL=https://github.com/sviete/AIS-utils/blob/master/dnsmasq/dnsmasq-2.80.tar.xz?raw=true
+TERMUX_PKG_SHA256=3770b9ce49ae051f6d87f2c703560b4893be2dfc0d40af4bac20535644112be2
+TERMUX_PKG_BUILD_IN_SRC=true
