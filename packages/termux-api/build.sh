bdd6cb49657198d885fe4903c0e276b4758bc401
rysiekstation <rysiekstation@gmail.com>
2020-06-21 13:34:57 +0200

    termux api

--- packages/termux-api/build.sh
+++ packages/termux-api/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.termux.com/wiki/Termux:API
 TERMUX_PKG_DESCRIPTION="Termux API commands (install also the Termux:API app)"
 TERMUX_PKG_LICENSE="MIT"
-TERMUX_PKG_VERSION=0.50
-TERMUX_PKG_SRCURL=https://github.com/termux/termux-api-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=6b26e7028361e492ca187045e44170cf4bf94007ddb59ba0df580cce8610d282
+TERMUX_PKG_VERSION=0.48
+TERMUX_PKG_SHA256=6d7fc7eeeb55fe7a2709d7c8679dac9b4e9f17dd7a106b16c9bbcc568df65e2a
+TERMUX_PKG_SRCURL=https://github.com/araczkowski/termux-api-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="bash"
