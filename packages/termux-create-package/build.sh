33faed7b6dac370742974da31d2f5abd78e4c31b
Andrzej Raczkowski <araczkowski@gmail.com>
2020-07-07 21:11:52 +0200

    Update build.sh

--- packages/termux-create-package/build.sh
+++ packages/termux-create-package/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/termux-create-package
-TERMUX_PKG_DESCRIPTION="Utility to create Termux packages"
+TERMUX_PKG_DESCRIPTION="Utility to create Termux packages."
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_VERSION=0.8
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-create-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
