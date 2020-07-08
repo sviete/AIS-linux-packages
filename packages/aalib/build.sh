5dce15d6b5a64d833b417278aec631bd1de103c3
rysiekstation <rysiekstation@gmail.com>
2020-07-08 20:47:19 +0200

    packages/aalib/build.sh

--- packages/aalib/build.sh
+++ packages/aalib/build.sh
@@ -6,3 +6,4 @@ TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/aa-project/aalib-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=fbddda9230cf6ee2a4f5706b4b11e2190ae45f5eda1f0409dc4f99b35e0a70ee
 TERMUX_PKG_DEPENDS="libx11, ncurses"
+
