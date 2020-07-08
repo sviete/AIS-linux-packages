658a2e098848083ff81769f77aeffdb7bab6c078
rysiekstation <rysiekstation@gmail.com>
2020-07-08 13:44:25 +0200

    build ais-info/commit_per_file.sh

--- ais-info/commit_per_file.sh
+++ ais-info/commit_per_file.sh
@@ -1,9 +1,10 @@
-# get changed files
-
+# pull
+git pull
 # credential caching, it will be cached for 7200 seconds (2 hour) .
 git config --global credential.helper 'cache --timeout 7200'
-
+# get changed files
 files=`git diff --name-only`
+git pull
 for file in $files; do
   echo "Adding file: $file"
   git add $file
