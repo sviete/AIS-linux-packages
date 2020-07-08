0667f5dc179e98fc7b5561ac37937ad381f8b440
rysiekstation <rysiekstation@gmail.com>
2020-07-08 22:42:31 +0200

    packages/chromaprint/build.sh

--- packages/chromaprint/build.sh
+++ packages/chromaprint/build.sh
@@ -11,3 +11,4 @@ termux_step_post_make_install() {
   ln -sf "${TERMUX_PREFIX}/lib/libchromaprint.so" "${TERMUX_PREFIX}/lib/libchromaprint.so.${TERMUX_PKG_VERSION:0:1}"
   ln -sf "${TERMUX_PREFIX}/lib/libchromaprint.so" "${TERMUX_PREFIX}/lib/libchromaprint.so.${TERMUX_PKG_VERSION}"
 }
+
