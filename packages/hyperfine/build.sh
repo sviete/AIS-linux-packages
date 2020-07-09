18bac0e09837d776fb6f4d6d262395b2cffee300
rysiekstation <rysiekstation@gmail.com>
2020-07-09 12:27:16 +0200

    packages/hyperfine/build.sh

--- packages/hyperfine/build.sh
+++ packages/hyperfine/build.sh
@@ -6,3 +6,4 @@ TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/sharkdp/hyperfine/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=b949d6c1a78e9c1c5a7bb6c241fcd51d6faf00bba5719cc312f57b5b301cc854
 TERMUX_PKG_BUILD_IN_SRC=true
+
