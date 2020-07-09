02e1d7f0bf8c9f99c128beb2c3885c89af46af3f
rysiekstation <rysiekstation@gmail.com>
2020-07-09 17:59:31 +0200

    packages/libvorbis/build.sh

--- packages/libvorbis/build.sh
+++ packages/libvorbis/build.sh
@@ -8,3 +8,4 @@ TERMUX_PKG_SHA256=af00bb5a784e7c9e69f56823de4637c350643deedaf333d0fa86ecdba6fcb4
 TERMUX_PKG_DEPENDS="libogg"
 TERMUX_PKG_BREAKS="libvorbis-dev"
 TERMUX_PKG_REPLACES="libvorbis-dev"
+
