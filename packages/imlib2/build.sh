a4f9e01de24c11e907acb3e2299eba0ae80aed71
rysiekstation <rysiekstation@gmail.com>
2020-07-09 12:36:03 +0200

    packages/imlib2/build.sh

--- packages/imlib2/build.sh
+++ packages/imlib2/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/enlightenment/imlib2-${TERMU
 TERMUX_PKG_SHA256=668696d0a8961bb57353fe96669f44bbdf1696221a65da563408a5010c5bd735
 TERMUX_PKG_DEPENDS="freetype, giflib, libandroid-shmem, libbz2, libid3tag, libjpeg-turbo, libpng, libtiff, libxext, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="LIBS=-landroid-shmem"
+
