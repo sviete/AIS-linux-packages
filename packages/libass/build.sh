a8a4625a3537acee46230faf3a974ae677eab7b6
rysiekstation <rysiekstation@gmail.com>
2020-07-09 14:12:58 +0200

    packages/libass/build.sh

--- packages/libass/build.sh
+++ packages/libass/build.sh
@@ -8,3 +8,4 @@ TERMUX_PKG_SHA256=881f2382af48aead75b7a0e02e65d88c5ebd369fe46bc77d9270a94aa8fd38
 TERMUX_PKG_DEPENDS="fontconfig, fribidi, glib, harfbuzz"
 TERMUX_PKG_BREAKS="libass-dev"
 TERMUX_PKG_REPLACES="libass-dev"
+
