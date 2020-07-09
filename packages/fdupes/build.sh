fb4bb94b3ee98ff7f32f5612a1d06e5ae42c7d9e
rysiekstation <rysiekstation@gmail.com>
2020-07-09 09:51:00 +0200

    packages/fdupes/build.sh

--- packages/fdupes/build.sh
+++ packages/fdupes/build.sh
@@ -6,3 +6,4 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/adrianlopezroche/fdupes/releases/download/$TERMUX_PKG_VERSION/fdupes-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=eb9e3bd3e722ebb2a272e45a1073f78c60f8989b151c3661421b86b14b203410
 TERMUX_PKG_DEPENDS="ncurses,pcre2"
+
