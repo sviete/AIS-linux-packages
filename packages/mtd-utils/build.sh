e925249ae696b987cfe98afcb44c091ade892e69
rysiekstation <rysiekstation@gmail.com>
2020-07-10 22:12:27 +0200

    packages/mtd-utils/build.sh

--- packages/mtd-utils/build.sh
+++ packages/mtd-utils/build.sh
@@ -6,3 +6,4 @@ TERMUX_PKG_SRCURL=ftp://ftp.infradead.org/pub/mtd-utils/mtd-utils-$TERMUX_PKG_VE
 TERMUX_PKG_SHA256=8d15e8b70f036d6af1a66011f8ca0e048e9675fa7983d33bea92c24313a232d2
 TERMUX_PKG_DEPENDS="openssl, liblzo, libuuid, zlib, zstd"
 TERMUX_PKG_BUILD_IN_SRC=true
+
