5c8e5e89e51ebd9fdb9ab111ba63414107f60307
rysiekstation <rysiekstation@gmail.com>
2020-07-10 21:25:50 +0200

    packages/llbuild/build.sh

--- packages/llbuild/build.sh
+++ packages/llbuild/build.sh
@@ -5,3 +5,4 @@ TERMUX_PKG_VERSION=5.2
 TERMUX_PKG_SRCURL=https://github.com/apple/swift-llbuild/archive/swift-${TERMUX_PKG_VERSION}-RELEASE.tar.gz
 TERMUX_PKG_SHA256=6c781a4f7887a10e0443eaebde4d5f52333417392ed2422f012917c879014224
 TERMUX_PKG_DEPENDS="libc++, libandroid-spawn, libsqlite"
+
