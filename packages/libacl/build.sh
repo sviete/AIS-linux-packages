5a93a55db6d6614908179665d1eb32c80d4ce00b
rysiekstation <rysiekstation@gmail.com>
2020-07-10 20:44:23 +0200

    packages/libacl/build.sh

--- packages/libacl/build.sh
+++ packages/libacl/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_SRCURL=https://download.savannah.gnu.org/releases/acl/acl-${TERMUX_PK
 TERMUX_PKG_SHA256=06be9865c6f418d851ff4494e12406568353b891ffe1f596b34693c387af26c7
 TERMUX_PKG_DEPENDS="attr"
 
+
