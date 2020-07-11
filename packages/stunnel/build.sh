473c0b479c6cd3a29c2698c274830f48095062d2
rysiekstation <rysiekstation@gmail.com>
2020-07-11 15:37:45 +0200

    packages/stunnel/build.sh

--- packages/stunnel/build.sh
+++ packages/stunnel/build.sh
@@ -8,3 +8,4 @@ TERMUX_PKG_SHA256=7384bfb356b9a89ddfee70b5ca494d187605bb516b4fff597e167f97e2236b
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-shared --with-ssl=$TERMUX_PREFIX --disable-fips"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/stunnel3 share/man/man8/stunnel.*.8"
+
