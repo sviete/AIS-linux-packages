a3deb54f803db0652473436f6f46f308b1802799
rysiekstation <rysiekstation@gmail.com>
2020-07-11 18:20:23 +0200

    packages/tergent/build.sh

--- packages/tergent/build.sh
+++ packages/tergent/build.sh
@@ -6,3 +6,4 @@ TERMUX_PKG_SRCURL=https://github.com/aeolwyr/tergent/archive/${TERMUX_PKG_VERSIO
 TERMUX_PKG_SHA256=b16e5ba1a9333d6e14b94d8cf3f5ad27d3af124f6f2bbebb0eca62ac95783281
 TERMUX_PKG_DEPENDS="termux-api"
 TERMUX_PKG_BUILD_IN_SRC=true
+
