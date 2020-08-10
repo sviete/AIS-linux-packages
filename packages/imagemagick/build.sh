0cfc31c3d20e27764c7ea2b12e79681c2c9af2db
rysiekstation <rysiekstation@gmail.com>
2020-08-10 18:48:46 +0200

    build packages/imagemagick/build.sh

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://www.imagemagick.org/
 TERMUX_PKG_DESCRIPTION="Suite to create, edit, compose, or convert images in a variety of formats"
 TERMUX_PKG_LICENSE="ImageMagick"
-TERMUX_PKG_VERSION=7.0.10.24
+TERMUX_PKG_VERSION=7.0.10.26
 TERMUX_PKG_SRCURL=https://github.com/ImageMagick/ImageMagick/archive/$(echo $TERMUX_PKG_VERSION | sed 's/\(.*\)\./\1-/').tar.gz
-TERMUX_PKG_SHA256=c555b4724127f9993500c9a7bab148e6075a7397957516b9e0dd9faa02d5c98e
+TERMUX_PKG_SHA256=bc4aa7e8fa6cb3339d4826fe82f0371d4870f6bfae6fe1f29ec0a8f04d54604a
 TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, fribidi, glib, harfbuzz, libandroid-support, libbz2, libcairo, libffi, libgraphite, libjpeg-turbo, liblzma, libpixman, libpng, librsvg, libtiff, libuuid, libwebp, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, littlecms, openjpeg, pango, pcre, zlib"
 TERMUX_PKG_BREAKS="imagemagick-dev, imagemagick-x"
 TERMUX_PKG_REPLACES="imagemagick-dev, imagemagick-x"
@@ -26,7 +26,7 @@ share/ImageMagick-7/francais.xml
 
 termux_step_pre_configure() {
 	if [ $TERMUX_ARCH = "i686" ]; then
-		# Avoid "libMagickCore-7.Q16HDRI.so: error: undefined reference to '__atomic_load'":
+		# Avoid "libMagickCore-7.Q16HDRI.so: error: undefined reference to '__atomic_load'"
 		LDFLAGS+=" -latomic"
 	fi
 }
