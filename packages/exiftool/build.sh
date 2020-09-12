6a8c090559475b1656e85b247c94c32814af2d89
rysiekstation <rysiekstation@gmail.com>
2020-09-12 06:09:11 +0200

    build packages/exiftool/build.sh

--- packages/exiftool/build.sh
+++ packages/exiftool/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://exiftool.org/
 TERMUX_PKG_DESCRIPTION="Utility for reading, writing and editing meta information in a wide variety of files."
 TERMUX_PKG_LICENSE="Artistic-License-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=12.05
+TERMUX_PKG_VERSION=12.06
 TERMUX_PKG_SRCURL="http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=7c8572c626e6e2eda65038d1533a0f15649dfffc6bdb67a609765d1a81288bbe
+TERMUX_PKG_SHA256=792d0aaafbe82ae0188f9900d9c4750d2ed28deaa9ed128eec4923e4adb1e48b
 TERMUX_PKG_DEPENDS="perl"
 
 termux_step_make_install() {
@@ -12,7 +12,7 @@ termux_step_make_install() {
 
 	install -Dm700 "$TERMUX_PKG_SRCDIR"/exiftool "$TERMUX_PREFIX"/bin/exiftool
 	find "$TERMUX_PKG_SRCDIR"/lib -name "*.pod" -delete
-	mkdir -p "$TERMUX_PREFIX/lib/perl5/site_perl/${current_perl_version}"
+	mkdir -p "$TERMUX_PREFIX/lib/perl5/site_perl/$current_perl_version"
 	rm -rf "$TERMUX_PREFIX/lib/perl5/site_perl/${current_perl_version}"/{Image,File}
 	cp -a "$TERMUX_PKG_SRCDIR"/lib/{Image,File} "$TERMUX_PREFIX/lib/perl5/site_perl/${current_perl_version}/"
 }
