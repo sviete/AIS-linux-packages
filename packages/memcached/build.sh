fa3d0f140b493071f2fd2b652668326fbffbcb9e
rysiekstation <rysiekstation@gmail.com>
2020-09-12 06:12:42 +0200

    build packages/memcached/build.sh

--- packages/memcached/build.sh
+++ packages/memcached/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://memcached.org/
 TERMUX_PKG_DESCRIPTION="Free & open source, high-performance, distributed memory object caching system"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.6.6
+TERMUX_PKG_VERSION=1.6.7
 TERMUX_PKG_SRCURL=https://www.memcached.org/files/memcached-$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=908f0eecfa559129c9e44edc46f02e73afe8faca355b4efc5c86d902fc3e32f7
+TERMUX_PKG_SHA256=7bbdac9b031d8cfca4a1207f28df598b90ee2e9b44667f7eabd0fe1a59ca5173
 TERMUX_PKG_DEPENDS="libevent, libsasl"
 TERMUX_PKG_BREAKS="memcached-dev"
 TERMUX_PKG_REPLACES="memcached-dev"
@@ -15,7 +15,7 @@ termux_step_pre_configure() {
 
 	export ac_cv_c_endian=little
 
-	# fix SASL configuration path
+	# Fix SASL configuration path
 	perl -p -i -e "s#/etc/sasl#$TERMUX_PREFIX/etc/sasl#" $TERMUX_PKG_BUILDDIR/sasl_defs.c
 
 	# getsubopt() taken from https://github.com/lxc/lxc/blob/master/src/include/getsubopt.c
