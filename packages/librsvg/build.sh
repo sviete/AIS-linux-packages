ee6d72c97fd93511cdfe200c481b59dab12a3e2a
rysiekstation <rysiekstation@gmail.com>
2020-09-12 06:12:00 +0200

    build packages/librsvg/build.sh

--- packages/librsvg/build.sh
+++ packages/librsvg/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/action/show/Projects/LibRsvg
 TERMUX_PKG_DESCRIPTION="Library to render SVG files using cairo"
 TERMUX_PKG_LICENSE="LGPL-2.0"
-TERMUX_PKG_VERSION=2.49.4
+TERMUX_PKG_VERSION=2.50.0
 TERMUX_PKG_SRCURL=http://ftp.gnome.org/pub/GNOME/sources/librsvg/${TERMUX_PKG_VERSION:0:4}/librsvg-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=a7a66ac14827e6ca1e29cb757b022601108458d8cdf6cea22fa9699156b249f1
+TERMUX_PKG_SHA256=b3fadba240f09b9c9898ab20cb7311467243e607cf8f928b7c5f842474ee3df4
 TERMUX_PKG_DEPENDS="gdk-pixbuf, libcairo, libcroco, pango, zlib"
 TERMUX_PKG_BREAKS="librsvg-dev"
 TERMUX_PKG_REPLACES="librsvg-dev"
@@ -17,7 +17,7 @@ termux_step_pre_configure() {
 
 	LDFLAGS+=" -fuse-ld=lld"
 
-	#See https://github.com/GNOME/librsvg/blob/master/COMPILING.md
+	# See https://github.com/GNOME/librsvg/blob/master/COMPILING.md
 	export RUST_TARGET=$CARGO_TARGET_NAME
 }
 
