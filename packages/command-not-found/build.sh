14707535c514d1a3b393e28d335a3dacc1ccfbc4
andrzej <you@example.com>
2020-10-20 17:30:55 +0200

    build packages/command-not-found/build.sh

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=1.57
+TERMUX_PKG_VERSION=1.59
 TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1dae247d89ef809c20d058ef3738f160a27de5ffb049c8864ff7079f26bbdbd4
+TERMUX_PKG_SHA256=fa7f3da30eccdc4e78f7f392fea10e93ce626f4f195d31294a374de4aa156de4
