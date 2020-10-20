f40a6306be4f5cf5c7f029db376fd4796b42694a
andrzej <you@example.com>
2020-10-20 17:31:10 +0200

    build packages/croc/build.sh

--- packages/croc/build.sh
+++ packages/croc/build.sh
@@ -1,9 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/schollz/croc
 TERMUX_PKG_DESCRIPTION="Easily and securely send things from one computer to another."
 TERMUX_PKG_LICENSE=MIT
-TERMUX_PKG_VERSION=8.4.0
+TERMUX_PKG_VERSION=8.5.1
 TERMUX_PKG_SRCURL=https://github.com/schollz/croc/releases/download/v${TERMUX_PKG_VERSION}/croc_${TERMUX_PKG_VERSION}_src.tar.gz
-TERMUX_PKG_SHA256=2f0de360e252974dbb7d2ab688f17b0ad6e842b48a33bd33ec9272055aca2780
+TERMUX_PKG_SHA256=b76e5523a8c621ddbaf6dacf3e70c6f614b8a35d494be43412082e9bf7323df2
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_make() {
