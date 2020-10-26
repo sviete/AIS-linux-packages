c8167644cc96c009f87be2b79cd60b0dc6658dcf
andrzej <you@example.com>
2020-10-26 08:06:13 +0100

    build packages/libsearpc/build.sh

--- packages/libsearpc/build.sh
+++ packages/libsearpc/build.sh
@@ -1,10 +1,9 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/haiwen/libsearpc
 TERMUX_PKG_DESCRIPTION="A simple C language RPC framework (mainly for seafile)"
 TERMUX_PKG_LICENSE="Apache-2.0"
-TERMUX_PKG_VERSION=3.2
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/haiwen/libsearpc/archive/v${TERMUX_PKG_VERSION}-latest.tar.gz
-TERMUX_PKG_SHA256=6ebb4a46c3927f379ae6356a5de0f8f884d23a109530f7cc48ce5fffae5811c7
+TERMUX_PKG_VERSION=1:3.2.0
+TERMUX_PKG_SRCURL=https://github.com/haiwen/libsearpc/archive/v${TERMUX_PKG_VERSION:2}.tar.gz
+TERMUX_PKG_SHA256=cd00197fcc40b45b1d5e892b2d08dfa5947f737e0d80f3ef26419334e75b0bff
 TERMUX_PKG_DEPENDS="glib, libjansson"
 TERMUX_PKG_BREAKS="libsearpc-dev"
 TERMUX_PKG_REPLACES="libsearpc-dev"
@@ -12,5 +11,3 @@ TERMUX_PKG_REPLACES="libsearpc-dev"
 termux_step_post_get_source() {
 	./autogen.sh
 }
-
-
