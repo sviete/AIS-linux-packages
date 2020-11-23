201932783853e59ee65b107a9a6fa245eaf46122
andrzej <you@example.com>
2020-11-23 11:03:24 +0100

    build packages/capstone/build.sh

--- packages/capstone/build.sh
+++ packages/capstone/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=http://www.capstone-engine.org/
-TERMUX_PKG_DESCRIPTION="Lightweight multi-platform, multi-architecture disassembly framework."
+TERMUX_PKG_DESCRIPTION="Lightweight multi-platform, multi-architecture disassembly framework"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_VERSION=4.0.2
 TERMUX_PKG_REVISION=1
@@ -8,4 +8,3 @@ TERMUX_PKG_SHA256=7c81d798022f81e7507f1a60d6817f63aa76e489aa4e7055255f21a22f5e52
 TERMUX_PKG_BREAKS="capstone-dev"
 TERMUX_PKG_REPLACES="capstone-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DINSTALL_LIB_DIR=$TERMUX_PREFIX/lib"
-
