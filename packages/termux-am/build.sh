ff567ef3a2261a7a9acc46cfcb37709151e9629e
andrzej <you@example.com>
2020-11-23 12:55:46 +0100

    build packages/termux-am/build.sh

--- packages/termux-am/build.sh
+++ packages/termux-am/build.sh
@@ -3,8 +3,8 @@ TERMUX_PKG_DESCRIPTION="Android Oreo-compatible am command reimplementation"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="Michal Bednarski @michalbednarski"
 TERMUX_PKG_VERSION=0.3
+TERMUX_PKG_SRCURL=https://github.com/termux/TermuxAm/archive/v$TERMUX_PKG_VERSION.zip
 TERMUX_PKG_SHA256=3abfaca319f59fa769480c33d50e2ee1ff73122eac7c658f9e2a66a2da29ebb7
-TERMUX_PKG_SRCURL=https://github.com/araczkowski/TermuxAm/archive/v$TERMUX_PKG_VERSION.zip
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_CONFLICTS="termux-tools (<< 0.51)"
@@ -34,4 +34,3 @@ termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/libexec/termux-am
 	cp $TERMUX_PKG_SRCDIR/app/build/outputs/apk/release/app-release-unsigned.apk $TERMUX_PREFIX/libexec/termux-am/am.apk
 }
-
