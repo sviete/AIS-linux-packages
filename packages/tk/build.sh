3c8482e6297e096e86da4eff28326cdf247da693
andrzej <you@example.com>
2020-11-23 12:59:37 +0100

    build packages/tk/build.sh

--- packages/tk/build.sh
+++ packages/tk/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=http://tcl.sourceforge.net/
 TERMUX_PKG_DESCRIPTION="A windowing toolkit for use with tcl"
-TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="license.terms"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=8.6.10
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/sourceforge/tcl/tk${TERMUX_PKG_VERSION}-src.tar.gz
