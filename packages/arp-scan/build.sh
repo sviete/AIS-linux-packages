ac98e31209cadf0dc3b8a735e882ad633f18051e
andrzej <you@example.com>
2020-11-24 10:24:23 +0100

    packages/arp-scan/build.sh

--- packages/arp-scan/build.sh
+++ packages/arp-scan/build.sh
@@ -5,12 +5,10 @@ TERMUX_PKG_VERSION=1.9.7
 TERMUX_PKG_SRCURL=https://github.com/royhills/arp-scan/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e03c36e4933c655bd0e4a841272554a347cd0136faf42c4a6564059e0761c039
 TERMUX_PKG_DEPENDS="libpcap"
-
 if [[ ${TERMUX_ARCH_BITS} == 32 ]]; then
 	# Retrieved from compilation on device:
 	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="pgac_cv_snprintf_long_long_int_format=%lld"
 fi
-
 termux_step_pre_configure () {
 	cp ${TERMUX_PKG_BUILDER_DIR}/hsearch/* ${TERMUX_PKG_SRCDIR}/
 	aclocal
@@ -18,5 +16,3 @@ termux_step_pre_configure () {
 	automake --add-missing
 	autoconf
 }
-
-
