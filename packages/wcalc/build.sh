1a1cdd1d35fd5bb0161fbf5fe28e1ac0c1731fb0
andrzej <you@example.com>
2020-11-24 13:47:35 +0100

    packages/wcalc/build.sh

--- packages/wcalc/build.sh
+++ packages/wcalc/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/w-calc/wcalc-${TERMUX_PKG_VER
 TERMUX_PKG_SHA256=0e2c17c20f935328dcdc6cb4c06250a6732f9ee78adf7a55c01133960d6d28ee
 TERMUX_PKG_DEPENDS="flex,libmpfr,libgmp,readline"
 
+
