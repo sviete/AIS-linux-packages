5ad02b7672b15642e64de60eec21e073c187a8f3
andrzej <you@example.com>
2020-11-24 12:41:21 +0100

    packages/mu/build.sh

--- packages/mu/build.sh
+++ packages/mu/build.sh
@@ -5,3 +5,4 @@ TERMUX_PKG_VERSION=1.4.13
 TERMUX_PKG_SRCURL=https://github.com/djcb/mu/releases/download/$TERMUX_PKG_VERSION/mu-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=b26a504f8660b6374e0285daf7e26e6eb4ec0668bf2c8b4b1cd7667c4ebe6e8e
 TERMUX_PKG_DEPENDS="glib, libc++, libxapian, libgmime"
+
