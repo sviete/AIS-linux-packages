c409734e95170a40e1869b085d27028953914249
andrzej <you@example.com>
2020-11-24 10:57:58 +0100

    packages/dictd/build.sh

--- packages/dictd/build.sh
+++ packages/dictd/build.sh
@@ -5,3 +5,4 @@ TERMUX_PKG_VERSION=1.13.0
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/dict/dictd/dictd-${TERMUX_PKG_VERSION}/dictd-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=eeba51af77e87bb1b166c6bc469aad463632d40fb2bdd65e6675288d8e1a81e4
 TERMUX_PKG_DEPENDS="libmaa, zlib"
+
