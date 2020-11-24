44d92aa0d8e509df6b63bd50c06236cbd3c48e66
andrzej <you@example.com>
2020-11-24 13:23:38 +0100

    packages/sshpass/build.sh

--- packages/sshpass/build.sh
+++ packages/sshpass/build.sh
@@ -6,3 +6,4 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://downloads.sourceforge.net/project/sshpass/sshpass/${TERMUX_PKG_VERSION}/sshpass-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60
 
+
