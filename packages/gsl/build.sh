68da7491d44e5532e4c1b02c356b7c0b3f0c1057
andrzej <you@example.com>
2020-11-24 11:25:45 +0100

    packages/gsl/build.sh

--- packages/gsl/build.sh
+++ packages/gsl/build.sh
@@ -11,3 +11,4 @@ TERMUX_PKG_RM_AFTER_INSTALL="bin/ share/man/man1"
 # Workaround for linker on Android 5 (fixed in Android 6) not supporting RTLD_GLOBAL.
 # See https://github.com/termux/termux-packages/issues/507
 export GSL_LDFLAGS="-Lcblas/.libs/ -lgslcblas"
+
