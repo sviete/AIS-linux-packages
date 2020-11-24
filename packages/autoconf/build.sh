25914191f47ab9ee219889c411685d154144265e
andrzej <you@example.com>
2020-11-24 10:27:31 +0100

    packages/autoconf/build.sh

--- packages/autoconf/build.sh
+++ packages/autoconf/build.sh
@@ -6,14 +6,10 @@ TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/autoconf/autoconf-${TERMUX_PKG_
 TERMUX_PKG_SHA256=64ebcec9f8ac5b2487125a86a7760d2591ac9e1d3dbd59489633f9de62a57684
 TERMUX_PKG_DEPENDS="m4, make, perl"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-
 termux_step_post_get_source() {
 	perl -p -i -e "s|/bin/sh|$TERMUX_PREFIX/bin/sh|" lib/*/*.m4
 }
-
 termux_step_post_massage() {
 	perl -p -i -e "s|/usr/bin/m4|$TERMUX_PREFIX/bin/m4|" bin/*
 	perl -p -i -e "s|CONFIG_SHELL-/bin/sh|CONFIG_SHELL-$TERMUX_PREFIX/bin/sh|" bin/autoconf
 }
-
-
