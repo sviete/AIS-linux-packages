aa4d3f5cd15567d3e410954edb99274a60d89560
andrzej <you@example.com>
2020-11-24 10:25:36 +0100

    packages/aspell-de/build.sh

--- packages/aspell-de/build.sh
+++ packages/aspell-de/build.sh
@@ -6,7 +6,6 @@ TERMUX_PKG_VERSION=20161207-7-0
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_SRCURL=ftp://ftp.gnu.org/gnu/aspell/dict/de/aspell6-de-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=c2125d1fafb1d4effbe6c88d4e9127db59da9ed92639c7cbaeae1b7337655571
-
 termux_step_configure () {
     # aspell configure doesn't play nicely with cross-compile but it's so trivial
     # we can easily replace it.
@@ -20,4 +19,3 @@ datadir = $TERMUX_PREFIX/lib/aspell-0.60
 EOF
     cat $TERMUX_PKG_SRCDIR/Makefile.pre >> $TERMUX_PKG_SRCDIR/Makefile
 }
-
