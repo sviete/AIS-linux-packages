bd0154639c6781c5ffee7e037be21c87eab64442
andrzej <you@example.com>
2020-11-24 12:32:19 +0100

    packages/lzlib/build.sh

--- packages/lzlib/build.sh
+++ packages/lzlib/build.sh
@@ -5,3 +5,4 @@ TERMUX_PKG_VERSION=1.11
 TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/lzip/lzlib/lzlib-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=6c5c5f8759d1ab7c4c3c53788ea2d9daad04aeddcf338226893f8ff134914d36
 TERMUX_PKG_DEPENDS="lzlib"
+
