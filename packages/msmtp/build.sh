2014effe3966f27e60c87feeaf6ec58c44caf594
andrzej <you@example.com>
2020-11-24 12:40:37 +0100

    packages/msmtp/build.sh

--- packages/msmtp/build.sh
+++ packages/msmtp/build.sh
@@ -5,3 +5,4 @@ TERMUX_PKG_VERSION=1.8.13
 TERMUX_PKG_SRCURL=https://marlam.de/msmtp/releases/msmtp-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=ada945ab8d519102bb632f197273b3326ded25b38c003b0cf3861d1d6d4a9bb9
 TERMUX_PKG_DEPENDS="libgnutls, libidn2"
+
