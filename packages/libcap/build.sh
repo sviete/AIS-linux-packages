5e7be703f4a786f6f64e3ce4f50c5aea55469443
andrzej <you@example.com>
2020-11-24 11:51:14 +0100

    packages/libcap/build.sh

--- packages/libcap/build.sh
+++ packages/libcap/build.sh
@@ -17,3 +17,4 @@ termux_step_make() {
 termux_step_make_install() {
 	make CC="$CC -Wl,-rpath=$TERMUX_PREFIX/lib -Wl,--enable-new-dtags" prefix="$TERMUX_PREFIX" RAISE_SETFCAP=no lib=/lib PTHREADS=no install
 }
+
