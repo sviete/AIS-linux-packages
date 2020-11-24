5fa91f5da4fe8140dee6b6df00747fa04b16c78d
andrzej <you@example.com>
2020-11-24 13:21:58 +0100

    packages/solidity/build.sh

--- packages/solidity/build.sh
+++ packages/solidity/build.sh
@@ -6,3 +6,4 @@ TERMUX_PKG_SRCURL=https://github.com/ethereum/solidity/releases/download/v${TERM
 TERMUX_PKG_SHA256=c69205d902ac8dd66f5aeaa78a08e5411cdb26a0b9479fcf44f394b7008b484c
 TERMUX_PKG_DEPENDS="boost-static"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DUSE_Z3=OFF -DUSE_CVC4=OFF"
+
