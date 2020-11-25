b976a4ff3f74beb36094de74cb2e7a338f5c76f3
andrzej <you@example.com>
2020-11-25 12:23:25 +0100

    packages/tar/build.sh

--- packages/tar/build.sh
+++ packages/tar/build.sh
@@ -7,16 +7,13 @@ TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/tar/tar-${TERMUX_PKG_VERSION}.t
 TERMUX_PKG_SHA256=d0d3ae07f103323be809bc3eac0dcc386d52c5262499fe05511ac4788af1fdd8
 TERMUX_PKG_DEPENDS="libandroid-glob, libiconv"
 TERMUX_PKG_ESSENTIAL=true
-
 # When cross-compiling configure guesses that d_ino in struct dirent only exists
 # if triplet matches linux*-gnu*, so we force set it explicitly:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="gl_cv_struct_dirent_d_ino=yes"
 # this needed to disable tar's implementation of mkfifoat() so it is possible
 # to use own implementation (see patch 'mkfifoat.patch').
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" ac_cv_func_mkfifoat=yes"
-
 termux_step_pre_configure() {
 	CPPFLAGS+=" -D__USE_FORTIFY_LEVEL=0"
 	LDFLAGS+=" -landroid-glob"
 }
-
