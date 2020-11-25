8780e3b747b29fdb4d85e7b3cd6968fef869a05b
andrzej <you@example.com>
2020-11-25 11:29:44 +0100

    packages/mediainfo/build.sh

--- packages/mediainfo/build.sh
+++ packages/mediainfo/build.sh
@@ -6,11 +6,9 @@ TERMUX_PKG_VERSION=20.09
 TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/mediainfo/${TERMUX_PKG_VERSION}/mediainfo_${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=fad981d2c8154c98f4107da1d5bfafbe59fd7198519cc470e06bdd335c7819c3
 TERMUX_PKG_DEPENDS="libmediainfo"
-
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR="${TERMUX_PKG_SRCDIR}/Project/GNU/CLI"
 	TERMUX_PKG_BUILDDIR="${TERMUX_PKG_SRCDIR}"
 	cd "${TERMUX_PKG_SRCDIR}"
 	./autogen.sh
 }
-
