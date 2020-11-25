f2e514f1bc66cfd3dcaa4612c855ce411c9ed338
andrzej <you@example.com>
2020-11-25 11:07:13 +0100

    packages/libpixman/build.sh

--- packages/libpixman/build.sh
+++ packages/libpixman/build.sh
@@ -8,10 +8,8 @@ TERMUX_PKG_SHA256=6d200dec3740d9ec4ec8d1180e25779c00bc749f94278c8b9021f5534db223
 TERMUX_PKG_BREAKS="libpixman-dev"
 TERMUX_PKG_REPLACES="libpixman-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-libpng"
-
 termux_step_pre_configure() {
 	if [ $TERMUX_ARCH = arm ]; then
 		CFLAGS+=" -fno-integrated-as"
 	fi
 }
-
