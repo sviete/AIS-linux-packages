99d741e76cda2c1d6fa261e7ef8800f00c31f22d
andrzej <you@example.com>
2020-11-25 12:35:50 +0100

    packages/uftrace/build.sh

--- packages/uftrace/build.sh
+++ packages/uftrace/build.sh
@@ -7,16 +7,13 @@ TERMUX_PKG_SRCURL=https://github.com/namhyung/uftrace/archive/v${TERMUX_PKG_VERS
 TERMUX_PKG_SHA256=418d30c959d3b6d0dcafd55e588a5d414a9984b30f2522a5af004a268824a5a2
 TERMUX_PKG_DEPENDS="capstone, libandroid-glob, libandroid-spawn, libelf, libdw, ncurses, python"
 TERMUX_PKG_BUILD_DEPENDS="argp"
-
 termux_step_pre_configure() {
 	# uftrace uses custom configure script implementation, so we need to provide some flags
 	export CFLAGS="${CFLAGS} -I${TERMUX_PREFIX}/include -I${TERMUX_PREFIX}/include/python3.9 -DEFD_SEMAPHORE=1 -DEF_ARM_ABI_FLOAT_HARD=0x400 -w"
 	export LDFLAGS="${LDFLAGS} -Wl,--wrap=_Unwind_Resume -landroid-glob -largp -lpython3.9"
-
 	if [ "$TERMUX_ARCH" = "i686" ]; then
 		export ARCH="i386"
 	else
 		export ARCH="$TERMUX_ARCH"
 	fi
 }
-
