29956f6080a0b1ccaea15c1b4cc4136650e9fcd4
andrzej <you@example.com>
2020-11-25 12:47:20 +0100

    packages/yadm/build.sh

--- packages/yadm/build.sh
+++ packages/yadm/build.sh
@@ -6,14 +6,11 @@ TERMUX_PKG_SRCURL=https://github.com/TheLocehiliosan/yadm/archive/$TERMUX_PKG_VE
 TERMUX_PKG_SHA256=d022d118a1a477e09afa00c80e10bd94b150d99709e57f01ba48ca2eaaeeb2de
 TERMUX_PKG_DEPENDS="git"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_make() {
 	# Do not try to run 'make' as it causes a build failure.
 	return
 }
-
 termux_step_make_install() {
 	install -Dm700 "$TERMUX_PKG_SRCDIR"/yadm "$TERMUX_PREFIX"/bin/
 	install -Dm600 "$TERMUX_PKG_SRCDIR"/yadm.1 "$TERMUX_PREFIX"/share/man/man1/
 }
-
