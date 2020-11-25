93b8051d144e421d11f7ebf211003dd1d5f33c46
andrzej <you@example.com>
2020-11-25 11:18:15 +0100

    packages/libwren/build.sh

--- packages/libwren/build.sh
+++ packages/libwren/build.sh
@@ -9,13 +9,11 @@ TERMUX_PKG_BREAKS="wren-dev, wren (<< 0.3.0)"
 TERMUX_PKG_REPLACES="wren-dev, wren (<< 0.3.0)"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
-
 termux_step_make() {
 	local QUIET_BUILD=
 	if [ "$TERMUX_QUIET_BUILD" = true ]; then
 		QUIET_BUILD="-s"
 	fi
-
 	cd projects/make
 	if [ "$TERMUX_ARCH" = i686 ] || [ "$TERMUX_ARCH" = arm ]; then
 		RELEASE=release_32bit
@@ -24,15 +22,11 @@ termux_step_make() {
 	fi
 	make -j $TERMUX_MAKE_PROCESSES $QUIET_BUILD config=${RELEASE}
 }
-
 termux_step_make_install() {
 	install -Dm600 "$TERMUX_PKG_SRCDIR"/src/include/wren.h \
 		"$TERMUX_PREFIX"/include/wren.h
-
 	install -Dm600 "$TERMUX_PKG_SRCDIR"/lib/libwren.so \
 		"$TERMUX_PREFIX"/lib/libwren.so
-
 	install -Dm600 "$TERMUX_PKG_SRCDIR"/lib/libwren.a \
 		"$TERMUX_PREFIX"/lib/libwren.a
 }
-
