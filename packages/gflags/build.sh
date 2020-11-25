c82f68fc6dd89e2057bbb4902bddd8a5e7718fb5
andrzej <you@example.com>
2020-11-25 10:11:34 +0100

    packages/gflags/build.sh

--- packages/gflags/build.sh
+++ packages/gflags/build.sh
@@ -16,10 +16,8 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBUILD_gflags_LIBS=ON
 -DINSTALL_HEADERS=ON
 "
-
 termux_step_post_make_install() {
 	#Any old packages using the library name of libgflags
 	ln -sfr "$TERMUX_PREFIX"/lib/pkgconfig/gflags.pc \
 		"$TERMUX_PREFIX"/lib/pkgconfig/libgflags.pc
 }
-
