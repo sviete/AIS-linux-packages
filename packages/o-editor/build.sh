c417962e4b98efeb76c1b7868fce2bf99943578c
andrzej <you@example.com>
2020-11-25 11:46:12 +0100

    packages/o-editor/build.sh

--- packages/o-editor/build.sh
+++ packages/o-editor/build.sh
@@ -8,20 +8,15 @@ TERMUX_PKG_SRCURL=https://github.com/xyproto/o/archive/$TERMUX_PKG_VERSION.tar.g
 TERMUX_PKG_SHA256=571c695de6bb5a2e012fb64cd9dd0a8d6607740a33770c17617874ec587f678c
 TERMUX_PKG_BREAKS="o"
 TERMUX_PKG_REPLACES="o"
-
 termux_step_make() {
 	termux_setup_golang
-
 	export GOPATH=$TERMUX_PKG_BUILDDIR
 	mkdir -p "$GOPATH"/src/github.com/xyproto
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/xyproto/o
-
 	cd "$GOPATH"/src/github.com/xyproto/o
 	go build
 }
-
 termux_step_make_install() {
 	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/xyproto/o/o
 	install -Dm600 -t "$TERMUX_PREFIX"/share/man/man1 "$TERMUX_PKG_SRCDIR"/o.1
 }
-
