ab2f6c4c4857c09014f4e36fb2f7e526588a25b9
andrzej <you@example.com>
2020-11-25 09:38:30 +0100

    packages/ca-certificates/build.sh

--- packages/ca-certificates/build.sh
+++ packages/ca-certificates/build.sh
@@ -7,28 +7,22 @@ TERMUX_PKG_SRCURL=https://curl.haxx.se/ca/cacert.pem
 TERMUX_PKG_SHA256=bb28d145ed1a4ee67253d8ddb11268069c9dafe3db25a9eee654974c4e43eee5
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-
 termux_step_make_install() {
 	local CERTDIR=$TERMUX_PREFIX/etc/tls
 	local CERTFILE=$CERTDIR/cert.pem
-
 	mkdir -p $CERTDIR
-
 	termux_download $TERMUX_PKG_SRCURL \
 		$CERTFILE \
 		$TERMUX_PKG_SHA256
 	touch $CERTFILE
-
 	# Build java keystore which is split out into a ca-certificates-java subpackage:
 	local KEYUTIL_JAR=$TERMUX_PKG_CACHEDIR/keyutil-0.4.0.jar
 	termux_download \
 		https://github.com/use-sparingly/keyutil/releases/download/0.4.0/keyutil-0.4.0.jar \
 		$KEYUTIL_JAR \
 		18f1d2c82839d84949b1ad015343c509e81ef678c24db6112acc6c0761314610
-
 	local JAVA_KEYSTORE_DIR=$PREFIX/lib/jvm/openjdk-9/lib/security
 	mkdir -p $JAVA_KEYSTORE_DIR
-
 	java -jar $KEYUTIL_JAR \
 		--import \
 		--new-keystore $JAVA_KEYSTORE_DIR/jssecacerts \
@@ -36,5 +30,3 @@ termux_step_make_install() {
 		--force-new-overwrite \
 		--import-pem-file $CERTFILE
 }
-
-
