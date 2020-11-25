c6259bacc3bc99f0901510ac130c7852cb69723b
andrzej <you@example.com>
2020-11-25 12:42:03 +0100

    packages/w3m/build.sh

--- packages/w3m/build.sh
+++ packages/w3m/build.sh
@@ -12,11 +12,9 @@ TERMUX_PKG_SHA256=0467bb5429b75749205a3f57b9f5e8abba49929272aeab6fce94ff17953f07
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="libgc, ncurses, openssl, zlib"
 TERMUX_PKG_SUGGESTS="perl"
-
 # ac_cv_func_bcopy=yes to avoid w3m defining it's own bcopy function, which
 # breaks 64-bit builds where NDK headers define bcopy as a macro:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_func_setpgrp_void=yes ac_cv_func_bcopy=yes"
-
 #Overwrite the default /usr/bin/firefox with termux-open-url as default external browser. That way, pressing "M" on a URL will open a link in Androids default Browser.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-browser=termux-open-url"
 #Overwrite the default editor to just vi, as the default was /usr/bin/vi.
@@ -24,8 +22,5 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --with-editor=nano"
 # Build w3mimg with X11/imlib2.
 # w3mimgdisplay is in w3m-img subpackage.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-image=x11 --with-imagelib=imlib2"
-
 # For Makefile.in.patch:
 export TERMUX_PKG_BUILDER_DIR
-
-
