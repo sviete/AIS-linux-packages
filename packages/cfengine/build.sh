8211115a91d5d163a46dbbad472b8dfed51bf35b
andrzej <you@example.com>
2020-11-25 09:41:08 +0100

    packages/cfengine/build.sh

--- packages/cfengine/build.sh
+++ packages/cfengine/build.sh
@@ -12,24 +12,20 @@ TERMUX_PKG_DEPENDS="liblmdb, openssl, libandroid-glob, pcre, libyaml, libxml2"
 # core doesn't work with out-of-tree builds
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-workdir=$TERMUX_PREFIX/var/lib/cfengine --without-pam --without-selinux-policy --without-systemd-service --with-lmdb=$TERMUX_PREFIX --with-openssl=$TERMUX_PREFIX --with-yaml=$TERMUX_PREFIX --with-pcre=$TERMUX_PREFIX --with-prefix=$TERMUX_PREFIX --with-libxml2=$TERMUX_PREFIX"
-
 termux_step_post_get_source() {
   # commit-based zips from github include the commit sha so rename to normalize for later steps
   mv masterfiles-* masterfiles
   rm -rf libntech
   mv libntech-* libntech
 }
-
 termux_step_pre_configure() {
   EXPLICIT_VERSION=${TERMUX_PKG_VERSION}
   LDFLAGS+=" -landroid-glob"
   NO_CONFIGURE=1 ./autogen.sh $TERMUX_PKG_EXTRA_CONFIGURE_ARGS --prefix=$TERMUX_PREFIX/var/lib/cfengine --bindir=$TERMUX_PREFIX/bin
-
   cd masterfiles
   ./autogen.sh --prefix=$TERMUX_PREFIX/var/lib/cfengine --bindir=$TERMUX_PREFIX/bin
   make install
 }
-
 termux_step_create_debscripts() {
         cat << EOF > ./postinst
         #!$TERMUX_PREFIX/bin/sh
@@ -39,4 +35,3 @@ termux_step_create_debscripts() {
         fi
 EOF
 }
-
