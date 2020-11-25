3e3060d1c87a129af3f9fade8ae0edd98872cf18
andrzej <you@example.com>
2020-11-25 12:44:40 +0100

    packages/x11-repo/build.sh

--- packages/x11-repo/build.sh
+++ packages/x11-repo/build.sh
@@ -7,16 +7,13 @@ TERMUX_PKG_DEPENDS="termux-keyring"
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_CONFFILES="etc/apt/sources.list.d/x11.list"
-
 termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/etc/apt/sources.list.d
 	echo "deb https://dl.bintray.com/xeffyr/x11-packages x11 main" > $TERMUX_PREFIX/etc/apt/sources.list.d/x11.list
 }
-
 termux_step_create_debscripts() {
 	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
 	echo "echo Downloading updated package list ..." >> postinst
 	echo "apt update" >> postinst
 	echo "exit 0" >> postinst
 }
-
