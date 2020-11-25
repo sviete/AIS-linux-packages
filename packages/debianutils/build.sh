f2f21afa7d276ce3bcc0dedd30d58ed34f447abf
andrzej <you@example.com>
2020-11-25 09:50:46 +0100

    packages/debianutils/build.sh

--- packages/debianutils/build.sh
+++ packages/debianutils/build.sh
@@ -4,7 +4,6 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=4.11.2
 TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/d/debianutils/debianutils_${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=3b680e81709b740387335fac8f8806d71611dcf60874e1a792e862e48a1650de
-
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/add-shell
 bin/installkernel
@@ -13,4 +12,3 @@ share/man/man8/add-shell.8
 share/man/man8/installkernel.8
 share/man/man8/remove-shell.8
 "
-
