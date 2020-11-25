8468671a243d9621812277b62c72a143dfe9fdb8
andrzej <you@example.com>
2020-11-25 12:41:05 +0100

    packages/vis/build.sh

--- packages/vis/build.sh
+++ packages/vis/build.sh
@@ -8,8 +8,6 @@ TERMUX_PKG_SRCURL=https://github.com/martanne/vis/archive/v$TERMUX_PKG_VERSION.t
 TERMUX_PKG_SHA256=9ab4a3f1c5953475130b3c286af272fe5cfdf7cbb7f9fbebd31e9ea4f34e487d
 TERMUX_PKG_DEPENDS="liblua53, libtermkey, lua-lpeg, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_pre_configure() {
 	CFLAGS+=" -I$TERMUX_PREFIX/include/lua5.3"
 }
-
