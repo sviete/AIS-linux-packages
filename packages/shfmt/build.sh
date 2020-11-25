25301f3d75ce2e57536654a0d1d3972343577073
andrzej <you@example.com>
2020-11-25 12:15:31 +0100

    packages/shfmt/build.sh

--- packages/shfmt/build.sh
+++ packages/shfmt/build.sh
@@ -5,19 +5,14 @@ TERMUX_PKG_VERSION=3.2.0
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/mvdan/sh/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6755f587fcb6f037f819b96a322b0273d0ab6ecb5911c005b9aae74292c4a819
-
 termux_step_make_install() {
 	cd "$TERMUX_PKG_SRCDIR"
-
 	termux_setup_golang
-
 	export GOPATH="$TERMUX_PKG_BUILDDIR"
 	mkdir -p "$GOPATH/src/github.com/mvdan"
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH/src/github.com/mvdan/sh"
-
 	go build -modcacherw \
 		-ldflags "-X main.version=$TERMUX_PKG_VERSION" \
 		-o "$TERMUX_PREFIX/bin/shfmt" \
 		./cmd/shfmt
 }
-
