b3cd43b1b58fa92fdfe668b3af6fd46401e3bcd9
andrzej <you@example.com>
2020-11-25 11:23:32 +0100

    packages/links/build.sh

--- packages/links/build.sh
+++ packages/links/build.sh
@@ -7,10 +7,8 @@ TERMUX_PKG_SRCURL=http://links.twibright.com/download/links-$TERMUX_PKG_VERSION.
 TERMUX_PKG_SHA256=f0e1fd131b5e9ff943bcc667e0aa6331048749ad4b921b6fadd14e919d9f79ac
 TERMUX_PKG_DEPENDS="brotli, libbz2, libevent, liblzma, libpng, librsvg, libtiff, libx11, openssl, zlib, zstd"
 TERMUX_PKG_BUILD_DEPENDS="libxt"
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --mandir=$TERMUX_PREFIX/share/man
 --enable-graphics
 --without-openmp
 "
-
