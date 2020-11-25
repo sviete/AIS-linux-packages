f3332678fea9976edc2feb861c0ab703088c2cd7
andrzej <you@example.com>
2020-11-25 11:41:24 +0100

    packages/netpbm/build.sh

--- packages/netpbm/build.sh
+++ packages/netpbm/build.sh
@@ -8,14 +8,12 @@ TERMUX_PKG_DEPENDS="libpng, libxml2, libjpeg-turbo, libtiff, zlib"
 TERMUX_PKG_BREAKS="netpbm-dev"
 TERMUX_PKG_REPLACES="netpbm-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_configure() {
 	# Put the android libpng-config script in the path (before the host one):
 	TERMUX_PKG_LIBPNG_CONFIG_DIR=$TERMUX_PKG_TMPDIR/libpng-config
 	mkdir -p $TERMUX_PKG_LIBPNG_CONFIG_DIR
 	cp $TERMUX_PREFIX/bin/libpng-config $TERMUX_PKG_LIBPNG_CONFIG_DIR/
 	export PATH=$TERMUX_PKG_LIBPNG_CONFIG_DIR:$PATH
-
 	# See $SRC/doc/INSTALL about netpbm build system. For automatic builds it recommends just copying config.mk.in
 	cd $TERMUX_PKG_SRCDIR
 	cp config.mk.in config.mk
@@ -28,7 +26,6 @@ termux_step_configure() {
 	echo "STATICLIB_TOO = n" >> config.mk
 	echo "INTTYPES_H = <inttypes.h>" >> config.mk
 	export STRIPPROG=$STRIP
-
 	echo "CC_FOR_BUILD = cc" >> config.mk
 	echo "LD_FOR_BUILD = cc" >> config.mk
 	echo "CFLAGS_FOR_BUILD = " >> config.mk
@@ -36,14 +33,10 @@ termux_step_configure() {
 	echo "JPEGLIB = ${TERMUX_PREFIX}/lib/libjpeg.so" >> config.mk
 	echo "TIFFLIB = ${TERMUX_PREFIX}/lib/libtiff.so" >> config.mk
 	echo "TIFFLIB_NEEDS_Z = N" >> config.mk
-
 	cp $TERMUX_PKG_BUILDER_DIR/standardppmdfont.c lib/
 }
-
 termux_step_make_install() {
 	rm -Rf /tmp/netpbm
 	make -j 1 package pkgdir=/tmp/netpbm
 	./installnetpbm
 }
-
-
