cd512c7c5c230e80d24f5cf0f66f192fb7f48e4f
andrzej <you@example.com>
2020-11-25 12:32:16 +0100

    packages/tokei/build.sh

--- packages/tokei/build.sh
+++ packages/tokei/build.sh
@@ -6,10 +6,8 @@ TERMUX_PKG_SRCURL=https://github.com/XAMPPRocky/tokei/archive/v$TERMUX_PKG_VERSI
 TERMUX_PKG_SHA256=031dabbe1253af53fea8258e11eeb352371b6cf0c790db573ca7be33f761c733
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--features all"
-
 termux_step_post_make_install() {
 	install -Dm700 \
 		"$TERMUX_PKG_SRCDIR/target/$CARGO_TARGET_NAME"/release/tokei \
 		"$TERMUX_PREFIX"/bin/tokei
 }
-
