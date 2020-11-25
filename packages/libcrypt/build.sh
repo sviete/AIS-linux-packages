6ae7779288887d4056fb452932f7a186640d1186
andrzej <you@example.com>
2020-11-25 10:46:46 +0100

    packages/libcrypt/build.sh

--- packages/libcrypt/build.sh
+++ packages/libcrypt/build.sh
@@ -7,13 +7,9 @@ TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_BREAKS="libcrypt-dev"
 TERMUX_PKG_REPLACES="libcrypt-dev"
-
 termux_step_make_install() {
 	$CC $CFLAGS $CPPFLAGS $LDFLAGS -Wall -Wextra -fPIC -shared $TERMUX_PKG_BUILDER_DIR/crypt3.c -lcrypto -o $TERMUX_PREFIX/lib/libcrypt.so
 	mkdir -p $TERMUX_PREFIX/include/
 	cp $TERMUX_PKG_BUILDER_DIR/crypt.h $TERMUX_PREFIX/include/
 	cp $TERMUX_PKG_BUILDER_DIR/LICENSE $TERMUX_PKG_SRCDIR/
 }
-
-
-
