2e4734f131c8bdf3719e4bad1effe012b6ef1d1d
andrzej <you@example.com>
2020-11-25 12:43:14 +0100

    packages/wireguard-tools/build.sh

--- packages/wireguard-tools/build.sh
+++ packages/wireguard-tools/build.sh
@@ -6,10 +6,8 @@ TERMUX_PKG_SRCURL=https://git.zx2c4.com/wireguard-tools/snapshot/wireguard-tools
 TERMUX_PKG_SHA256=51bc85e33a5b3cf353786ae64b0f1216d7a871447f058b6137f793eb0f53b7fd
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS=" -C src WITH_BASHCOMPLETION=yes WITH_WGQUICK=no WITH_SYSTEMDUNITS=no"
-
 termux_step_post_make_install() {
 	cd src/wg-quick
 	$CC $CFLAGS $LDFLAGS -DWG_CONFIG_SEARCH_PATHS="\"$TERMUX_ANDROID_HOME/.wireguard $TERMUX_PREFIX/etc/wireguard /data/misc/wireguard /data/data/com.wireguard.android/files\"" -o wg-quick android.c
 	install -Dm0700 wg-quick $TERMUX_PREFIX/bin/wg-quick
 }
-
