4cf1767c500b988bc9a95525bc9d4707846d1150
andrzej <you@example.com>
2020-11-25 11:42:51 +0100

    packages/ninja/build.sh

--- packages/ninja/build.sh
+++ packages/ninja/build.sh
@@ -5,16 +5,13 @@ TERMUX_PKG_VERSION=1.10.1
 TERMUX_PKG_SRCURL=https://github.com/ninja-build/ninja/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=a6b6f7ac360d4aabd54e299cc1d8fa7b234cd81b9401693da21221c62569a23e
 TERMUX_PKG_DEPENDS="libc++, libandroid-spawn"
-
 termux_step_pre_configure() {
 	CXXFLAGS+=" $CPPFLAGS"
 	LDFLAGS+=" -landroid-spawn"
 }
-
 termux_step_configure() {
 	$TERMUX_PKG_SRCDIR/configure.py
 }
-
 termux_step_make() {
 	if $TERMUX_ON_DEVICE_BUILD; then
 		$TERMUX_PKG_SRCDIR/configure.py --bootstrap
@@ -23,8 +20,6 @@ termux_step_make() {
 		ninja -j $TERMUX_MAKE_PROCESSES
 	fi
 }
-
 termux_step_make_install() {
 	cp ninja $TERMUX_PREFIX/bin
 }
-
