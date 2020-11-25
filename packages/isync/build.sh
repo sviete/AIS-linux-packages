bfd011bb776fa1425d008442341cd13647567a74
andrzej <you@example.com>
2020-11-25 10:32:05 +0100

    packages/isync/build.sh

--- packages/isync/build.sh
+++ packages/isync/build.sh
@@ -6,4 +6,3 @@ TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/isync/isync/${TERMUX
 TERMUX_PKG_SHA256=f2213bf7f90266e1295deafe39b02d1ba0b4c7f3b897c09cd17c60f0d4f4c882
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-sasl ac_cv_header_db=no ac_cv_berkdb4=no"
 TERMUX_PKG_DEPENDS="openssl, zlib"
-
