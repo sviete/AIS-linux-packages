03ec51583168f9c7265257999d349fa32539dee4
andrzej <you@example.com>
2020-11-25 12:33:13 +0100

    packages/tracepath/build.sh

--- packages/tracepath/build.sh
+++ packages/tracepath/build.sh
@@ -5,19 +5,15 @@ TERMUX_PKG_VERSION=20200821
 TERMUX_PKG_SRCURL=https://github.com/iputils/iputils/archive/s${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f265da0d02dd2259efd8c57a9c2e0c8bb3361abb14639fcffb26707be5783a5b
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_configure() {
 	return
 }
-
 termux_step_make() {
 	return
 }
-
 termux_step_make_install() {
 	CPPFLAGS+=" -DPACKAGE_VERSION=\"$TERMUX_PKG_VERSION\" -DHAVE_ERROR_H"
 	$CC $CFLAGS $CPPFLAGS $LDFLAGS -o $TERMUX_PREFIX/bin/tracepath iputils_common.c tracepath.c
-
 	local MANDIR=$TERMUX_PREFIX/share/man/man8
 	mkdir -p $MANDIR
 	cd $TERMUX_PKG_SRCDIR/doc
@@ -29,12 +25,9 @@ termux_step_make_install() {
 		custom-man.xsl \
 		tracepath.xml
 	cp tracepath.8 $MANDIR/
-
 	# Setup traceroute as an alias for tracepath, since traceroute
 	# requires root which most Termux user does not have, and tracepath
 	# is probably good enough for most:
 	(cd $TERMUX_PREFIX/bin && ln -f -s tracepath traceroute)
 	(cd $MANDIR && ln -f -s tracepath.8 traceroute.8)
 }
-
-
