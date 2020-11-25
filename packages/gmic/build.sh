8c424e5a9eeb24b8d67ef738c60b467ab80b8628
andrzej <you@example.com>
2020-11-25 10:14:58 +0100

    packages/gmic/build.sh

--- packages/gmic/build.sh
+++ packages/gmic/build.sh
@@ -7,22 +7,17 @@ TERMUX_PKG_SHA256=46177ef6612be56574eafa1b428636fe1c7388211cc852a9e0575bce3ccf43
 TERMUX_PKG_DEPENDS="libc++, libcurl, fftw, libpng, libjpeg-turbo, libtiff, zlib"
 TERMUX_PKG_BUILD_DEPENDS="graphicsmagick"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_configure() {
 	return 0;
 }
-
 termux_step_make() {
 	cd src/
 	make USR="$TERMUX_PREFIX" STRIP="$STRIP" \
 	     CFLAGS="$CXXFLAGS" LIBS="$LDFLAGS" cli
 }
-
 termux_step_make_install() {
 	cp src/gmic $TERMUX_PREFIX/bin/
 	cp src/gmic-gm $TERMUX_PREFIX/bin/
 	cp man/gmic.1.gz $TERMUX_PREFIX/share/man/man1/
 	cp man/gmic.1.gz $TERMUX_PREFIX/share/man/man1/gmic-gm.1.gz
 }
-
-
