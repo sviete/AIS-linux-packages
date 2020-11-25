57454de52a33c82f028e9fdcd217bc6b980bb9ce
andrzej <you@example.com>
2020-11-25 10:59:44 +0100

    packages/libmad/build.sh

--- packages/libmad/build.sh
+++ packages/libmad/build.sh
@@ -7,18 +7,13 @@ TERMUX_PKG_SRCURL=ftp://ftp.mars.org/pub/mpeg/libmad-${TERMUX_PKG_VERSION}.tar.g
 TERMUX_PKG_SHA256=bbfac3ed6bfbc2823d3775ebb931087371e142bb0e9bb1bee51a76a6e0078690
 TERMUX_PKG_BREAKS="libmad-dev"
 TERMUX_PKG_REPLACES="libmad-dev"
-
 termux_step_pre_configure() {
 	if [ $TERMUX_ARCH = arm ]; then
 		# See https://www.mars.org/pipermail/mad-dev/2013-July/001433.html
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --enable-fpm=default"
 	fi
 }
-
 termux_post_configure() {
 	cd $TERMUX_PKG_SRCDIR
 	sed -i -e 's/-force-mem//g' Makefile
 }
-
-
-
