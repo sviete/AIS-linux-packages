3c18df04a1c87b3107d78aff992b52bbecb9d359
andrzej <you@example.com>
2020-11-25 12:13:07 +0100

    packages/seccure/build.sh

--- packages/seccure/build.sh
+++ packages/seccure/build.sh
@@ -8,19 +8,15 @@ TERMUX_PKG_SRCURL=http://point-at-infinity.org/seccure/seccure-$TERMUX_PKG_VERSI
 TERMUX_PKG_SHA256=6566ce4afea095f83690b93078b910ca5b57b581ebc60e722f6e3fe8e098965b
 TERMUX_PKG_DEPENDS="libgcrypt"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_make() {
 	make seccure-key
 }
-
 termux_step_make_install() {
 	install -Dm700 seccure-key "$TERMUX_PREFIX"/bin/
 	install -Dm600 seccure.1 "$TERMUX_PREFIX"/share/man/man1/
-
 	for i in encrypt decrypt sign verify signcrypt veridec dh; do
 		ln -sfr "$TERMUX_PREFIX"/bin/seccure-key "$TERMUX_PREFIX"/bin/seccure-${i}
 		ln -sfr "$TERMUX_PREFIX"/share/man/man1/seccure.1 "$TERMUX_PREFIX"/share/man/man1/seccure-${i}.1
 	done
 	unset i
 }
-
