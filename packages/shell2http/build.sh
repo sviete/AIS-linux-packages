1b87f114005148067fd5e95977b018b1de0db764
andrzej <you@example.com>
2020-11-25 12:14:48 +0100

    packages/shell2http/build.sh

--- packages/shell2http/build.sh
+++ packages/shell2http/build.sh
@@ -6,12 +6,9 @@ TERMUX_PKG_VERSION=1.13
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/msoap/shell2http/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6094762a3e54efddcbc361d80ef281624d2647f17f6b0c787cab713626d861e3
-
 termux_step_make() {
 	termux_setup_golang
-
 	cd "$TERMUX_PKG_SRCDIR"
-
 	export GOPATH="${TERMUX_PKG_BUILDDIR}"
 	mkdir -p "${GOPATH}/src/github.com/msoap/"
 	cp -a "${TERMUX_PKG_SRCDIR}" "${GOPATH}/src/github.com/msoap/shell2http"
@@ -19,8 +16,6 @@ termux_step_make() {
 	go get -d -v
 	go build
 }
-
 termux_step_make_install() {
 	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/msoap/shell2http/shell2http
 }
-
