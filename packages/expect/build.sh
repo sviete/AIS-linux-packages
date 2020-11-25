73a2d5850f7d741513dba1ff037873ca25752052
andrzej <you@example.com>
2020-11-25 10:01:35 +0100

    packages/expect/build.sh

--- packages/expect/build.sh
+++ packages/expect/build.sh
@@ -8,14 +8,10 @@ TERMUX_PKG_SHA256=49a7da83b0bdd9f46d04a04deec19c7767bb9a323e40c4781f89caf760b92c
 TERMUX_PKG_DEPENDS="tcl"
 TERMUX_PKG_BREAKS="expect-dev"
 TERMUX_PKG_REPLACES="expect-dev"
-
 termux_step_pre_configure() {
 	autoconf
 }
-
 termux_step_post_make_install() {
 	cd $TERMUX_PREFIX/lib
 	ln -f -s expect${TERMUX_PKG_VERSION}/libexpect${TERMUX_PKG_VERSION}.so .
 }
-
-
