a76331ac822aacaa32e2d400e5c62bea30d92863
andrzej <you@example.com>
2020-11-25 11:29:59 +0100

    packages/megatools/build.sh

--- packages/megatools/build.sh
+++ packages/megatools/build.sh
@@ -8,12 +8,9 @@ TERMUX_PKG_SHA256=8dc1ca348633fd49de7eb832b323e8dc295f1c55aefb484d30e6475218558b
 TERMUX_PKG_DEPENDS="glib, libandroid-support, libcurl, libgmp, openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-fuse --enable-docs-build"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_pre_configure() {
     mkdir -p m4
     autoreconf -v --install
-
     sed -i -e 's/-V -qversion//' configure
     sed -i -e 's/GOBJECT_INTROSPECTION_CHECK/#GOBJECT_INTROSPECTION_CHECK/' configure
 }
-
