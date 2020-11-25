a39e59df178adc47e4ccd9ac84a247895fa7e222
andrzej <you@example.com>
2020-11-25 11:34:19 +0100

    packages/mpc/build.sh

--- packages/mpc/build.sh
+++ packages/mpc/build.sh
@@ -7,11 +7,8 @@ TERMUX_PKG_SRCURL=https://www.musicpd.org/download/mpc/${TERMUX_PKG_VERSION:0:1}
 TERMUX_PKG_SHA256=4f40ccbe18f5095437283cfc525a97815e983cbfd3a29e48ff610fa4f1bf1296
 TERMUX_PKG_DEPENDS="libiconv, libmpdclient"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Diconv=enabled"
-
 # There seems to be issues with sphinx-build when using concurrent builds:
 TERMUX_MAKE_PROCESSES=1
-
 termux_step_pre_configure() {
 	LDFLAGS+=" -liconv"
 }
-
