39ed60d33b51f16f6f0d5c7dc3403bcfdc053427
andrzej <you@example.com>
2020-11-25 10:07:27 +0100

    packages/fselect/build.sh

--- packages/fselect/build.sh
+++ packages/fselect/build.sh
@@ -5,10 +5,8 @@ TERMUX_PKG_VERSION=0.7.2
 TERMUX_PKG_SRCURL=https://github.com/jhspetersson/fselect/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=8b2cbf8aff709ffcab49ed59330655669ab185a524e89a101141d80cc025063b
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_post_make_install() {
 	install -Dm700 \
 		"$TERMUX_PKG_SRCDIR/target/$CARGO_TARGET_NAME"/release/fselect \
 		"$TERMUX_PREFIX"/bin/fselect
 }
-
