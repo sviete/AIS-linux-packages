a71bbc44183516ec5e3ec3609ab0a4cd6355cee2
andrzej <you@example.com>
2020-11-25 10:08:39 +0100

    packages/gatling/build.sh

--- packages/gatling/build.sh
+++ packages/gatling/build.sh
@@ -7,14 +7,11 @@ TERMUX_PKG_SHA256=6fa329d0ced0c80deb8dde5460e9d9e984bee94f265043d7fdec0e253dce9a
 TERMUX_PKG_DEPENDS="libcrypt, libiconv, openssl, zlib"
 TERMUX_PKG_BUILD_DEPENDS="libowfat"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_make() {
 	CPPFLAGS+=" -I$TERMUX_PREFIX/include/libowfat"
 	LDFLAGS+=" -lcrypt -lcrypto -liconv"
 	make -j $TERMUX_MAKE_PROCESSES CROSS="${CC/clang}"
 }
-
 termux_step_make_install() {
 	make install prefix=$TERMUX_PREFIX MANDIR=$TERMUX_PREFIX/share/man
 }
-
