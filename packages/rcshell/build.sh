6cb166bc93e1d219c129ce07ee1a57b7bbedf4c3
andrzej <you@example.com>
2020-11-25 12:04:55 +0100

    packages/rcshell/build.sh

--- packages/rcshell/build.sh
+++ packages/rcshell/build.sh
@@ -7,9 +7,7 @@ TERMUX_PKG_SRCURL=https://sources.voidlinux.org/rc-${TERMUX_PKG_VERSION}/rc-${TE
 TERMUX_PKG_SHA256=5ed26334dd0c1a616248b15ad7c90ca678ae3066fa02c5ddd0e6936f9af9bfd8
 TERMUX_PKG_DEPENDS="readline"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_func_setpgrp_void=yes
 rc_cv_sysv_sigcld=no
 "
-
