064834d93b2cb1264ebc81b15ec58546ac14ff71
andrzej <you@example.com>
2020-11-25 12:07:22 +0100

    packages/rgbds/build.sh

--- packages/rgbds/build.sh
+++ packages/rgbds/build.sh
@@ -5,4 +5,3 @@ TERMUX_PKG_VERSION=0.4.1
 TERMUX_PKG_SRCURL=https://github.com/rednex/rgbds/releases/download/v${TERMUX_PKG_VERSION}/rgbds-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=e4253de7f1fdf1cd202276b07150010e182490052c6da6ae6ddac5685b9fec45
 TERMUX_PKG_DEPENDS="libpng, zlib"
-
