f8913271d10bda1b722fff526d87b8af32bb3caf
andrzej <you@example.com>
2020-11-25 09:51:00 +0100

    packages/debootstrap/build.sh

--- packages/debootstrap/build.sh
+++ packages/debootstrap/build.sh
@@ -8,9 +8,7 @@ TERMUX_PKG_SHA256=5e5a8147ecdd6be0eea5ac4d6ed8192cc653e93f744dd3306c9b1cc51d6ca3
 TERMUX_PKG_DEPENDS="perl, proot, wget"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-
 termux_step_post_make_install() {
     mkdir -p ${TERMUX_PREFIX}/share/man/man8/
     install ${TERMUX_PKG_SRCDIR}/debootstrap.8 ${TERMUX_PREFIX}/share/man/man8/
 }
-
