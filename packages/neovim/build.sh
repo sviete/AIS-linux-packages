05c0f0bca5aeb50277e57f16af322a85a3f718b1
andrzej <you@example.com>
2020-11-25 11:40:55 +0100

    packages/neovim/build.sh

--- packages/neovim/build.sh
+++ packages/neovim/build.sh
@@ -7,7 +7,6 @@ TERMUX_PKG_SRCURL=https://github.com/neovim/neovim/archive/v${TERMUX_PKG_VERSION
 TERMUX_PKG_SHA256=2f76aac59363677f37592e853ab2c06151cca8830d4b3fe4675b4a52d41fc42c
 TERMUX_PKG_DEPENDS="libiconv, libuv, luv, libmsgpack, libandroid-support, libvterm, libtermkey, liblua53, libunibilium"
 TERMUX_PKG_HOSTBUILD=true
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DENABLE_JEMALLOC=OFF
 -DGETTEXT_MSGFMT_EXECUTABLE=$(which msgfmt)
@@ -20,29 +19,22 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DLUA_INCLUDE_DIR=$TERMUX_PREFIX/include/lua5.3
 "
 TERMUX_PKG_CONFFILES="share/nvim/sysinit.vim"
-
 termux_step_host_build() {
 	termux_setup_cmake
-
 	mkdir -p $TERMUX_PKG_HOSTBUILD_DIR/deps
 	cd $TERMUX_PKG_HOSTBUILD_DIR/deps
 	cmake $TERMUX_PKG_SRCDIR/third-party
 	make -j 1
-
 	cd $TERMUX_PKG_SRCDIR
 	make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$TERMUX_PKG_HOSTBUILD_DIR -DUSE_BUNDLED_LUAROCKS=ON" install
 	make distclean
 	rm -Rf build/
 }
-
 termux_step_pre_configure() {
 	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DLUA_MATH_LIBRARY=$TERMUX_STANDALONE_TOOLCHAIN/sysroot/usr/lib/$TERMUX_HOST_PLATFORM/$TERMUX_PKG_API_LEVEL/libm.so"
 }
-
 termux_step_post_make_install() {
 	local _CONFIG_DIR=$TERMUX_PREFIX/share/nvim
 	mkdir -p $_CONFIG_DIR
 	cp $TERMUX_PKG_BUILDER_DIR/sysinit.vim $_CONFIG_DIR/
 }
-
-
