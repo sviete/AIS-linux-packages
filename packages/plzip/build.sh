25cd0eb1bbeef1e12c248edd4183bae02b94d19b
andrzej <you@example.com>
2020-11-25 11:56:16 +0100

    packages/plzip/build.sh

--- packages/plzip/build.sh
+++ packages/plzip/build.sh
@@ -5,4 +5,3 @@ TERMUX_PKG_VERSION=1.8
 TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/lzip/plzip/plzip-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=edafae3c15142ac0ebd84c2231ff81da4f68db58359a737e750f2780686c3612
 TERMUX_PKG_DEPENDS="lzlib"
-
