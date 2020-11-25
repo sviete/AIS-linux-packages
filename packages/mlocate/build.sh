a25ee4a65f898bc46b1f8f4596967425631a5cd5
andrzej <you@example.com>
2020-11-25 11:32:53 +0100

    packages/mlocate/build.sh

--- packages/mlocate/build.sh
+++ packages/mlocate/build.sh
@@ -9,16 +9,12 @@ TERMUX_PKG_VERSION=0.26
 TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://releases.pagure.org/mlocate/mlocate-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=3063df79fe198fb9618e180c54baf3105b33d88fe602ff2d8570aaf944f1263e
-
 termux_step_pre_configure() {
 	CPPFLAGS+=" -DLINE_MAX=_POSIX2_LINE_MAX"
 }
-
 termux_step_create_debscripts() {
 	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
 	echo "mkdir -p $TERMUX_PREFIX/var/mlocate/" >> postinst
 	echo "exit 0" >> postinst
 	chmod 0755 postinst
 }
-
-
