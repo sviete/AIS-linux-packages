d63833447bf41a19a347be0001b43eb4a6e55c2c
andrzej <you@example.com>
2020-11-25 12:46:23 +0100

    packages/xorg-util-macros/build.sh

--- packages/xorg-util-macros/build.sh
+++ packages/xorg-util-macros/build.sh
@@ -8,9 +8,7 @@ TERMUX_PKG_REVISION=8
 TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/util/util-macros-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=d7e43376ad220411499a79735020f9d145fdc159284867e99467e0d771f3e712
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-
 termux_step_post_make_install() {
 	mkdir -p ${TERMUX_PREFIX}/lib/pkgconfig
 	mv ${TERMUX_PREFIX}/share/pkgconfig/xorg-macros.pc ${TERMUX_PREFIX}/lib/pkgconfig/
 }
-
