fe165596a5e25e7f85ccd9c5ee84dd7e0b66e6a3
andrzej <you@example.com>
2020-11-25 12:47:06 +0100

    packages/xvidcore/build.sh

--- packages/xvidcore/build.sh
+++ packages/xvidcore/build.sh
@@ -7,23 +7,17 @@ TERMUX_PKG_SHA256=abbdcbd39555691dd1c9b4d08f0a031376a3b211652c0d8b3b8aa9be1303ce
 TERMUX_PKG_BREAKS="xvidcore-dev"
 TERMUX_PKG_REPLACES="xvidcore-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_pre_configure() {
 	# Certain packages are not safe to build on device because their
 	# build.sh script deletes specific files in $TERMUX_PREFIX.
 	if $TERMUX_ON_DEVICE_BUILD; then
 		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
 	fi
-
 	rm -f $TERMUX_PREFIX/lib/libxvid*
 	export TERMUX_PKG_BUILDDIR=$TERMUX_PKG_BUILDDIR/build/generic
 	export TERMUX_PKG_SRCDIR=$TERMUX_PKG_BUILDDIR
-
 	if [ $TERMUX_ARCH = i686 ]; then
 		# Avoid text relocations:
 		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" --disable-assembly"
 	fi
 }
-
-
-
