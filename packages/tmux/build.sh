4340837543ac1f82760e8646796e89a289752d07
andrzej <you@example.com>
2020-11-25 12:31:47 +0100

    packages/tmux/build.sh

--- packages/tmux/build.sh
+++ packages/tmux/build.sh
@@ -8,26 +8,20 @@ TERMUX_PKG_SRCURL=https://github.com/tmux/tmux/archive/${TERMUX_PKG_VERSION}.tar
 TERMUX_PKG_SHA256=b9617dd4d1c541ebc21b6b5760d58102fc039a593786aab273b5dd95dd514bea
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-static"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 TERMUX_PKG_CONFFILES="etc/tmux.conf"
-
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
 	./autogen.sh
 }
-
 termux_step_post_make_install() {
 	cp $TERMUX_PKG_BUILDER_DIR/tmux.conf $TERMUX_PREFIX/etc/tmux.conf
-
 	mkdir -p $TERMUX_PREFIX/share/bash-completion/completions
 	termux_download \
 		https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/homebrew_1.0.0/completions/tmux \
 		$TERMUX_PREFIX/share/bash-completion/completions/tmux \
 		05e79fc1ecb27637dc9d6a52c315b8f207cf010cdcee9928805525076c9020ae
 }
-
 termux_step_create_debscripts() {
 	echo "#!$TERMUX_PREFIX/bin/sh" > postinst
 	echo "mkdir -p $TERMUX_PREFIX/var/run" >> postinst
 }
-
