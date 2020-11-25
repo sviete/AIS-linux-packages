fdb2583a49421a474bee8126e978629954e5afc8
andrzej <you@example.com>
2020-11-25 09:58:41 +0100

    packages/elinks/build.sh

--- packages/elinks/build.sh
+++ packages/elinks/build.sh
@@ -6,7 +6,6 @@ TERMUX_PKG_REVISION=5
 TERMUX_PKG_SRCURL=https://dl.bintray.com/xeffyr/sources/elinks/elinks-v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4e5000ee0d2fe3f18515946872d9c95123942c8a20e8a50cf24db2f17967cdf9
 TERMUX_PKG_DEPENDS="libexpat, libiconv, libidn, openssl, libbz2, zlib"
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-256-colors
 --enable-true-color
@@ -15,10 +14,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --without-brotli
 --without-gc
 "
-
 TERMUX_MAKE_PROCESSES=1
-
 termux_step_pre_configure() {
     ./autogen.sh
 }
-
