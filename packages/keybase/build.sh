296ab4abc774b87069f3869c1cc6e7c5f8dafc13
andrzej <you@example.com>
2020-11-25 10:36:52 +0100

    packages/keybase/build.sh

--- packages/keybase/build.sh
+++ packages/keybase/build.sh
@@ -6,22 +6,16 @@ TERMUX_PKG_SRCURL=https://github.com/keybase/client/archive/v${TERMUX_PKG_VERSIO
 TERMUX_PKG_SHA256=2610dbc44d84b93822f9e4ec510aac473116c953f1073a45aef983898a2289fd
 TERMUX_PKG_REPLACES="kbfs"
 TERMUX_PKG_CONFLICTS="kbfs"
-
 termux_step_make_install() {
 	cd $TERMUX_PKG_SRCDIR
-
 	termux_setup_golang
-
 	mkdir -p .gopath/src/github.com/keybase
 	ln -sf "$PWD" .gopath/src/github.com/keybase/client
 	export GOPATH="$PWD/.gopath"
-
 	go build -v -tags 'production' -o keybase github.com/keybase/client/go/keybase
 	go build -v -tags 'production' -o git-remote-keybase github.com/keybase/client/go/kbfs/kbfsgit/git-remote-keybase
 	go build -v -tags 'production' -o kbfsfusebin github.com/keybase/client/go/kbfs/kbfsfuse
-
 	cp keybase $TERMUX_PREFIX/bin/keybase
 	cp git-remote-keybase $TERMUX_PREFIX/bin/git-remote-keybase
 	cp kbfsfusebin $TERMUX_PREFIX/bin/kbfsfuse
 }
-
