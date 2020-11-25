5ddf9aa39528ddc7b5b9d3e45ab11ccee5f58852
andrzej <you@example.com>
2020-11-25 10:38:34 +0100

    packages/kubectl/build.sh

--- packages/kubectl/build.sh
+++ packages/kubectl/build.sh
@@ -6,21 +6,16 @@ TERMUX_PKG_VERSION=1.19.4
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://dl.k8s.io/v$TERMUX_PKG_VERSION/kubernetes-src.tar.gz
 TERMUX_PKG_SHA256=ab5b730703c89fb36cff52e8b2ee28c6945f960c8f353ea2658abe4876e0d8a4
-
 termux_step_get_source() {
 	mkdir -p "$TERMUX_PKG_CACHEDIR"
 	mkdir -p "$TERMUX_PKG_SRCDIR"
-
 	termux_download "$TERMUX_PKG_SRCURL" "$TERMUX_PKG_CACHEDIR"/kubernetes-src.tar.gz \
 		"$TERMUX_PKG_SHA256"
-
 	tar xf "$TERMUX_PKG_CACHEDIR"/kubernetes-src.tar.gz \
 		-C "$TERMUX_PKG_SRCDIR"
 }
-
 termux_step_make() {
 	termux_setup_golang
-
 	# Needed to generate manpages.
 	#(
 	#	export GOPATH="$TERMUX_PKG_BUILDDIR/host"
@@ -29,21 +24,16 @@ termux_step_make() {
 	#	cd "$TERMUX_PKG_SRCDIR"
 	#	./hack/update-generated-docs.sh
 	#)
-
 	export GOPATH="$TERMUX_PKG_BUILDDIR/target"
 	#chmod +w "$TERMUX_PKG_SRCDIR"/_output
 	#rm -rf "$TERMUX_PKG_SRCDIR"/_output
-
 	cd "$TERMUX_PKG_SRCDIR"/cmd/kubectl
 	go build .
 }
-
 termux_step_make_install() {
 	install -Dm700 "$TERMUX_PKG_SRCDIR"/cmd/kubectl/kubectl \
 		"$TERMUX_PREFIX"/bin/kubectl
-
 	#mkdir -p "$TERMUX_PREFIX"/share/man/man1
 	#cp -f "$TERMUX_PKG_SRCDIR"/docs/man/man1/kubectl-*.1 \
 	#	"$TERMUX_PREFIX"/share/man/man1/
 }
-
