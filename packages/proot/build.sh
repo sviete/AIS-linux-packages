d4e8db700c772d44590e81291fc3587adb9f93bc
andrzej <you@example.com>
2020-11-25 11:58:40 +0100

    packages/proot/build.sh

--- packages/proot/build.sh
+++ packages/proot/build.sh
@@ -8,23 +8,16 @@ TERMUX_PKG_REVISION=33
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
 TERMUX_PKG_SHA256=bae4b87e54bff55600a765b3c5154e5cd677ea85ca64d2fb5dc79255767b4e08
 TERMUX_PKG_DEPENDS="libtalloc"
-
 # Install loader in libexec instead of extracting it every time
 export PROOT_UNBUNDLE_LOADER=$TERMUX_PREFIX/libexec/proot
-
 termux_step_pre_configure() {
 	CPPFLAGS+=" -DARG_MAX=131072"
 }
-
 termux_step_make_install() {
 	cd $TERMUX_PKG_SRCDIR/src
 	make V=1
 	make install
-
 	mkdir -p $TERMUX_PREFIX/share/man/man1
 	cp $TERMUX_PKG_SRCDIR/doc/proot/man.1 $TERMUX_PREFIX/share/man/man1/proot.1
-
 	cp $TERMUX_PKG_BUILDER_DIR/termux-chroot $TERMUX_PREFIX/bin/
 }
-
-
