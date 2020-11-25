da6d06b7bb36645fed73a1b5b70ae86c7e704122
andrzej <you@example.com>
2020-11-25 09:42:50 +0100

    packages/clash/build.sh

--- packages/clash/build.sh
+++ packages/clash/build.sh
@@ -6,13 +6,10 @@ TERMUX_PKG_VERSION=1.3.0
 TERMUX_PKG_SRCURL="https://github.com/Dreamacro/clash/archive/v${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256=34a31e12eb7e639bdc2e19421fe3a8d9ad710e7d4c13bfc361c2023c9dfe04f6
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_make() {
 	termux_setup_golang
-
 	mkdir ./gopath
 	export GOPATH="$PWD/gopath"
-
 	GOBUILD=CGO_ENABLED=0 \
 		go build \
 			-trimpath \
@@ -22,8 +19,6 @@ termux_step_make() {
 			-o "clash.bin" \
 			main.go
 }
-
 termux_step_make_install() {
 	mv ./clash.bin "${TERMUX_PREFIX}/bin/clash"
 }
-
