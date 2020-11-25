6c236637a17bbee5e69d8210f0935ded9eca1917
andrzej <you@example.com>
2020-11-25 12:43:43 +0100

    packages/wordgrinder/build.sh

--- packages/wordgrinder/build.sh
+++ packages/wordgrinder/build.sh
@@ -7,17 +7,13 @@ TERMUX_PKG_SRCURL=https://github.com/davidgiven/wordgrinder/archive/$TERMUX_PKG_
 TERMUX_PKG_SHA256=856cbed2b4ccd5127f61c4997a30e642d414247970f69932f25b4b5a81b18d3f
 TERMUX_PKG_DEPENDS="liblua53, ncurses, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_make() {
 	termux_setup_ninja
-
 	# Missing and causes install failure.
 	touch licenses/COPYING.LuaFileSystem
-
 	make CC=gcc OBJDIR="$PWD/build" "$PWD"/build/lua
 	make OBJDIR="$PWD/build" LUA_PACKAGE=lua53
 }
-
 termux_step_make_install() {
 	install -Dm700 \
 		"$TERMUX_PKG_SRCDIR"/bin/wordgrinder-lua53-curses-release \
@@ -26,4 +22,3 @@ termux_step_make_install() {
 		"$TERMUX_PKG_SRCDIR"/bin/wordgrinder.1 \
 		"$TERMUX_PREFIX"/share/man/man1/
 }
-
