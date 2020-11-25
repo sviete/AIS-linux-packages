4b1376fe18f168fca8c5021cd7ef1817600674c2
andrzej <you@example.com>
2020-11-25 12:36:19 +0100

    packages/unifdef/build.sh

--- packages/unifdef/build.sh
+++ packages/unifdef/build.sh
@@ -5,10 +5,8 @@ TERMUX_PKG_VERSION=2.12
 TERMUX_PKG_SRCURL=https://dotat.at/prog/unifdef/unifdef-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=fba564a24db7b97ebe9329713ac970627b902e5e9e8b14e19e024eb6e278d10b
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_make_install() {
 	mkdir -p "$TERMUX_PREFIX"/share/man/man1/
 	install -Dm700 unifdef "$TERMUX_PREFIX"/bin/
 	install -Dm600 unifdef.1 "$TERMUX_PREFIX"/share/man/man1/
 }
-
