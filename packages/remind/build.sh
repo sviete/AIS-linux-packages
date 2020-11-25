1c2721fec4210031f816cf456a2f8eeb9af1b95a
andrzej <you@example.com>
2020-11-25 12:06:22 +0100

    packages/remind/build.sh

--- packages/remind/build.sh
+++ packages/remind/build.sh
@@ -7,8 +7,6 @@ TERMUX_PKG_SHA256=10f946fd5b5cd83e4ba435ca493d3ebff235a5808aef1f871010942e95ea8e
 TERMUX_PKG_DEPENDS="libandroid-glob"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_RM_AFTER_INSTALL="bin/tkremind share/man/man1/tkremind.1 bin/cm2rem.tcl share/man/man1/cm2rem.1"
-
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
 }
-
