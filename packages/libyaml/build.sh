56bcf436db0fbd902842a5f90f6394abe60deea5
andrzej <you@example.com>
2020-11-25 11:21:51 +0100

    packages/libyaml/build.sh

--- packages/libyaml/build.sh
+++ packages/libyaml/build.sh
@@ -7,13 +7,10 @@ TERMUX_PKG_SRCURL=https://github.com/yaml/libyaml/archive/$TERMUX_PKG_VERSION.ta
 TERMUX_PKG_SHA256=fa240dbf262be053f3898006d502d514936c818e422afdcf33921c63bed9bf2e
 TERMUX_PKG_BREAKS="libyaml-dev"
 TERMUX_PKG_REPLACES="libyaml-dev"
-
 termux_step_pre_configure() {
 	./bootstrap
 }
-
 termux_step_post_make_install() {
 	cd $TERMUX_PREFIX/lib
 	ln -s -f libyaml-0.so libyaml.so
 }
-
