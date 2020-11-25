c2b2fd0a1096951be07575618d8ff5bae9bb3556
andrzej <you@example.com>
2020-11-25 11:33:36 +0100

    packages/mosh/build.sh

--- packages/mosh/build.sh
+++ packages/mosh/build.sh
@@ -6,11 +6,9 @@ TERMUX_PKG_REVISION=26
 TERMUX_PKG_SRCURL=https://github.com/mobile-shell/mosh/releases/download/mosh-${TERMUX_PKG_VERSION}/mosh-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=da600573dfa827d88ce114e0fed30210689381bbdcff543c931e4d6a2e851216
 TERMUX_PKG_DEPENDS="libandroid-support, libc++, libprotobuf, ncurses, openssl, openssh"
-
 termux_step_pre_configure() {
 	termux_setup_protobuf
 }
-
 termux_step_post_make_install() {
 	cd $TERMUX_PREFIX/bin
 	mv mosh mosh.pl
@@ -20,5 +18,3 @@ termux_step_post_make_install() {
 		-std=c++11 -Wall -Wextra -Werror \
 		$TERMUX_PKG_BUILDER_DIR/mosh.cc -o mosh
 }
-
-
