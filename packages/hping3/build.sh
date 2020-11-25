c20f99e24c817beac804782b9b91c5bb11615580
andrzej <you@example.com>
2020-11-25 10:24:54 +0100

    packages/hping3/build.sh

--- packages/hping3/build.sh
+++ packages/hping3/build.sh
@@ -7,11 +7,7 @@ TERMUX_PKG_SRCURL=http://www.hping.org/hping3-20051105.tar.gz
 TERMUX_PKG_SHA256=f5a671a62a11dc8114fa98eade19542ed1c3aa3c832b0e572ca0eb1a5a4faee8
 TERMUX_PKG_DEPENDS="libandroid-shmem, libpcap, tcl"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_post_configure () {
 	export LDFLAGS+=" -landroid-shmem"
 	mkdir -p ${TERMUX_PREFIX}/share/man/man8
 }
-
-
-
