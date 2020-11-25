aaa81b81ef622c024453123302172b6625a3e379
andrzej <you@example.com>
2020-11-25 10:38:48 +0100

    packages/lastpass-cli/build.sh

--- packages/lastpass-cli/build.sh
+++ packages/lastpass-cli/build.sh
@@ -7,14 +7,10 @@ TERMUX_PKG_SRCURL=https://github.com/lastpass/lastpass-cli/archive/v$TERMUX_PKG_
 TERMUX_PKG_SHA256=f38e1ee7e06e660433a575a23b061c2f66ec666d746e988716b2c88de59aed73
 TERMUX_PKG_DEPENDS="libcurl, libxml2, openssl, pinentry"
 TERMUX_PKG_SUGGESTS="termux-api"
-
 termux_step_post_make_install() {
 	ninja install-doc
-
 	install -Dm600 "$TERMUX_PKG_SRCDIR"/contrib/lpass_zsh_completion \
 		"$TERMUX_PREFIX"/share/zsh/site-functions/_lpass
-
 	install -Dm600 "$TERMUX_PKG_SRCDIR"/contrib/completions-lpass.fish \
 		"$TERMUX_PREFIX"/share/fish/completions/lpass.fish
 }
-
