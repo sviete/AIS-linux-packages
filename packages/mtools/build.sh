ddab604ea999f5c65c6bc46fda56a51ac40a59f8
andrzej <you@example.com>
2020-11-25 11:36:17 +0100

    packages/mtools/build.sh

--- packages/mtools/build.sh
+++ packages/mtools/build.sh
@@ -5,13 +5,10 @@ TERMUX_PKG_VERSION=4.0.25
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/gnu/mtools/mtools-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=fd161eec3bb7a93d13936db67725ad3e17f2d5f4e6fa8f7667fbc7ac728e2c15
 TERMUX_PKG_DEPENDS="libandroid-support, libiconv"
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-floppyd
 ac_cv_lib_bsd_main=no
 "
-
 termux_step_pre_configure() {
 	export LIBS="-liconv"
 }
-
