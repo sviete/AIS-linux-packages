65804a03e776ecea9f5513ec24f4732607d27189
andrzej <you@example.com>
2020-11-25 11:41:10 +0100

    packages/netcat-openbsd/build.sh

--- packages/netcat-openbsd/build.sh
+++ packages/netcat-openbsd/build.sh
@@ -8,21 +8,17 @@ TERMUX_PKG_SHA256=bb6427c49015c8d485c013898b08808192bf5719c40a79676162e5c2d971a3
 TERMUX_PKG_DEPENDS="libbsd"
 TERMUX_PKG_CONFLICTS="netcat"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_pre_configure() {
 	local p
 	for p in $(cat debian/patches/series); do
 		echo "Applying debian/patches/$p"
 		patch -p1 -i debian/patches/$p
 	done
-
 	sed -i -e 's@-lresolv@@g' \
 		-e 's@CFLAGS=@CFLAGS?=@g' \
 		Makefile
-
 	CFLAGS+=" $CPPFLAGS $LDFLAGS"
 }
-
 termux_step_make_install() {
 	install -Dm700 nc $TERMUX_PREFIX/bin/netcat-openbsd
 	ln -sfr $TERMUX_PREFIX/bin/netcat-openbsd $TERMUX_PREFIX/bin/netcat
@@ -33,9 +29,7 @@ termux_step_make_install() {
 	ln -sfr $TERMUX_PREFIX/share/man/man1/netcat-openbsd.1 \
 		$TERMUX_PREFIX/share/man/man1/nc.1
 }
-
 termux_step_install_license() {
 	mkdir -p $TERMUX_PREFIX/share/doc/netcat-openbsd
 	head -n28 netcat.c | tail -n+2 > $TERMUX_PREFIX/share/doc/netcat-openbsd/LICENSE
 }
-
