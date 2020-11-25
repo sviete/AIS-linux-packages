b4ea648ef09cff4b4357e8b5eef09e65a05eb322
andrzej <you@example.com>
2020-11-25 11:35:33 +0100

    packages/mpv/build.sh

--- packages/mpv/build.sh
+++ packages/mpv/build.sh
@@ -7,16 +7,12 @@ TERMUX_PKG_SRCURL=https://github.com/mpv-player/mpv/archive/v${TERMUX_PKG_VERSIO
 TERMUX_PKG_SHA256=9163f64832226d22e24bbc4874ebd6ac02372cd717bef15c28a0aa858c5fe592
 TERMUX_PKG_DEPENDS="ffmpeg, libandroid-glob, libandroid-support, libarchive, libcaca, libiconv, liblua52, pulseaudio, openal-soft, zlib"
 TERMUX_PKG_RM_AFTER_INSTALL="share/icons share/applications"
-
 termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-glob"
 }
-
 termux_step_make_install() {
 	cd $TERMUX_PKG_SRCDIR
-
 	./bootstrap.py
-
 	./waf configure \
 		--prefix=$TERMUX_PREFIX \
 		--disable-gl \
@@ -31,12 +27,8 @@ termux_step_make_install() {
 		--enable-caca \
 		--disable-alsa \
 		--disable-x11
-
 	./waf -v install
-
 	# Use opensles audio out be default:
 	mkdir -p $TERMUX_PREFIX/etc/mpv
 	cp $TERMUX_PKG_BUILDER_DIR/mpv.conf $TERMUX_PREFIX/etc/mpv/mpv.conf
 }
-
-
