02476786a383bf6802df7a05c90ae4a7c12c2a5d
andrzej <you@example.com>
2020-11-25 09:43:19 +0100

    packages/cmark/build.sh

--- packages/cmark/build.sh
+++ packages/cmark/build.sh
@@ -8,12 +8,9 @@ TERMUX_PKG_SHA256=6a94aeaa59a583fadcbf28de81dea8641b3f56d935dda5b2447a3c8df6c95f
 TERMUX_PKG_BREAKS="cmark-dev"
 TERMUX_PKG_REPLACES="cmark-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_INSTALL_LIBDIR=$TERMUX_PREFIX/lib"
-
 termux_step_post_make_install() {
     cd $TERMUX_PREFIX/bin
     ln -f -s cmark-gfm cmark
-
     cd $TERMUX_PREFIX/share/man/man1
     ln -f -s cmark-gfm.1 cmark.1
 }
-
