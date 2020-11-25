67164dd8ef7a28d9f64b2177b78f2b8739e700df
andrzej <you@example.com>
2020-11-25 09:35:51 +0100

    packages/boinc/build.sh

--- packages/boinc/build.sh
+++ packages/boinc/build.sh
@@ -7,18 +7,14 @@ TERMUX_PKG_SHA256=8a3f9453b16acfd8c636d18b4939c461c751aa1dd9e108cf60b90a3909bfa0
 TERMUX_PKG_DEPENDS="libandroid-shmem, libc++, libcurl, openssl, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-server
 --disable-manager
 "
-
 TERMUX_PKG_CONFFILES="etc/boinc-client.conf"
-
 termux_step_pre_configure() {
 	CFLAGS="${CFLAGS/-Oz/-O2}"
 	CXXFLAGS="${CXXFLAGS/-Oz/-O2}"
 	LDFLAGS+=" -landroid-shmem"
 	./_autosetup
 }
-
