69659da19b0d88a9c4530b069f1204561faf5de1
andrzej <you@example.com>
2020-11-25 09:33:12 +0100

    packages/bat/build.sh

--- packages/bat/build.sh
+++ packages/bat/build.sh
@@ -7,18 +7,14 @@ TERMUX_PKG_SHA256=a2848389c8e213f63856ce7c664a4c069e3f28438e770ff9b93df272f61cde
 # bat calls less with '--RAW-CONTROL-CHARS' which busybox less does not support:
 TERMUX_PKG_DEPENDS="less, libgit2"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_pre_configure() {
 	CFLAGS="$CFLAGS $CPPFLAGS"
-
 	# See https://github.com/nagisa/rust_libloading/issues/54
 	export CC_x86_64_unknown_linux_gnu=gcc
 	export CFLAGS_x86_64_unknown_linux_gnu=""
 	export LIBGIT2_SYS_USE_PKG_CONFIG=1
 }
-
 termux_step_post_make_install() {
 	mkdir -p $TERMUX_PREFIX/share/man/man1
 	cp $(find . -name bat.1) $TERMUX_PREFIX/share/man/man1/
 }
-
