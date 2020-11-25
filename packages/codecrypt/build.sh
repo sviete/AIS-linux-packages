ab4d8600fff26c4d789c847be5b52e8bf1bc0259
andrzej <you@example.com>
2020-11-25 09:44:17 +0100

    packages/codecrypt/build.sh

--- packages/codecrypt/build.sh
+++ packages/codecrypt/build.sh
@@ -8,11 +8,9 @@ TERMUX_PKG_SRCURL=https://github.com/exaexa/codecrypt/archive/v$TERMUX_PKG_VERSI
 TERMUX_PKG_SHA256=25f11bc361b4f8aca7245698334b5715b7d594d708a75e8cdb2aa732dc46eb96
 TERMUX_PKG_DEPENDS="cryptopp, fftw, libgmp"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-cryptopp"
-
 termux_step_pre_configure() {
 	./autogen.sh
 	export LIBS="-lm"
 	export CRYPTOPP_CFLAGS="-I$TERMUX_PREFIX/include"
 	export CRYPTOPP_LIBS="-L$TERMUX_PREFIX/lib -lcryptopp"
 }
-
