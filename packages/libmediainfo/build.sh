9bd3ec53340fdef5b02924ffd1cd49279095fb0a
andrzej <you@example.com>
2020-11-25 11:00:44 +0100

    packages/libmediainfo/build.sh

--- packages/libmediainfo/build.sh
+++ packages/libmediainfo/build.sh
@@ -7,16 +7,13 @@ TERMUX_PKG_SRCURL=https://mediaarea.net/download/source/libmediainfo/${TERMUX_PK
 TERMUX_PKG_SHA256=a3d51fae55d8838e372def5d3fd172a795fa93ffa3389214bedb89e9dec90154
 TERMUX_PKG_DEPENDS="libcurl, libzen, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-shared --enable-static --with-libcurl"
-
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR="${TERMUX_PKG_SRCDIR}/Project/GNU/Library"
 	TERMUX_PKG_BUILDDIR="${TERMUX_PKG_SRCDIR}"
 	cd "${TERMUX_PKG_SRCDIR}"
 	./autogen.sh
 }
-
 termux_step_post_make_install() {
 	ln -sf "${TERMUX_PREFIX}/lib/libmediainfo.so" "${TERMUX_PREFIX}/lib/libmediainfo.so.0"
 	ln -sf "${TERMUX_PREFIX}/lib/libmediainfo.so" "${TERMUX_PREFIX}/lib/libmediainfo.so.0.0"
 }
-
