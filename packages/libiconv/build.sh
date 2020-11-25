a9a5a9c17236070238c3e54f52e86d08475966d6
andrzej <you@example.com>
2020-11-25 10:54:26 +0100

    packages/libiconv/build.sh

--- packages/libiconv/build.sh
+++ packages/libiconv/build.sh
@@ -7,7 +7,5 @@ TERMUX_PKG_SRCURL=https://ftp.gnu.org/pub/gnu/libiconv/libiconv-$TERMUX_PKG_VERS
 TERMUX_PKG_SHA256=e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04
 TERMUX_PKG_BREAKS="libandroid-support (<= 24), libiconv-dev, libandroid-support-dev"
 TERMUX_PKG_REPLACES="libandroid-support (<= 24), libiconv-dev, libandroid-support-dev"
-
 # Enable extra encodings (such as CP437) needed by some programs:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--enable-extra-encodings"
-
