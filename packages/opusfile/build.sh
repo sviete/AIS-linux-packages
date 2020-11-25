0962c5ac9223efac23f2e2fa96296e60b5fb0229
andrzej <you@example.com>
2020-11-25 11:48:22 +0100

    packages/opusfile/build.sh

--- packages/opusfile/build.sh
+++ packages/opusfile/build.sh
@@ -10,9 +10,7 @@ TERMUX_PKG_DEPENDS="libopus, libogg"
 TERMUX_PKG_BREAKS="opusfile-dev"
 TERMUX_PKG_REPLACES="opusfile-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-http"
-
 termux_step_pre_configure() {
 	echo "PACKAGE_VERSION=$TERMUX_PKG_VERSION" > package_version
 	./autogen.sh
 }
-
