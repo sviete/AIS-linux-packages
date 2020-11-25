86653b44a130c3529513d9cb660d147f6341e8b4
andrzej <you@example.com>
2020-11-25 12:24:22 +0100

    packages/tcl/build.sh

--- packages/tcl/build.sh
+++ packages/tcl/build.sh
@@ -10,7 +10,6 @@ TERMUX_PKG_DEPENDS="libsqlite, zlib"
 TERMUX_PKG_BREAKS="tcl-dev, tcl-static"
 TERMUX_PKG_REPLACES="tcl-dev, tcl-static"
 TERMUX_PKG_NO_STATICSPLIT=true
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_func_memcmp_working=yes
 ac_cv_func_memcmp=yes
@@ -24,19 +23,16 @@ tcl_cv_strtoul_unbroken=ok
 --enable-man-symlinks
 --mandir=$TERMUX_PREFIX/share/man
 "
-
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR=$TERMUX_PKG_SRCDIR/unix
 	CFLAGS+=" -DBIONIC_IOCTL_NO_SIGNEDNESS_OVERLOAD"
 }
-
 termux_step_post_make_install() {
 	# expect needs private headers
 	make install-private-headers
 	local _MAJOR_VERSION=${TERMUX_PKG_VERSION:0:3}
 	cd $TERMUX_PREFIX/bin
 	ln -f -s tclsh$_MAJOR_VERSION tclsh
-
 	# Hack to use system libsqlite (https://www.sqlite.org/howtocompile.html#tcl)
 	# since --with-system-sqlite fails to build:
 	local NEW_LIBDIR=$TERMUX_PREFIX/lib/tcl$_MAJOR_VERSION/sqlite
@@ -50,11 +46,8 @@ termux_step_post_make_install() {
 	local LIBSQLITE_VERSION=$($PKG_CONFIG --modversion sqlite3)
 	echo "package ifneeded sqlite3 $LIBSQLITE_VERSION [list load [file join \$dir libtclsqlite3.so] Sqlite3]" > \
 		$NEW_LIBDIR/pkgIndex.tcl
-
 	# Needed to install $TERMUX_PKG_LICENSE_FILE.
 	TERMUX_PKG_SRCDIR=$(dirname "$TERMUX_PKG_SRCDIR")
-
 	#avoid conflict with perl
 	mv $TERMUX_PREFIX/share/man/man3/Thread.3 $TERMUX_PREFIX/share/man/man3/Tcl_Thread.3
 }
-
