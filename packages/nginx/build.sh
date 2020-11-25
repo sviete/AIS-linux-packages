ea0c831908ae5c2725ee4ce526fdf35feb52938d
andrzej <you@example.com>
2020-11-25 11:42:22 +0100

    packages/nginx/build.sh

--- packages/nginx/build.sh
+++ packages/nginx/build.sh
@@ -17,25 +17,20 @@ etc/nginx/nginx.conf
 etc/nginx/scgi_params
 etc/nginx/uwsgi_params
 etc/nginx/win-utf"
-
 termux_step_pre_configure() {
 	# Certain packages are not safe to build on device because their
 	# build.sh script deletes specific files in $TERMUX_PREFIX.
 	if $TERMUX_ON_DEVICE_BUILD; then
 		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
 	fi
-
 	CPPFLAGS="$CPPFLAGS -DIOV_MAX=1024"
 	LDFLAGS="$LDFLAGS -landroid-glob"
-
 	# remove config from previous installs
 	rm -rf "$TERMUX_PREFIX/etc/nginx"
 }
-
 termux_step_configure() {
 	DEBUG_FLAG=""
 	$TERMUX_DEBUG && DEBUG_FLAG="--with-debug"
-
 	./configure \
 		--prefix=$TERMUX_PREFIX \
 		--crossbuild="Linux:3.16.1:$TERMUX_ARCH" \
@@ -65,16 +60,13 @@ termux_step_configure() {
 		--with-http_gunzip_module \
 		$DEBUG_FLAG
 }
-
 termux_step_post_make_install() {
 	# many parts are taken directly from Arch PKGBUILD
 	# https://git.archlinux.org/svntogit/packages.git/tree/trunk/PKGBUILD?h=packages/nginx
-
 	# set default port to 8080
 	sed -i "s| 80;| 8080;|" "$TERMUX_PREFIX/etc/nginx/nginx.conf"
 	cp conf/mime.types "$TERMUX_PREFIX/etc/nginx/"
 	rm "$TERMUX_PREFIX"/etc/nginx/*.default
-
 	# move default html dir
 	sed -e "44s|html|$TERMUX_PREFIX/share/nginx/html|" \
 		-e "54s|html|$TERMUX_PREFIX/share/nginx/html|" \
@@ -82,18 +74,15 @@ termux_step_post_make_install() {
 	rm -rf "$TERMUX_PREFIX/share/nginx"
 	mkdir -p "$TERMUX_PREFIX/share/nginx"
 	mv "$TERMUX_PREFIX/html/" "$TERMUX_PREFIX/share/nginx"
-
 	# install vim contrib
 	for i in ftdetect indent syntax; do
 		install -Dm644 "$TERMUX_PKG_SRCDIR/contrib/vim/${i}/nginx.vim" \
 			"$TERMUX_PREFIX/share/vim/vimfiles/${i}/nginx.vim"
 	done
-
 	# install man pages
 	mkdir -p "$TERMUX_PREFIX/share/man/man8"
 	cp "$TERMUX_PKG_SRCDIR/man/nginx.8" "$TERMUX_PREFIX/share/man/man8/"
 }
-
 termux_step_post_massage() {
 	# keep empty dirs which were deleted in massage
 	mkdir -p "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/var/log/nginx"
@@ -101,4 +90,3 @@ termux_step_post_massage() {
 		mkdir -p "$TERMUX_PKG_MASSAGEDIR/$TERMUX_PREFIX/var/lib/nginx/$dir"
 	done
 }
-
