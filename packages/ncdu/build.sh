89f0f806fab459ca92e0570918d6ab91819049f7
andrzej <you@example.com>
2020-11-25 11:39:00 +0100

    packages/ncdu/build.sh

--- packages/ncdu/build.sh
+++ packages/ncdu/build.sh
@@ -7,4 +7,3 @@ TERMUX_PKG_SRCURL=https://dev.yorhel.nl/download/ncdu-${TERMUX_PKG_VERSION}.tar.
 TERMUX_PKG_SHA256=b02ddc4dbf1db139cc6fbbe2f54a282770380f0ca5c17089855eab52a9ea3fb0
 TERMUX_PKG_DEPENDS="ncurses, libandroid-support"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-shell=$TERMUX_PREFIX/bin/bash"
-
