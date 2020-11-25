250c2ad38d475c9c44e42301ff5fec0ce19b9865
andrzej <you@example.com>
2020-11-25 09:42:21 +0100

    packages/cicada/build.sh

--- packages/cicada/build.sh
+++ packages/cicada/build.sh
@@ -7,8 +7,6 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/mitnk/cicada/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=8c037894ff9bb1225e1406ce2efbe14197441b920f91bbd6281e7553c3b322a0
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_pre_configure() {
     rm -f Makefile
 }
-
