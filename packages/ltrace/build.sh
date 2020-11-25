69106dcf0ecb47267becb946e3226ecdc054ad4b
andrzej <you@example.com>
2020-11-25 11:25:27 +0100

    packages/ltrace/build.sh

--- packages/ltrace/build.sh
+++ packages/ltrace/build.sh
@@ -5,19 +5,14 @@ TERMUX_PKG_VERSION=1:0.7.3
 TERMUX_PKG_SRCURL=https://github.com/dkogan/ltrace/archive/82c66409c7a93ca6ad2e4563ef030dfb7e6df4d4.tar.gz
 TERMUX_PKG_SHA256=4aecf69e4a33331aed1e50ce4907e73a98cbccc4835febc3473863474304d547
 TERMUX_PKG_DEPENDS="libc++, libelf"
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-werror
 --without-libunwind
 ac_cv_host=$TERMUX_ARCH-generic-linux-gnu
 "
-
 termux_step_pre_configure() {
 	if [ "$TERMUX_ARCH" == "arm" ]; then
 		CFLAGS+=" -DSHT_ARM_ATTRIBUTES=0x70000000+3"
 	fi
-
 	autoreconf -i ../src
 }
-
-
