d9b57003a45131b222786510a978173209dd2899
andrzej <you@example.com>
2020-11-25 09:38:59 +0100

    packages/calc/build.sh

--- packages/calc/build.sh
+++ packages/calc/build.sh
@@ -7,7 +7,6 @@ TERMUX_PKG_SRCURL=http://www.isthe.com/chongo/src/calc/calc-${TERMUX_PKG_VERSION
 TERMUX_PKG_SHA256=57af44181ca3af7348c82ee628cfd221677a09fef11a29d2e5667726d5aafc90
 TERMUX_PKG_DEPENDS="ncurses, ncurses-ui-libs, readline"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_make() {
 	# Fails with -j$TERMUX_MAKE_PROCESSES for some reason
 	make USE_READLINE="-DUSE_READLINE" READLINE_EXTRAS="-lhistory -lncurses" \
@@ -15,9 +14,7 @@ termux_step_make() {
 		READLINE_INCLUDE="-I$TERMUX_PREFIX/include" \
 		DEFAULT_LIB_INSTALL_PATH="$TERMUX_PREFIX/lib" CC="$CC" -j1
 }
-
 termux_step_make_install() {
 	make T="$TERMUX_PREFIX" BINDIR="/bin" LIBDIR="/lib" INCDIR="/include" \
 		SCRIPTDIR="/share/calc/cscript" CALC_SHAREDIR="/share/calc" install
 }
-
