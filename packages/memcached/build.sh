97d0ed59532445738dcdead598de1adb7598206c
andrzej <you@example.com>
2020-11-25 11:30:14 +0100

    packages/memcached/build.sh

--- packages/memcached/build.sh
+++ packages/memcached/build.sh
@@ -9,19 +9,13 @@ TERMUX_PKG_BREAKS="memcached-dev"
 TERMUX_PKG_REPLACES="memcached-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-static --enable-sasl --disable-coverage"
-
 termux_step_pre_configure() {
 	CPPFLAGS+=" -D__USE_GNU"
-
 	export ac_cv_c_endian=little
-
 	# Fix SASL configuration path
 	perl -p -i -e "s#/etc/sasl#$TERMUX_PREFIX/etc/sasl#" $TERMUX_PKG_BUILDDIR/sasl_defs.c
-
 	# getsubopt() taken from https://github.com/lxc/lxc/blob/master/src/include/getsubopt.c
 	cp $TERMUX_PKG_BUILDER_DIR/getsubopt.c $TERMUX_PKG_SRCDIR
 	cp $TERMUX_PKG_BUILDER_DIR/getsubopt.h $TERMUX_PKG_SRCDIR
-
 	autoreconf -vfi
 }
-
