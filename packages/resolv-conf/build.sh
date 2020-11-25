a5ccbc024c0fac19d268b2084026c82a6d32c56a
andrzej <you@example.com>
2020-11-25 12:06:52 +0100

    packages/resolv-conf/build.sh

--- packages/resolv-conf/build.sh
+++ packages/resolv-conf/build.sh
@@ -3,15 +3,11 @@ TERMUX_PKG_DESCRIPTION="Resolver configuration file"
 TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_VERSION=1.3
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
-
 TERMUX_PKG_CONFFILES="
 etc/hosts
 etc/resolv.conf
 "
-
 termux_step_make_install() {
 	printf "127.0.0.1 localhost\n::1 ip6-localhost\n" > $TERMUX_PREFIX/etc/hosts
 	printf "nameserver 8.8.8.8\nnameserver 8.8.4.4\n" > $TERMUX_PREFIX/etc/resolv.conf
 }
-
-
