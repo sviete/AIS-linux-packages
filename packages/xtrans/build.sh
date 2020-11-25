8f3663d6078a87a308f661919c0c1d048d10423d
andrzej <you@example.com>
2020-11-25 12:46:51 +0100

    packages/xtrans/build.sh

--- packages/xtrans/build.sh
+++ packages/xtrans/build.sh
@@ -9,9 +9,7 @@ TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/releases/individual/lib/xtrans-${
 TERMUX_PKG_SHA256=377c4491593c417946efcd2c7600d1e62639f7a8bbca391887e2c4679807d773
 TERMUX_PKG_NO_DEVELSPLIT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
-
 termux_step_post_make_install() {
 	mkdir -p ${TERMUX_PREFIX}/lib/pkgconfig
 	mv ${TERMUX_PREFIX}/share/pkgconfig/xtrans.pc ${TERMUX_PREFIX}/lib/pkgconfig
 }
-
