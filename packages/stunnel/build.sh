8a03dd074133576e25f078418bb28117d26de1e5
andrzej <you@example.com>
2020-11-25 12:21:16 +0100

    packages/stunnel/build.sh

--- packages/stunnel/build.sh
+++ packages/stunnel/build.sh
@@ -7,4 +7,3 @@ TERMUX_PKG_SHA256=af5ab973dde11807c38735b87bdd87563a47d2fa1c72a07929fcfce80a600f
 TERMUX_PKG_DEPENDS="openssl"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-shared --with-ssl=$TERMUX_PREFIX --disable-fips"
 TERMUX_PKG_RM_AFTER_INSTALL="bin/stunnel3 share/man/man8/stunnel.*.8"
-
