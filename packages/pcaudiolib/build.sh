a61a38373b5496b846452c099969ae0ad11792d7
andrzej <you@example.com>
2020-11-25 11:52:12 +0100

    packages/pcaudiolib/build.sh

--- packages/pcaudiolib/build.sh
+++ packages/pcaudiolib/build.sh
@@ -6,8 +6,6 @@ TERMUX_PKG_SRCURL=https://github.com/espeak-ng/pcaudiolib/archive/$TERMUX_PKG_VE
 TERMUX_PKG_SHA256=699a5a347b1e12dc5b122e192e19f4db01621826bf41b9ebefb1cbc63ae2180b
 TERMUX_PKG_DEPENDS="pulseaudio"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_pre_configure() {
 	./autogen.sh
 }
-
