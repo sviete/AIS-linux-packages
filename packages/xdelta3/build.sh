8d18ddf16a95c46e0c812424119f422679a8eea5
andrzej <you@example.com>
2020-11-25 12:45:08 +0100

    packages/xdelta3/build.sh

--- packages/xdelta3/build.sh
+++ packages/xdelta3/build.sh
@@ -5,12 +5,9 @@ TERMUX_PKG_VERSION=3.1.0
 TERMUX_PKG_SRCURL=https://github.com/jmacd/xdelta/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7515cf5378fca287a57f4e2fee1094aabc79569cfe60d91e06021a8fd7bae29d
 TERMUX_PKG_DEPENDS=liblzma
-
 termux_step_post_get_source() {
 	TERMUX_PKG_SRCDIR+=/xdelta3
 }
-
 termux_step_pre_configure() {
 	autoreconf --install
 }
-
