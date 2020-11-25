6fb48120327546a5df4c87d3392f8f03647221d7
andrzej <you@example.com>
2020-11-25 11:33:07 +0100

    packages/moreutils/build.sh

--- packages/moreutils/build.sh
+++ packages/moreutils/build.sh
@@ -7,11 +7,9 @@ TERMUX_PKG_SRCURL=http://ftp.debian.org/debian/pool/main/m/moreutils/moreutils_$
 TERMUX_PKG_SHA256=01f0b331e07e62c70d58c2dabbb68f5c4ddae4ee6f2d8f070fd1e316108af72c
 TERMUX_PKG_DEPENDS="perl"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 # chronic requires set of external perl modules.
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/chronic
 share/man/man1/chronic.1
 share/man/man1/parallel.1
 "
-
