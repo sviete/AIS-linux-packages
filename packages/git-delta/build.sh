80de9ab2b57167e84cd113a9813ddec398c046b7
andrzej <you@example.com>
2020-11-25 10:13:31 +0100

    packages/git-delta/build.sh

--- packages/git-delta/build.sh
+++ packages/git-delta/build.sh
@@ -6,17 +6,14 @@ TERMUX_PKG_SRCURL=https://github.com/dandavison/delta/archive/${TERMUX_PKG_VERSI
 TERMUX_PKG_SHA256=4068cb88a4110bbef24b8148e3ae283ff007f4f6aa4780789cda7a412928daa0
 TERMUX_PKG_DEPENDS="git"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_pre_configure() {
 	rm -f Makefile release.Makefile
 	export CC_x86_64_unknown_linux_gnu=gcc
 	export CFLAGS_x86_64_unknown_linux_gnu="-O2"
 }
-
 termux_step_post_make_install() {
 	install -Dm700 -t "$TERMUX_PREFIX"/bin \
 		"$TERMUX_PKG_SRCDIR/target/$CARGO_TARGET_NAME"/release/delta
 	install -Dm600 "$TERMUX_PKG_SRCDIR"/etc/completion/completion.bash \
 		"$TERMUX_PREFIX"/share/bash-completion/completions/delta
 }
-
