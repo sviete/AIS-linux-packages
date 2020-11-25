fb585aac777d84ce5d83b999a3d9f87f8f89afd2
andrzej <you@example.com>
2020-11-25 12:43:28 +0100

    packages/wol/build.sh

--- packages/wol/build.sh
+++ packages/wol/build.sh
@@ -7,11 +7,8 @@ TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/wake-on-lan/wol/${TE
 TERMUX_PKG_SHA256=e0086c9b9811df2bdf763ec9016dfb1bcb7dba9fa6d7858725b0929069a12622
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=$TERMUX_PREFIX/share/man"
 TERMUX_PKG_RM_AFTER_INSTALL="info/"
-
 termux_step_pre_configure() {
 	export ac_cv_func_mmap_fixed_mapped=yes
 	export jm_cv_func_working_malloc=yes
 	export ac_cv_func_alloca_works=yes
 }
-
-
