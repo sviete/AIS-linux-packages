eaedab65fedc1fd6a2242f9e9f73410a92e1c371
andrzej <you@example.com>
2020-11-25 10:07:12 +0100

    packages/frogcomposband/build.sh

--- packages/frogcomposband/build.sh
+++ packages/frogcomposband/build.sh
@@ -7,18 +7,14 @@ TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-x11 --bindir=$TERMUX_PREFIX/bin --sysconfdir=$TERMUX_PREFIX/share/frogcomposband"
 TERMUX_PKG_RM_AFTER_INSTALL="share/angband/xtra share/angband/icons"
-
 termux_step_pre_configure () {
 	./autogen.sh
 	perl -p -i -e 's|ncursesw5-config|ncursesw6-config|g' configure
 }
-
 termux_step_post_make_install () {
 	rm -Rf $TERMUX_PREFIX/share/frogcomposband/fonts
 }
-
 termux_step_install_license() {
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/LICENSE \
 		$TERMUX_PREFIX/share/doc/frogcomposband/LICENSE
 }
-
