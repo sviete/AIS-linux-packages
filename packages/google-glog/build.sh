2e5a7460e04df3e2dcbc48c0726ee2f3aaa02999
andrzej <you@example.com>
2020-11-25 10:17:08 +0100

    packages/google-glog/build.sh

--- packages/google-glog/build.sh
+++ packages/google-glog/build.sh
@@ -8,14 +8,11 @@ TERMUX_PKG_SHA256=f28359aeba12f30d73d9e4711ef356dc842886968112162bc73002645139c3
 TERMUX_PKG_DEPENDS="libc++"
 TERMUX_PKG_BREAKS="google-glog-dev"
 TERMUX_PKG_REPLACES="google-glog-dev"
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DBUILD_SHARED_LIBS=ON"
-
 termux_step_pre_configure() {
 	LDFLAGS+=" -llog"
 }
-
 termux_step_post_make_install() {
 	install -Dm600 "$TERMUX_PKG_SRCDIR"/libglog.pc.in \
 		"$TERMUX_PREFIX"/lib/pkgconfig/libglog.pc
@@ -25,4 +22,3 @@ termux_step_post_make_install() {
 	sed -i "s|@includedir@|$TERMUX_PREFIX/include|g" "$TERMUX_PREFIX"/lib/pkgconfig/libglog.pc
 	sed -i "s|@VERSION@|$TERMUX_PKG_VERSION|g" "$TERMUX_PREFIX"/lib/pkgconfig/libglog.pc
 }
-
