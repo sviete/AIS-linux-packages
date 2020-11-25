88b448684426f950123b50837dd1a8e2409ceed4
andrzej <you@example.com>
2020-11-25 10:09:23 +0100

    packages/gcal/build.sh

--- packages/gcal/build.sh
+++ packages/gcal/build.sh
@@ -9,7 +9,6 @@ TERMUX_PKG_DEPENDS="ncurses"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 ac_cv_header_spawn_h=no
 "
-
 termux_step_pre_configure() {
 	# Certain packages are not safe to build on device because their
 	# build.sh script deletes specific files in $TERMUX_PREFIX.
@@ -17,11 +16,9 @@ termux_step_pre_configure() {
 		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
 	fi
 }
-
 termux_step_post_make_install() {
 	# XXX: share/info/dir is currently included in emacs.
 	# We should probably make texinfo regenerate that file
 	# just as the man package does with the man database.
 	rm -f $TERMUX_PREFIX/share/info/dir
 }
-
