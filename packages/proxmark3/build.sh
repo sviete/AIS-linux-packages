287d8e074e30f268c836bf77d6da8a03352345e5
andrzej <you@example.com>
2020-11-25 11:59:23 +0100

    packages/proxmark3/build.sh

--- packages/proxmark3/build.sh
+++ packages/proxmark3/build.sh
@@ -8,14 +8,11 @@ TERMUX_PKG_SHA256=db93c2d3b9b7f477aca5628ed0906d9dba9c1999080452b24c601f38ab5b52
 TERMUX_PKG_BUILD_DEPENDS="readline"
 TERMUX_PKG_BUILD_IN_SRC="true"
 TERMUX_PKG_BLACKLISTED_ARCHES="i686, x86_64"
-
 termux_step_make() {
 	LDLIBS="-L${TERMUX_PREFIX}/lib" INCLUDES_CLIENT="-I${TERMUX_PREFIX}/include" \
 	make client -j $TERMUX_MAKE_PROCESSES CC=$CC CXX=$CXX LD=$CXX cpu_arch=generic SKIPREVENGTEST=1 SKIPQT=1 SKIPBT=1 SKIPPTHREAD=1
 }
-
 termux_step_make_install() {
 	install -Dm700 "$TERMUX_PKG_BUILDDIR"/client/proxmark3 \
 		"$TERMUX_PREFIX"/bin/proxmark3
 }
-
