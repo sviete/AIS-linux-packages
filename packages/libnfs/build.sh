65dd67ab9056d9ccf09a62867922cd0a9d9bfe64
andrzej <you@example.com>
2020-11-25 11:04:06 +0100

    packages/libnfs/build.sh

--- packages/libnfs/build.sh
+++ packages/libnfs/build.sh
@@ -7,8 +7,6 @@ TERMUX_PKG_SRCURL=https://github.com/sahlberg/libnfs/archive/libnfs-$TERMUX_PKG_
 TERMUX_PKG_SHA256=6ee77e9fe220e2d3e3b1f53cfea04fb319828cc7dbb97dd9df09e46e901d797d
 TERMUX_PKG_BREAKS="libnfs-dev"
 TERMUX_PKG_REPLACES="libnfs-dev"
-
 termux_step_pre_configure() {
 	autoreconf -fi
 }
-
