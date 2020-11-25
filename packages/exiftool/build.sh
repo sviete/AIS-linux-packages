9dcda20027ceee8f1b18e3eaf290215d42db58b2
andrzej <you@example.com>
2020-11-25 10:01:06 +0100

    packages/exiftool/build.sh

--- packages/exiftool/build.sh
+++ packages/exiftool/build.sh
@@ -6,14 +6,11 @@ TERMUX_PKG_VERSION=12.10
 TERMUX_PKG_SRCURL="http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=da611df6b25e5f7750ebe34c6b565bb7a4027c4ca209f9de723555f9dc204976
 TERMUX_PKG_DEPENDS="perl"
-
 termux_step_make_install() {
 	local current_perl_version=$(. $TERMUX_SCRIPTDIR/packages/perl/build.sh; echo $TERMUX_PKG_VERSION)
-
 	install -Dm700 "$TERMUX_PKG_SRCDIR"/exiftool "$TERMUX_PREFIX"/bin/exiftool
 	find "$TERMUX_PKG_SRCDIR"/lib -name "*.pod" -delete
 	mkdir -p "$TERMUX_PREFIX/lib/perl5/site_perl/$current_perl_version"
 	rm -rf "$TERMUX_PREFIX/lib/perl5/site_perl/${current_perl_version}"/{Image,File}
 	cp -a "$TERMUX_PKG_SRCDIR"/lib/{Image,File} "$TERMUX_PREFIX/lib/perl5/site_perl/${current_perl_version}/"
 }
-
