0b1be6200dbe06c71ed0ca25dd82f37b62daa661
andrzej <you@example.com>
2020-11-25 12:31:19 +0100

    packages/tk/build.sh

--- packages/tk/build.sh
+++ packages/tk/build.sh
@@ -9,27 +9,21 @@ TERMUX_PKG_SHA256=63df418a859d0a463347f95ded5cd88a3dd3aaa1ceecaeee362194bc30f3e3
 TERMUX_PKG_DEPENDS="fontconfig, libx11, libxft, libxss, tcl"
 TERMUX_PKG_NO_STATICSPLIT=true
 TERMUX_PKG_MAKE_INSTALL_TARGET="install install-private-headers"
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-threads
 --enable-64bit
 "
-
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR+="/unix"
 }
-
 termux_step_post_make_install() {
 	ln -sfr "$TERMUX_PREFIX/bin/wish${TERMUX_PKG_VERSION:0:3}" \
 		"$TERMUX_PREFIX"/bin/wish
 	ln -sfr "$TERMUX_PREFIX/lib/libtk${TERMUX_PKG_VERSION:0:3}.so" \
 		"$TERMUX_PREFIX"/lib/libtk.so
-
 	cd "$TERMUX_PKG_SRCDIR"/../
-
 	for dir in compat generic generic/ttk unix; do
 		install -dm755 "$TERMUX_PREFIX/include/tk-private/$dir"
 		install -m644 -t "$TERMUX_PREFIX/include/tk-private/$dir" "$dir"/*.h
 	done
 }
-
