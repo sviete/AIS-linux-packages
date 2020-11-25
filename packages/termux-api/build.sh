d0034e918356223df31f07b0826762daeaed37c3
andrzej <you@example.com>
2020-11-25 12:26:01 +0100

    packages/termux-api/build.sh

--- packages/termux-api/build.sh
+++ packages/termux-api/build.sh
@@ -6,4 +6,3 @@ TERMUX_PKG_SRCURL=https://github.com/termux/termux-api-package/archive/v${TERMUX
 TERMUX_PKG_SHA256=89141dcbb0b4b55b1d6c40e19ede74287eb5873b0cb9e9323d66f3f04d26e657
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="bash, util-linux"
-
