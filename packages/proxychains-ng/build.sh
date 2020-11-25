04f6315eb03a2006ffc25f1d3e9b05a15b18b35e
andrzej <you@example.com>
2020-11-25 11:59:37 +0100

    packages/proxychains-ng/build.sh

--- packages/proxychains-ng/build.sh
+++ packages/proxychains-ng/build.sh
@@ -6,7 +6,6 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/rofl0r/proxychains-ng/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=ab31626af7177cc2669433bb244b99a8f98c08031498233bb3df3bcc9711a9cc
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_pre_configure() {
 	# Certain packages are not safe to build on device because their
 	# build.sh script deletes specific files in $TERMUX_PREFIX.
@@ -14,10 +13,8 @@ termux_step_pre_configure() {
 		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
 	fi
 }
-
 termux_step_post_make_install() {
 	# Remove conf file from previous build, otherwise nothing will be done and it won't be included in the package
 	rm -f "$TERMUX_PREFIX"/etc/proxychains.conf
 	make install-config
 }
-
