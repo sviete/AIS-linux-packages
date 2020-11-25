cae7d5d72985047699e386ce0282ef0d6dcc5078
andrzej <you@example.com>
2020-11-25 10:50:21 +0100

    packages/libffi/build.sh

--- packages/libffi/build.sh
+++ packages/libffi/build.sh
@@ -9,7 +9,6 @@ TERMUX_PKG_BREAKS="libffi-dev"
 TERMUX_PKG_REPLACES="libffi-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-multi-os-directory"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/libffi-${TERMUX_PKG_VERSION}/include"
-
 termux_step_pre_configure() {
 	if [ $TERMUX_ARCH = arm ]; then
 		CFLAGS+=" -fno-integrated-as"
@@ -19,4 +18,3 @@ termux_step_post_configure() {
 	# work around since mmap can't be written and marked executable in android anymore from userspace
 	echo "#define FFI_MMAP_EXEC_WRIT 1" >> fficonfig.h
 }
-
