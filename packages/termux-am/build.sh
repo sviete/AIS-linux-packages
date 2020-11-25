a75dc96e783bf4b0207dda2aa105a45c6e464036
andrzej <you@example.com>
2020-11-25 12:25:47 +0100

    packages/termux-am/build.sh

--- packages/termux-am/build.sh
+++ packages/termux-am/build.sh
@@ -8,7 +8,6 @@ TERMUX_PKG_SHA256=3abfaca319f59fa769480c33d50e2ee1ff73122eac7c658f9e2a66a2da29eb
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_CONFLICTS="termux-tools (<< 0.51)"
-
 termux_step_make() {
 	# Download and use a new enough gradle version to avoid the process hanging after running:
 	termux_download \
@@ -17,21 +16,16 @@ termux_step_make() {
 		c8c17574245ecee9ed7fe4f6b593b696d1692d1adbfef425bef9b333e3a0e8de
 	mkdir $TERMUX_PKG_TMPDIR/gradle
 	unzip -q $TERMUX_PKG_CACHEDIR/gradle-5.4-bin.zip -d $TERMUX_PKG_TMPDIR/gradle
-
 	# Avoid spawning the gradle daemon due to org.gradle.jvmargs
 	# being set (https://github.com/gradle/gradle/issues/1434):
 	rm gradle.properties
-
 	export ANDROID_HOME
 	export GRADLE_OPTS="-Dorg.gradle.daemon=false -Xmx1536m"
-
 	$TERMUX_PKG_TMPDIR/gradle/gradle-5.4/bin/gradle \
 		:app:assembleRelease
 }
-
 termux_step_make_install() {
 	cp $TERMUX_PKG_SRCDIR/am-libexec-packaged $TERMUX_PREFIX/bin/am
 	mkdir -p $TERMUX_PREFIX/libexec/termux-am
 	cp $TERMUX_PKG_SRCDIR/app/build/outputs/apk/release/app-release-unsigned.apk $TERMUX_PREFIX/libexec/termux-am/am.apk
 }
-
