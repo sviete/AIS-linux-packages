e78a63053a31e4501422854e6bfe1b83bb0645b9
andrzej <you@example.com>
2020-11-25 10:38:19 +0100

    packages/krb5/build.sh

--- packages/krb5/build.sh
+++ packages/krb5/build.sh
@@ -22,36 +22,27 @@ DEFCCNAME=$TERMUX_PREFIX/tmp/krb5cc_%{uid}
 DEFKTNAME=$TERMUX_PREFIX/etc/krb5.keytab
 DEFCKTNAME=$TERMUX_PREFIX/var/krb5/user/%{euid}/client.keytab
 "
-
 termux_step_post_get_source() {
 	TERMUX_PKG_SRCDIR+="/src"
 }
-
 termux_step_pre_configure() {
 	# cannot test these when cross compiling
 	export krb5_cv_attr_constructor_destructor='yes,yes'
 	export ac_cv_func_regcomp='yes'
 	export ac_cv_printf_positional='yes'
-
 	# bionic doesn't have getpass
 	cp "$TERMUX_PKG_BUILDER_DIR/netbsd_getpass.c" "$TERMUX_PKG_SRCDIR/clients/kpasswd/"
-
 	CFLAGS="$CFLAGS -D_PASSWORD_LEN=PASS_MAX"
 	export LIBS="-landroid-glob"
 }
-
 termux_step_post_make_install() {
 	# Enable logging to STDERR by default
 	echo -e "\tdefault = STDERR" >> $TERMUX_PKG_SRCDIR/config-files/krb5.conf
-
 	# Sample KDC config file
 	install -dm 700 $TERMUX_PREFIX/var/krb5kdc
 	install -pm 600 $TERMUX_PKG_SRCDIR/config-files/kdc.conf $TERMUX_PREFIX/var/krb5kdc/kdc.conf
-
 	# Default configuration file
 	install -pm 600 $TERMUX_PKG_SRCDIR/config-files/krb5.conf $TERMUX_PREFIX/etc/krb5.conf
-
 	install -dm 700 $TERMUX_PREFIX/share/aclocal
 	install -m 600 $TERMUX_PKG_SRCDIR/util/ac_check_krb5.m4 $TERMUX_PREFIX/share/aclocal
 }
-
