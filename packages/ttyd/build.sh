8659392215ee1cfac742cbd478e1a6e82c23600d
andrzej <you@example.com>
2020-11-25 12:35:07 +0100

    packages/ttyd/build.sh

--- packages/ttyd/build.sh
+++ packages/ttyd/build.sh
@@ -7,7 +7,6 @@ TERMUX_PKG_SRCURL=https://github.com/tsl0922/ttyd/archive/$TERMUX_PKG_VERSION.ta
 TERMUX_PKG_SHA256=d72dcca3dec00cda87b80a0a25ae4fee2f8b9098c1cdb558508dcb14fbb6fafc
 TERMUX_PKG_DEPENDS="json-c, libuv, libwebsockets, zlib"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DCMAKE_XXD=$TERMUX_PKG_TMPDIR/xxd"
-
 termux_step_pre_configure() {
 	termux_download \
 		https://raw.githubusercontent.com/vim/vim/v8.1.0427/src/xxd/xxd.c \
@@ -15,4 +14,3 @@ termux_step_pre_configure() {
 		021b38e02cd31951a80ef5185271d71f2def727eb8ff65b7a07aecfbd688b8e1
 	gcc $TERMUX_PKG_CACHEDIR/xxd.c -o $TERMUX_PKG_TMPDIR/xxd
 }
-
