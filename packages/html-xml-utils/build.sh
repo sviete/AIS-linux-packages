662ea93fa3bd297ec8618d3232fc686efc23f139
andrzej <you@example.com>
2020-11-25 10:25:23 +0100

    packages/html-xml-utils/build.sh

--- packages/html-xml-utils/build.sh
+++ packages/html-xml-utils/build.sh
@@ -7,8 +7,6 @@ TERMUX_PKG_SRCURL=https://www.w3.org/Tools/HTML-XML-utils/html-xml-utils-${TERMU
 TERMUX_PKG_SHA256=d86ac96ea660316bef814c17b2a96d54cdf91c69e59614459865c2bfdaee433f
 TERMUX_PKG_DEPENDS="libcurl, libidn2"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_pre_configure() {
 	automake
 }
-
