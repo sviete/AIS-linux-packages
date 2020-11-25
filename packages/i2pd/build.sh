5d08923968ed914736c67f9cd0b151c1dac01ce5
andrzej <you@example.com>
2020-11-25 10:28:01 +0100

    packages/i2pd/build.sh

--- packages/i2pd/build.sh
+++ packages/i2pd/build.sh
@@ -10,36 +10,28 @@ TERMUX_PKG_DEPENDS="boost, miniupnpc, openssl, zlib"
 TERMUX_PKG_FORCE_CMAKE=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-DWITH_UPNP:BOOL=ON"
 TERMUX_PKG_RM_AFTER_INSTALL="src"
-
 TERMUX_PKG_CONFFILES="
 etc/i2pd/i2pd.conf
 etc/i2pd/tunnels.conf
 "
-
 termux_step_pre_configure() {
 	TERMUX_PKG_SRCDIR+="/build"
 	CXXFLAGS="${CXXFLAGS/-Oz/-O2}"
 }
-
 termux_step_post_make_install() {
 	cd $TERMUX_PKG_SRCDIR/../
-
 	install -Dm600 -t "$TERMUX_PREFIX"/etc/i2pd \
 		./contrib/i2pd.conf \
 		./contrib/tunnels.conf
-
 	local _file _dir
 	while read -r -d '' _file; do
 		_dir="${_file#contrib/certificates}"
 		_dir="${_dir%/*}"
 		install -Dm600 "$_file" -t "${TERMUX_PREFIX}/share/i2pd/certificates/${_dir}"
 	done < <(find contrib/certificates -type f -print0)
-
 	install -Dm600 -t "${TERMUX_PREFIX}"/share/doc/i2pd/tunnels.d \
 		./contrib/tunnels.d/README \
 		./contrib/tunnels.d/IRC-Ilita.conf \
 		./contrib/tunnels.d/IRC-Irc2P.conf
-
 	install -Dm600 -t "${TERMUX_PREFIX}"/share/man/man1 ./debian/i2pd.1
 }
-
