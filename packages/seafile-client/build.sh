f61fd446c4a1ddeeac7bc2a58323ea141d175c83
andrzej <you@example.com>
2020-11-25 12:12:53 +0100

    packages/seafile-client/build.sh

--- packages/seafile-client/build.sh
+++ packages/seafile-client/build.sh
@@ -8,16 +8,13 @@ TERMUX_PKG_DEPENDS="ccnet, libcurl, python"
 TERMUX_PKG_BREAKS="seafile-client-dev"
 TERMUX_PKG_REPLACES="seafile-client-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
-
 termux_step_pre_configure() {
 	./autogen.sh
 	export CPPFLAGS="-I$TERMUX_PKG_SRCDIR/lib $CPPFLAGS"
 	export PYTHON="python3.9"
 }
-
 termux_step_post_configure() {
 	#the package has trouble to prepare some headers
 	cd $TERMUX_PKG_SRCDIR/lib
 	python $TERMUX_PREFIX/bin/searpc-codegen.py $TERMUX_PKG_SRCDIR/lib/rpc_table.py
 }
-
