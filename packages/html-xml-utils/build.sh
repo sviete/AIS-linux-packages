b1097447a2a3cdfb6f27c336bda416b324a149ad
rysiekstation <rysiekstation@gmail.com>
2020-11-28 08:34:33 +0100

    HTML-XML-utils

--- packages/html-xml-utils/build.sh
+++ packages/html-xml-utils/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://www.w3.org/Tools/HTML-XML-utils/
 TERMUX_PKG_DESCRIPTION="A number of simple utilities for manipulating HTML and XML files"
-TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_VERSION=7.9
 TERMUX_PKG_SRCURL=https://www.w3.org/Tools/HTML-XML-utils/html-xml-utils-${TERMUX_PKG_VERSION}.tar.gz
