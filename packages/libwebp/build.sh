e43163a8ad5aeb2f2c8475a21c7864436be37c54
tomasz75 <tomasz@sviete.pl>
2021-01-07 16:03:52 +0100

    libwebp

--- packages/libwebp/build.sh
+++ packages/libwebp/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/webmproject/libwebp
 TERMUX_PKG_DESCRIPTION="Library to encode and decode images in WebP format"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.1.0
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/webmproject/libwebp/archive/v$TERMUX_PKG_VERSION.tar.gz
@@ -21,6 +22,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-wic
 "
 TERMUX_PKG_RM_AFTER_INSTALL="share/man/man1"
+
 termux_step_pre_configure() {
 	./autogen.sh
 }
