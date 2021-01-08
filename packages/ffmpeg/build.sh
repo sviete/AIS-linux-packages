d8fd46993af142f7ab27010cb154baf6c71c532f
tomasz75 <tomasz@sviete.pl>
2021-01-08 10:22:23 +0100

    remove enable-libcidstab

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -4,13 +4,14 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # NOTE: mpv has to be rebuilt and version bumped after updating ffmpeg.
 TERMUX_PKG_VERSION=4.3.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://www.ffmpeg.org/releases/ffmpeg-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=ad009240d46e307b4e03a213a0f49c11b650e445b1f8be0dda2a9212b34d2ffb
 TERMUX_PKG_DEPENDS="libass, libbz2, libdav1d, libiconv, libsoxr, libx264, libx265, xvidcore, libvorbis, libmp3lame, libopus, libvpx, libgnutls, libandroid-glob, freetype, zlib, liblzma, libvidstab, libwebp"
 TERMUX_PKG_CONFLICTS="libav"
 TERMUX_PKG_BREAKS="ffmpeg-dev"
 TERMUX_PKG_REPLACES="ffmpeg-dev"
-
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-fopenmp -static-openmp"
 termux_step_configure() {
 	cd $TERMUX_PKG_BUILDDIR
 
@@ -60,14 +61,9 @@ termux_step_configure() {
 		--enable-libvpx \
 		--enable-shared \
 		--enable-libsoxr \
-		--enable-libvidstab \
 		--enable-libwebp \
 		--prefix=$TERMUX_PREFIX \
 		--target-os=android \
 		--extra-libs="-landroid-glob" \
 		$_EXTRA_CONFIGURE_FLAGS
-}
-
-termux_step_pre_configure() {
-    LDFLAGS+=" --disable-openmp"
 }
