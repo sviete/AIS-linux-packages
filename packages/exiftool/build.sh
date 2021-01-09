29afb94663fcb2fe24bafc0304bda893fd418fc3
tomasz75 <tomasz@sviete.pl>
2021-01-09 20:29:42 +0100

    build packages/exiftool/build.sh

--- packages/exiftool/build.sh
+++ packages/exiftool/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://exiftool.org/
 TERMUX_PKG_DESCRIPTION="Utility for reading, writing and editing meta information in a wide variety of files."
 TERMUX_PKG_LICENSE="Artistic-License-2.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=12.10
+TERMUX_PKG_VERSION=12.14
 TERMUX_PKG_SRCURL="http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-$TERMUX_PKG_VERSION.tar.gz"
-TERMUX_PKG_SHA256=da611df6b25e5f7750ebe34c6b565bb7a4027c4ca209f9de723555f9dc204976
+TERMUX_PKG_SHA256=c1682bd467b0cfb3a385cfe17e413e33a15070d4fe55b82f0302ca47f42341a4
 TERMUX_PKG_DEPENDS="perl"
 termux_step_make_install() {
 	local current_perl_version=$(. $TERMUX_SCRIPTDIR/packages/perl/build.sh; echo $TERMUX_PKG_VERSION)
