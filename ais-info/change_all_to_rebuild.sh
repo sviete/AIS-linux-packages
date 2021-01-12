44ea4ff948c8b42c0a894d17e7055e2440cf9e9b
tomasz75 <tomasz@sviete.pl>
2021-01-12 19:26:51 +0100

    build ais-info/change_all_to_rebuild.sh

--- ais-info/change_all_to_rebuild.sh
+++ ais-info/change_all_to_rebuild.sh
@@ -5,25 +5,27 @@ git config --global credential.helper 'cache --timeout 7200'
 
 # changes
 # builds=`ls packages/z*/build.sh`
-# builds=`ls packages/*/build.sh`
-# for build in $builds; do
-#   echo "Adding line to file: $build"
-#   echo "" >> $build
-#   git add $build
-#   git commit -m "$build"
-#   git push
-#   sleep 10
-#   echo "done ------------------"
-# done
-
-
-builds=`ls packages/*/build.sh`
+builds=`ls packages/[p-z]*/build.sh`
 for build in $builds; do
-  echo "Remove back, the line from the end of the file: $build"
-  sed -i '/^$/d' $build
+  echo "Adding line to file: $build"
+  echo "" >> $build
   git add $build
   git commit -m "$build"
   git push
-  sleep 10
+  echo "Sleep 5 minutes"
+  sleep 300
   echo "done ------------------"
 done
+
+
+# builds=`ls packages/n*/build.sh`
+# for build in $builds; do
+#   echo "Remove back, the line from the end of the file: $build"
+#   sed -i '/^$/d' $build
+#   git add $build
+#   git commit -m "$build"
+#   git push
+#   echo "Sleep 5 minutes"
+#   sleep 300
+#   echo "done ------------------"
+# done
