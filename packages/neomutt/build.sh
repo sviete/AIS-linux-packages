03ac7134c7e4a7d464f95a654ac7d5203e703592
tomasz75 <tomasz@sviete.pl>
2021-01-12 18:09:40 +0100

    packages/neomutt/build.sh

--- packages/neomutt/build.sh
+++ packages/neomutt/build.sh
@@ -9,10 +9,8 @@ TERMUX_PKG_SHA256=29ac51a5ac5b4524dfdbb5ac78b59a1cc28c1090c541fb3da7bc86f838ab64
 TERMUX_PKG_DEPENDS="gdbm, krb5, libiconv, libsasl, ncurses, openssl, zlib, zstd"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_CONFFILES="etc/neomuttrc"
-
 termux_step_configure() {
 	./configure --host=$TERMUX_HOST_PLATFORM \
 		--prefix=$TERMUX_PREFIX --with-mailpath=$PREFIX/var/mail \
 		--disable-gpgme --disable-idn --zstd --sasl --ssl --gdbm --gss
 }
-
