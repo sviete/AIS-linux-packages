7f56e490e3b5061b792a8bebca1418fc905794db
tomasz75 <tomasz@sviete.pl>
2021-01-15 14:24:41 +0100

    packages/lzip/build.sh

--- packages/lzip/build.sh
+++ packages/lzip/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://download.savannah.gnu.org/releases/lzip/lzip-${TERMUX_PKG_VERSION}.tar.lz
 TERMUX_PKG_SHA256=68c703c7b5198b3fd7a0c3f20011e9a8c938b8dec14824c4c873922fdb01719f
 TERMUX_PKG_DEPENDS="libc++"
+
