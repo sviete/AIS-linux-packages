ffd7843ad22052643b60a321122fd718ab5c4eb7
tomasz75 <tomasz@sviete.pl>
2021-01-16 00:49:36 +0100

    packages/plzip/build.sh

--- packages/plzip/build.sh
+++ packages/plzip/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_SRCURL=http://download.savannah.gnu.org/releases/lzip/plzip/plzip-$TE
 TERMUX_PKG_SHA256=edafae3c15142ac0ebd84c2231ff81da4f68db58359a737e750f2780686c3612
 TERMUX_PKG_DEPENDS="lzlib"
 
+
