b274c3ca16057faa417c99ee76d06ed06bada753
tomasz75 <tomasz@sviete.pl>
2021-01-15 21:11:09 +0100

    packages/o-editor/build.sh

--- packages/o-editor/build.sh
+++ packages/o-editor/build.sh
@@ -19,3 +19,4 @@ termux_step_make_install() {
 	install -Dm700 -t "$TERMUX_PREFIX"/bin "$GOPATH"/src/github.com/xyproto/o/o
 	install -Dm600 -t "$TERMUX_PREFIX"/share/man/man1 "$TERMUX_PKG_SRCDIR"/o.1
 }
+
