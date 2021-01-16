62215a2b871971fda983f3e099366b2e4fcca48f
tomasz75 <tomasz@sviete.pl>
2021-01-16 07:56:04 +0100

    packages/silversearcher-ag/build.sh

--- packages/silversearcher-ag/build.sh
+++ packages/silversearcher-ag/build.sh
@@ -8,3 +8,4 @@ TERMUX_PKG_SRCURL=http://geoff.greer.fm/ag/releases/the_silver_searcher-${TERMUX
 TERMUX_PKG_SHA256=d9621a878542f3733b5c6e71c849b9d1a830ed77cb1a1f6c2ea441d4b0643170
 TERMUX_PKG_DEPENDS="pcre, liblzma, zlib"
 
+
