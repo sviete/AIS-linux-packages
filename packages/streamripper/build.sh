af45071c292f2eb5a2a9719ca3b441b1ee28867f
tomasz75 <tomasz@sviete.pl>
2021-01-16 09:47:49 +0100

    packages/streamripper/build.sh

--- packages/streamripper/build.sh
+++ packages/streamripper/build.sh
@@ -8,3 +8,4 @@ TERMUX_PKG_SHA256=c1d75f2e9c7b38fd4695be66eff4533395248132f3cc61f375196403c4d8de
 TERMUX_PKG_DEPENDS="glib, libmad, libvorbis"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_lib_pthread_pthread_create=yes"
 
+
