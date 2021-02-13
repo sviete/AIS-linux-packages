d14d26e10edac840ff744a67c35c2a7aa52c9dd1
tomasz75 <tomasz@sviete.pl>
2021-02-13 13:42:14 +0100

    build packages/diffutils/build.sh

--- packages/diffutils/build.sh
+++ packages/diffutils/build.sh
@@ -9,10 +9,3 @@ TERMUX_PKG_SHA256=b3a7a6221c3dc916085f0d205abf6b8e1ba443d4dd965118da364a1dc1cb3a
 TERMUX_PKG_DEPENDS="libiconv"
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="ac_cv_path_PR_PROGRAM=${TERMUX_PREFIX}/bin/pr"
-termux_step_pre_configure() {
-	if $TERMUX_DEBUG; then
-		# When doing debug build, -D_FORTIFY_SOURCE=2 gives an
-		# error: use of undeclared identifier '__USE_FORTIFY_LEVEL'
-		export CFLAGS=${CFLAGS/-D_FORTIFY_SOURCE=2/}
-	fi
-}
