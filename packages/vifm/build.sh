76123af7ec7294e15407714bf52b234c29f1b9f6
tomasz75 <tomasz@sviete.pl>
2021-02-14 01:46:13 +0100

    build packages/vifm/build.sh

--- packages/vifm/build.sh
+++ packages/vifm/build.sh
@@ -8,10 +8,4 @@ TERMUX_PKG_SHA256=7b76f39b2225db2960ee125da204ce6264fe992ec2d9f2954ab74ba3c28ce0
 TERMUX_PKG_DEPENDS="ncurses, file"
 termux_step_pre_configure() {
 	autoreconf -if
-	if $TERMUX_DEBUG; then
-		# Debug build fails with:
-		# /home/builder/.termux-build/vifm/src/src/fops_common.c:745:27: error: 'umask' called with invalid mode
-		#      saved_umask = umask(~0600);
-		export CFLAGS=${CFLAGS/-D_FORTIFY_SOURCE=2/}
-	fi
 }
