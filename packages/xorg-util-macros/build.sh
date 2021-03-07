1e32853fa23471ad19ed1030d317949e6594e559
araczkowski <araczkowski@gmail.com>
2021-03-07 16:13:24 +0100

    xorg-util-macros

--- packages/xorg-util-macros/build.sh
+++ packages/xorg-util-macros/build.sh
@@ -1,6 +1,6 @@
 # X11 package
 TERMUX_PKG_HOMEPAGE=https://xorg.freedesktop.org/
-TERMUX_PKG_DESCRIPTION="X.Org Autotools macros"
+TERMUX_PKG_DESCRIPTION="X.Org Autotools macros."
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
 TERMUX_PKG_VERSION=1.19.3
