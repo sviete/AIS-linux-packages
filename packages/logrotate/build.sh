dbe8fb7a30d08fe22655fd888092923844729460
araczkowski <araczkowski@gmail.com>
2021-03-07 15:01:45 +0100

    logrotate

--- packages/logrotate/build.sh
+++ packages/logrotate/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/logrotate/logrotate
-TERMUX_PKG_DESCRIPTION="Simplify the administration of log files on a system which generates a lot of log files"
+TERMUX_PKG_DESCRIPTION="Simplify the administration of log files on a system which generates a lot of log files."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Rabby Sheikh @shoya0x00"
 TERMUX_PKG_VERSION=3.18.0
