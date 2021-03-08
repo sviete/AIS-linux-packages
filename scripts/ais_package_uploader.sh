4cf682b4d8058972e9e9762abe1712555882996b
araczkowski <araczkowski@gmail.com>
2021-03-08 21:34:40 +0100

    scp fix

--- scripts/ais_package_uploader.sh
+++ scripts/ais_package_uploader.sh
@@ -15,8 +15,8 @@ echo "xxx" > ~/test/123.txt
 mkdir -p ~/.ssh
 echo $AIS_SSH_PRIVATE_KEY > ~/.ssh/private.key
 sudo chmod 600  ~/.ssh/private.key
-ssh-keyscan -i ~/.ssh/private.key -p ${AIS_PORT} -T 240 ${AIS_SERVER_IP} > ~/.ssh/known_hosts
+ssh-keyscan -p ${AIS_PORT} -T 240 ${AIS_SERVER_IP} > ~/.ssh/known_hosts
 
 # rsync -i ~/.ssh/private.key -e ssh -p ${AIS_PORT} --progress --stats -ravzh $DEBFILES_DIR_PATH ${AIS_USER}@${AIS_SERVER_IP}:/var/www/ais-debs-staging
 
-scp -P ${AIS_PORT} -r ~/test ${AIS_USER}@${AIS_SERVER_IP}:/var/www/ais-debs-staging
+scp -i ~/.ssh/private.key -P ${AIS_PORT} -r ~/test ${AIS_USER}@${AIS_SERVER_IP}:/var/www/ais-debs-staging
