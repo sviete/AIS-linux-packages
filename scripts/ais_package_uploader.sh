76b8853d209e87e11390cc0aaa9c353a5bbf04e4
araczkowski <araczkowski@gmail.com>
2021-03-08 21:04:49 +0100

    ais package uploader

---
+++ scripts/ais_package_uploader.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+##
+##  Package uploader for Bintray and AI-Speaker
+##
+##  Copyright 2021 AI-Speaker <info@ai-speaker.com>
+##
+
+set -o errexit
+set -o nounset
+
+
+echo "Upload test.txt to ais"
+mkdir -p ~/test
+echo "xxx" > ~/test/123.txt
+echo $AIS_SSH_PRIVATE_KEY > ~/.ssh/private.key
+sudo chmod 600  ~/.ssh/private.key
+ssh-keyscan -p ${AIS_PORT} -T 240 ${AIS_SERVER_IP} > ~/.ssh/known_hosts
+
+# rsync -i ~/.ssh/private.key -e ssh -p ${AIS_PORT} --progress --stats -ravzh $DEBFILES_DIR_PATH ${AIS_USER}@${AIS_SERVER_IP}:/var/www/ais-debs-staging
+
+scp -P ${AIS_PORT} -r ~/test ${AIS_USER}@${AIS_SERVER_IP}:/var/www/ais-debs-staging
