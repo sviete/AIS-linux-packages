509ccda4c12e5cc2a45508f6c2447c2623815f49
araczkowski <araczkowski@gmail.com>
2021-03-09 06:39:17 +0100

    fix upload

--- scripts/ais_package_uploader.sh
+++ scripts/ais_package_uploader.sh
@@ -8,15 +8,22 @@
 set -o errexit
 set -o nounset
 
+echo "Prepare ssh connection to ais"
+mkdir -p ~/.ssh/
+ssh-keyscan -p ${AIS_PORT} -T 240 ${AIS_SERVER_IP} > ~/.ssh/known_hosts
+echo "$AIS_SSH_PRIVATE_KEY" > ~/.ssh/staging.key
+chmod 600 ~/.ssh/staging.key
+cat >>~/.ssh/config <<END
+Host staging
+  HostName $AIS_SERVER_IP
+  Port $AIS_PORT
+  User $AIS_USER
+  IdentityFile ~/.ssh/staging.key
+  StrictHostKeyChecking no
+  AddKeysToAgent yes
+END
 
 echo "Upload test.txt to ais"
 mkdir -p ~/test
 echo "xxx" > ~/test/123.txt
-mkdir -p ~/.ssh
-echo $AIS_SSH_PRIVATE_KEY > ~/.ssh/private.key
-sudo chmod 600  ~/.ssh/private.key
-ssh-keyscan -p ${AIS_PORT} -T 240 ${AIS_SERVER_IP} > ~/.ssh/known_hosts
-
-# rsync -i ~/.ssh/private.key -e ssh -p ${AIS_PORT} --progress --stats -ravzh $DEBFILES_DIR_PATH ${AIS_USER}@${AIS_SERVER_IP}:/var/www/ais-debs-staging
-
-scp -i ~/.ssh/private.key -P ${AIS_PORT} -r ~/test ${AIS_USER}@${AIS_SERVER_IP}:/var/www/ais-debs-staging
+scp -pr -v ~/test staging:/var/www/ais-debs-staging
