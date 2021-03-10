2dada5fbf8494ce8baecf83f85acce8056034927
araczkowski <araczkowski@gmail.com>
2021-03-10 07:03:36 +0100

    all

--- ais-info/change_all_to_rebuild.sh
+++ ais-info/change_all_to_rebuild.sh
@@ -1,7 +1,7 @@
 # pull
 git pull
 # credential caching, it will be cached for 72000 seconds (20 hour) .
-git config --global credential.helper 'cache --timeout 7200'
+# git config --global credential.helper 'cache --timeout 7200'
 
 # changes
 # builds=`ls packages/z*/build.sh`
