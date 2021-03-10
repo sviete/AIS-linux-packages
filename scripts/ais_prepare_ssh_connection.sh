03467f92fa037908c808de798d48d98518bd9598
araczkowski <araczkowski@gmail.com>
2021-03-10 04:33:26 +0100

    scripts

---
+++ scripts/ais_prepare_ssh_connection.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+##
+##  Script to create AI-Speaker package repos
+##
+##  Copyright 2021 AI-Speaker <info@ai-speaker.com>
+##
+
+echo "Prepare ssh connection to ais"
+mkdir -p ~/.ssh/
+ssh-keyscan -p ${AIS_PORT} -T 240 ${AIS_SERVER_IP} > ~/.ssh/known_hosts
+echo "$AIS_SSH_PRIVATE_KEY" > ~/.ssh/staging.key
+chmod 600 ~/.ssh/staging.key
+cat >>~/.ssh/config <<END
+Host staging
+  HostName $AIS_SERVER_IP
+  Port $AIS_PORT
+  User $AIS_USER
+  IdentityFile ~/.ssh/staging.key
+  StrictHostKeyChecking no
+  AddKeysToAgent yes
+END
