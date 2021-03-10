78dbebef3af5849bdeceaa8544c985baaebb8122
araczkowski <araczkowski@gmail.com>
2021-03-10 04:45:32 +0100

    fix bash

--- scripts/ais_prepare_ssh_connection.sh
+++ scripts/ais_prepare_ssh_connection.sh
@@ -21,4 +21,4 @@ Host staging
 END
 
 echo "Publish cross-compiled packages to http://powiedz.co/apt/dists/dom-dev/test/..."
-ssh staging "./var/www/github_action_hooks/publish_test_repo.sh"
+ssh staging "bash /var/www/github_action_hooks/publish_test_repo.sh"
