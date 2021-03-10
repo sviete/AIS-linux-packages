5a79169fd683af310c626cf772b7f7ab288de308
araczkowski <araczkowski@gmail.com>
2021-03-10 04:14:48 +0100

    auto publish

--- scripts/ais_publish_to_apt_rrepo.sh
+++ scripts/ais_publish_to_apt_rrepo.sh
@@ -5,7 +5,26 @@
 ##  Copyright 2021 AI-Speaker <info@ai-speaker.com>
 ##
 
-echo "Publish cross-compiled packages to http://powiedz.co/apt/dists/dom-dev/beta/..."
-echo "TODO"
+
+
+echo "Prepare ssh connection to ais"
+mkdir -p ~/.ssh/
+ssh-keyscan -p ${AIS_PORT} -T 240 ${AIS_SERVER_IP} > ~/.ssh/known_hosts
+echo "$AIS_SSH_PRIVATE_KEY" > ~/.ssh/staging.key
+chmod 600 ~/.ssh/staging.key
+cat >>~/.ssh/config <<END
+Host staging
+  HostName $AIS_SERVER_IP
+  Port $AIS_PORT
+  User $AIS_USER
+  IdentityFile ~/.ssh/staging.key
+  StrictHostKeyChecking no
+  AddKeysToAgent yes
+END
+
+
+echo "Publish cross-compiled packages to http://powiedz.co/apt/dists/dom-dev/test/..."
+ssh staging:/var/www/ais-debs-staging "./var/www/github_action_hooks/publish_test_repo.sh"
+
 
 exit 0
