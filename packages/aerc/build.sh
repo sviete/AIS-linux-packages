67b71de1bfb21b06497ddeeb9ac27950d5b59929
araczkowski <araczkowski@gmail.com>
2021-03-12 00:08:45 +0100

    build packages/aerc/build.sh

--- packages/aerc/build.sh
+++ packages/aerc/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION=0.5.2
 TERMUX_PKG_SRCURL=https://git.sr.ht/~sircmpwn/aerc/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=87b922440e53b99f260d2332996537decb452c838c774e9340b633296f9f68ee
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_pre_configure() {
 	termux_setup_golang
 }
