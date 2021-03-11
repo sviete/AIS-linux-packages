876fd52ffd62885625bd26b43640be54db7c0f9c
araczkowski <araczkowski@gmail.com>
2021-03-11 23:38:26 +0100

    build ais-info/commit_per_file.sh

--- ais-info/commit_per_file.sh
+++ ais-info/commit_per_file.sh
@@ -1,7 +1,7 @@
 # pull
 git pull
 # credential caching, it will be cached for 7200 seconds (2 hour) .
-git config --global credential.helper 'cache --timeout 7200'
+# git config --global credential.helper 'cache --timeout 7200'
 # get changed files
 files=`git diff --name-only`
 git pull
