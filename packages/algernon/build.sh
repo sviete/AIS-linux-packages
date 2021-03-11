479b87cd6a1b545e3f9556415b812539da5821e5
araczkowski <araczkowski@gmail.com>
2021-03-12 00:13:47 +0100

    build packages/algernon/build.sh

--- packages/algernon/build.sh
+++ packages/algernon/build.sh
@@ -8,16 +8,21 @@ TERMUX_PKG_SHA256=6127eb975da960fd8aa7732c82f3b5e62d14ea763801778552bdbeec28846b
 
 termux_step_make() {
 	termux_setup_golang
+
 	export GOPATH=$TERMUX_PKG_BUILDDIR
 	mkdir -p "$GOPATH"/src/github.com/xyproto
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/xyproto/algernon
+
 	cd "$GOPATH"/src/github.com/xyproto/algernon
+
 	go build
 }
+
 termux_step_make_install() {
 	install -Dm700 \
 		"$GOPATH"/src/github.com/xyproto/algernon/algernon \
 		"$TERMUX_PREFIX"/bin/
+
 	# Offline samples may be useful to get started with Algernon.
 	rm -rf "$TERMUX_PREFIX"/share/doc/algernon
 	mkdir -p "$TERMUX_PREFIX"/share/doc/algernon
