6e787bbe72f528853f7f768b748873b9637883c7
araczkowski <araczkowski@gmail.com>
2021-03-12 00:18:50 +0100

    build packages/alpine/build.sh

--- packages/alpine/build.sh
+++ packages/alpine/build.sh
@@ -17,18 +17,24 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-passfile=$TERMUX_ANDROID_HOME/.pine-passfile
 "
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_pre_configure() {
 	export TCC=$CC
 	export TRANLIB=$RANLIB
 	export SPELLPROG=${TERMUX_PREFIX}/bin/hunspell
 	export alpine_SSLVERSION=old
 	export TPATH=$PATH
+
 	export LIBS="-lcrypt"
+
 	# To get S_IREAD and friends:
 	CPPFLAGS+=" -D__USE_BSD"
+
 	cp $TERMUX_PKG_BUILDER_DIR/pine.conf $TERMUX_PREFIX/etc/pine.conf
+
 	touch $TERMUX_PKG_SRCDIR/imap/lnxok
 }
+
 termux_step_post_configure() {
 	cd pith
 	$CC_FOR_BUILD help_c_gen.c -o help_c_gen
@@ -36,11 +42,13 @@ termux_step_post_configure() {
 	touch -d "next hour" help_c_gen help_h_gen
 }
 termux_step_create_debscripts() {
+
 	echo "#!$TERMUX_PREFIX/bin/sh" >> postinst
 	echo "if [ ! -e $TERMUX_ANDROID_HOME/.alpine-smime/.pwd/MasterPassword.crt ] && [ ! -e $HOME/.alpine-smime/.pwd/MasterPassword.key ]; then" >> postinst
 	echo "echo 'warning making a passwordless masterpasword file'" >> postinst
 	echo "mkdir -p \$HOME/.alpine-smime/public \$HOME/.alpine-smime/.pwd \$HOME/.alpine-smime/private \$HOME/.alpine-smime/ca" >> postinst
-	echo "openssl req -x509 -newkey rsa:2048 -keyout \$HOME/.alpine-smime/.pwd/MasterPassword.key -out \$HOME/.alpine-smime/.pwd/MasterPassword.crt -days 10000 -nodes -subj '/C=US/ST=dont/L=use/O=this Name/OU=for/CN=anything.pl.sviete.dom'" >> postinst
+	echo "openssl req -x509 -newkey rsa:2048 -keyout \$HOME/.alpine-smime/.pwd/MasterPassword.key -out \$HOME/.alpine-smime/.pwd/MasterPassword.crt -days 10000 -nodes -subj '/C=US/ST=dont/L=use/O=this Name/OU=for/CN=anything.com.termux'" >> postinst
 	echo "touch \$HOME/.pine-passfile" >> postinst
 	echo "fi" >> postinst
 }
+
