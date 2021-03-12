a3620753c92958fd1afb598b2beda00d5bb77e41
araczkowski <araczkowski@gmail.com>
2021-03-12 10:20:22 +0100

    build packages/beanshell/build.sh

--- packages/beanshell/build.sh
+++ packages/beanshell/build.sh
@@ -9,25 +9,31 @@ TERMUX_PKG_DEPENDS="dash, termux-tools"
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_pre_configure() {
 	termux_download \
 		"$TERMUX_PKG_SRCURL" \
 		"$TERMUX_PKG_CACHEDIR/bsh-$TERMUX_PKG_VERSION.jar" \
 		"$TERMUX_PKG_SHA256"
 }
+
 termux_step_make() {
 	"$ANDROID_HOME/build-tools/${TERMUX_ANDROID_BUILD_TOOLS_VERSION}/dx" \
 		--dex --output=beanshell.jar "$TERMUX_PKG_CACHEDIR/bsh-$TERMUX_PKG_VERSION.jar"
 }
+
 termux_step_make_install() {
 	install -Dm600 beanshell.jar "$TERMUX_PREFIX/share/dex/beanshell.jar"
+
 	{
 		echo "#!$TERMUX_PREFIX/bin/sh"
 		echo "dalvikvm -Xcompiler-option --compiler-filter=speed -cp $TERMUX_PREFIX/share/dex/beanshell.jar bsh.Interpreter \"\$@\""
 	} > "$TERMUX_PREFIX"/bin/beanshell
+
 	chmod 700 "$TERMUX_PREFIX"/bin/beanshell
 	ln -sfr "$TERMUX_PREFIX"/bin/beanshell "$TERMUX_PREFIX"/bin/bsh
 }
+
 termux_step_create_debscripts() {
 	cat <<- EOF > ./postinst
 	#!${TERMUX_PREFIX}/bin/bash
