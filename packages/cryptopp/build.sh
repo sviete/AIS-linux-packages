3acec54fe54e0b346f3c1ac48ec062f0d5aaceb5
araczkowski <araczkowski@gmail.com>
2021-03-12 13:21:56 +0100

    build packages/cryptopp/build.sh

--- packages/cryptopp/build.sh
+++ packages/cryptopp/build.sh
@@ -9,24 +9,29 @@ TERMUX_PKG_BREAKS="cryptopp-dev"
 TERMUX_PKG_REPLACES="cryptopp-dev"
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_BUILD_IN_SRC=true
+
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/
 share/cryptopp/
 "
+
 termux_step_get_source() {
 	mkdir -p $TERMUX_PKG_CACHEDIR
 	termux_download $TERMUX_PKG_SRCURL $TERMUX_PKG_CACHEDIR/cryptopp.zip \
 		$TERMUX_PKG_SHA256
 	mkdir -p $TERMUX_PKG_SRCDIR
 }
+
 termux_step_post_get_source() {
 	cd $TERMUX_PKG_SRCDIR
 	unzip $TERMUX_PKG_CACHEDIR/cryptopp.zip
 }
+
 termux_step_make() {
 	CXXFLAGS+=" -fPIC -DCRYPTOPP_DISABLE_ASM"
 	make -j $TERMUX_MAKE_PROCESSES dynamic libcryptopp.pc CC=$CC CXX=$CXX
 }
+
 termux_step_make_install() {
 	make install-lib PREFIX=$TERMUX_PREFIX
 }
