cf8133b404a0dcadbdfa6d019c69557056c27535
araczkowski <araczkowski@gmail.com>
2021-03-13 00:18:27 +0100

    build packages/imagemagick/build.sh

--- packages/imagemagick/build.sh
+++ packages/imagemagick/build.sh
@@ -8,6 +8,7 @@ TERMUX_PKG_SHA256=c04992429200d1168d6f45e2615e8ad6939dddb8f7559ebda669bcac66bc31
 TERMUX_PKG_DEPENDS="fftw, fontconfig, freetype, fribidi, glib, harfbuzz, libandroid-support, libbz2, libcairo, libffi, libgraphite, libjpeg-turbo, liblzma, libpixman, libpng, librsvg, libtiff, libuuid, libwebp, libx11, libxau, libxcb, libxdmcp, libxext, libxml2, littlecms, openjpeg, pango, pcre, zlib"
 TERMUX_PKG_BREAKS="imagemagick-dev, imagemagick-x"
 TERMUX_PKG_REPLACES="imagemagick-dev, imagemagick-x"
+
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --with-x
 --without-gvc
@@ -19,9 +20,11 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-openmp
 ac_cv_func_ftime=no
 "
+
 TERMUX_PKG_RM_AFTER_INSTALL="
 share/ImageMagick-7/francais.xml
 "
+
 termux_step_pre_configure() {
 	if [ $TERMUX_ARCH = "i686" ]; then
 		#Avoid "libMagickCore-7.Q16HDRI.so: error: undefined reference to '__atomic_load'"
