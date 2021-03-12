38e0bbff41f69e81fd41efd26a66c56878e331c4
araczkowski <araczkowski@gmail.com>
2021-03-12 23:12:54 +0100

    build packages/hors/build.sh

--- packages/hors/build.sh
+++ packages/hors/build.sh
@@ -6,9 +6,11 @@ TERMUX_PKG_VERSION=0.7.3
 TERMUX_PKG_SRCURL=https://github.com/WindSoilder/hors/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=f0e37fc5bb743ca577f5cae686feae5b3f6b756e69951bd441336b4a008063d7
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_pre_configure() {
 	if [ $TERMUX_ARCH = arm ]; then
 		CFLAGS+=" -fno-integrated-as"
 	fi
+
 	rm -f Makefile
 }
