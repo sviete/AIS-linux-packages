9b8b78abfe312446a5780e00d8b233f3f7d9c988
araczkowski <araczkowski@gmail.com>
2021-03-12 14:37:34 +0100

    build packages/dictd/build.sh

--- packages/dictd/build.sh
+++ packages/dictd/build.sh
@@ -8,6 +8,7 @@ TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/project/dict/dictd/dictd-${T
 TERMUX_PKG_SHA256=eeba51af77e87bb1b166c6bc469aad463632d40fb2bdd65e6675288d8e1a81e4
 TERMUX_PKG_DEPENDS="libmaa, zlib"
 TERMUX_PKG_CONFFILES="etc/dict.conf"
+
 termux_step_post_make_install() {
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/dict.conf $TERMUX_PREFIX/etc/dict.conf
 }
