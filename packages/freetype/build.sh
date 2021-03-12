508f3d7f5cdc29b6df9ad8e16acdf020247e14ac
araczkowski <araczkowski@gmail.com>
2021-03-12 18:45:02 +0100

    build packages/freetype/build.sh

--- packages/freetype/build.sh
+++ packages/freetype/build.sh
@@ -15,3 +15,4 @@ termux_step_post_make_install() {
 	install -Dm700 freetype-config $TERMUX_PREFIX/bin/freetype-config
 	install -Dm600 ../src/docs/freetype-config.1 $TERMUX_PREFIX/share/man/man1/freetype-config.1
 }
+
