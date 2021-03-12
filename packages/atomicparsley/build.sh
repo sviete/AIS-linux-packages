49c8655d1e55ab8749e4a6f9b2af2d05545781fd
araczkowski <araczkowski@gmail.com>
2021-03-12 09:45:03 +0100

    build packages/atomicparsley/build.sh

--- packages/atomicparsley/build.sh
+++ packages/atomicparsley/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION=1:20210114.184825.1dbe1be
 TERMUX_PKG_SRCURL=https://github.com/wez/atomicparsley/archive/${TERMUX_PKG_VERSION:2}.tar.gz
 TERMUX_PKG_SHA256=8877262c86d0ad231a5b0eaa8ab9c0c1d4e06fafea0b96a819d9a5e565a28b8c
 TERMUX_PKG_DEPENDS="libc++, zlib"
+
 termux_step_make_install() {
 	install -Dm700 -t $TERMUX_PREFIX/bin AtomicParsley
 }
