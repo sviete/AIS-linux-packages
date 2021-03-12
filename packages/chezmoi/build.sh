bcba327349d0783ae0bbefdabc3a224a780384a0
araczkowski <araczkowski@gmail.com>
2021-03-12 12:01:14 +0100

    build packages/chezmoi/build.sh

--- packages/chezmoi/build.sh
+++ packages/chezmoi/build.sh
@@ -8,23 +8,30 @@ TERMUX_PKG_SHA256=23515591028202aaf7af908e38972f156ee08022f535bc74aaa9244b82da2f
 
 termux_step_make() {
 	termux_setup_golang
+
 	cd "$TERMUX_PKG_SRCDIR"
+
 	mkdir -p "${TERMUX_PKG_BUILDDIR}/src/github.com/twpayne"
 	cp -a "${TERMUX_PKG_SRCDIR}" "${TERMUX_PKG_BUILDDIR}/src/github.com/twpayne/chezmoi"
 	cd "${TERMUX_PKG_BUILDDIR}/src/github.com/twpayne/chezmoi"
+
 	go get -d -v
 	go build -tags noupgrade,noembeddocs \
 		-ldflags "-X github.com/twpayne/chezmoi/cmd.DocsDir=$TERMUX_PREFIX/share/doc/chezmoi -X main.version=${TERMUX_PKG_VERSION}" .
 }
+
 termux_step_make_install() {
 	install -Dm700 ${TERMUX_PKG_BUILDDIR}/src/github.com/twpayne/chezmoi/chezmoi $TERMUX_PREFIX/bin/chezmoi
+
 	mkdir -p $TERMUX_PREFIX/share/bash-completions \
 		$TERMUX_PREFIX/share/fish/completions \
 		$TERMUX_PREFIX/share/doc/chezmoi
+
 	install ${TERMUX_PKG_BUILDDIR}/src/github.com/twpayne/chezmoi/completions/chezmoi-completion.bash \
 		$TERMUX_PREFIX/share/bash-completions/chezmoi
 	install ${TERMUX_PKG_BUILDDIR}/src/github.com/twpayne/chezmoi/completions/chezmoi.fish \
 		$TERMUX_PREFIX/share/fish/completions/chezmoi.fish
+
 	install ${TERMUX_PKG_BUILDDIR}/src/github.com/twpayne/chezmoi/docs/{FAQ,HOWTO,QUICKSTART,REFERENCE}.md \
 		$TERMUX_PREFIX/share/doc/chezmoi/
 }
