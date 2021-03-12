3837a8d0537d60a980729957ab3edb4fec294883
araczkowski <araczkowski@gmail.com>
2021-03-12 12:36:33 +0100

    build packages/cmusfm/build.sh

--- packages/cmusfm/build.sh
+++ packages/cmusfm/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Arkq/cmusfm/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9d9fa7df01c3dd7eecd72656e61494acc3b0111c07ddb18be0ad233110833b63
 TERMUX_PKG_DEPENDS=libcurl
+
 termux_step_pre_configure() {
   autoreconf --install
 }
