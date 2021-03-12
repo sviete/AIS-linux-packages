f068d69f81cf681114da8617d61fb4f30c9f1d93
araczkowski <araczkowski@gmail.com>
2021-03-12 15:07:50 +0100

    build packages/dnsmap/build.sh

--- packages/dnsmap/build.sh
+++ packages/dnsmap/build.sh
@@ -5,6 +5,7 @@ TERMUX_PKG_MAINTAINER="Rabby Sheikh @xploitednoob"
 TERMUX_PKG_VERSION=0.35
 TERMUX_PKG_SRCURL=https://github.com/resurrecting-open-source-projects/dnsmap/archive/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=6c8d28e461530721ed19314a6788f756a46c98356b5d66fa8b54294778c1f193
+
 termux_step_pre_configure() {
 	./autogen.sh
 }
