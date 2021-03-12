3b3764620ed75b5cc25000faf1d98341a5021787
araczkowski <araczkowski@gmail.com>
2021-03-12 15:43:09 +0100

    build packages/dte/build.sh

--- packages/dte/build.sh
+++ packages/dte/build.sh
@@ -7,11 +7,13 @@ TERMUX_PKG_SRCURL="https://craigbarnes.gitlab.io/dist/dte/dte-${TERMUX_PKG_VERSI
 TERMUX_PKG_SHA256='80d2732269a308b5e1126ecc16c28cda032864f625a95184821a73c054f81a2d'
 TERMUX_PKG_DEPENDS="libandroid-support, libandroid-glob, libiconv, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_make() {
 	make \
 		-j$TERMUX_MAKE_PROCESSES V=1 \
 		LDLIBS='-landroid-support -landroid-glob -liconv -lcurses'
 }
+
 termux_step_make_install() {
     make install V=1 prefix="$TERMUX_PREFIX"
 }
