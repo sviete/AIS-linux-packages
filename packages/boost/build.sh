aa88b7195ef4f262c69fc6a63bbe98071016bd6f
araczkowski <araczkowski@gmail.com>
2021-03-12 11:00:44 +0100

    build packages/boost/build.sh

--- packages/boost/build.sh
+++ packages/boost/build.sh
@@ -11,6 +11,7 @@ TERMUX_PKG_BUILD_DEPENDS="python"
 TERMUX_PKG_BREAKS="libboost-python (<= 1.65.1-2), boost-dev"
 TERMUX_PKG_REPLACES="libboost-python (<= 1.65.1-2), boost-dev"
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_pre_configure() {
 	# Certain packages are not safe to build on device because their
 	# build.sh script deletes specific files in $TERMUX_PREFIX.
@@ -18,13 +19,17 @@ termux_step_pre_configure() {
 		termux_error_exit "Package '$TERMUX_PKG_NAME' is not safe for on-device builds."
 	fi
 }
+
 termux_step_make_install() {
 	CXXFLAGS+=" -std=c++14"
+
 	rm $TERMUX_PREFIX/lib/libboost* -f
 	rm $TERMUX_PREFIX/include/boost -rf
+
 	CC= CXX= LDFLAGS= CXXFLAGS= ./bootstrap.sh
 	echo "using clang : $TERMUX_ARCH : $CXX : <linkflags>-L$TERMUX_PREFIX/lib ; " >> project-config.jam
 	echo "using python : 3.9 : $TERMUX_PREFIX/bin/python3 : $TERMUX_PREFIX/include/python3.9 : $TERMUX_PREFIX/lib ;" >> project-config.jam
+
 	if [ "$TERMUX_ARCH" = arm ] || [ "$TERMUX_ARCH" = aarch64 ]; then
 		BOOSTARCH=arm
 		BOOSTABI=aapcs
@@ -32,11 +37,13 @@ termux_step_make_install() {
 		BOOSTARCH=x86
 		BOOSTABI=sysv
 	fi
+
 	if [ "$TERMUX_ARCH" = x86_64 ] || [ "$TERMUX_ARCH" = aarch64 ]; then
 		BOOSTAM=64
 	elif [ "$TERMUX_ARCH" = i686 ] || [ "$TERMUX_ARCH" = arm ]; then
 		BOOSTAM=32
 	fi
+
 	./b2 target-os=android -j${TERMUX_MAKE_PROCESSES} \
 		include=$TERMUX_PREFIX/include \
 		toolset=clang-$TERMUX_ARCH \
