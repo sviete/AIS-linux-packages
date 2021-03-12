2c2b0795792e777eff881c891805e4b57e1fd4d2
araczkowski <araczkowski@gmail.com>
2021-03-12 21:26:55 +0100

    build packages/goaccess/build.sh

--- packages/goaccess/build.sh
+++ packages/goaccess/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_SRCURL=https://tar.goaccess.io/goaccess-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=c4820e781f75d9446f6b1be5fc8d11c6ec94a47fa8dd523001698c589f46c1ba
 TERMUX_PKG_DEPENDS="ncurses, openssl"
 TERMUX_PKG_BUILD_IN_SRC=true
+
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --enable-utf8
 --with-openssl"
