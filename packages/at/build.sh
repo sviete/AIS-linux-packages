0bc8d3e08f5dd2d065a5f6eea78b55bf3debcfd8
araczkowski <araczkowski@gmail.com>
2021-03-12 09:40:00 +0100

    build packages/at/build.sh

--- packages/at/build.sh
+++ packages/at/build.sh
@@ -8,8 +8,10 @@ TERMUX_PKG_SRCURL=https://salsa.debian.org/debian/at/-/archive/upstream/${TERMUX
 TERMUX_PKG_SHA256=b1b515d3700d8f2447d2c6caebb84e2eaaa8dfda2c02a32232f2ad8f947cd685
 TERMUX_PKG_SUGGESTS="termux-services"
 TERMUX_PKG_BUILD_IN_SRC=true
+
 # Force make -j1.
 TERMUX_MAKE_PROCESSES=1
+
 # Setting loadavg_mx to 8.0 as most devices (8 core)
 # do not have loadavg below 5-6.
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
@@ -19,11 +21,14 @@ ac_cv_path_SENDMAIL=$TERMUX_PREFIX/bin/sendmail
 --with-jobdir=$TERMUX_PREFIX/var/spool/atd
 --with-atspool=$TERMUX_PREFIX/var/spool/atd
 "
+
 # at.allow and at.deny are not supported in Termux.
 TERMUX_PKG_RM_AFTER_INSTALL="
 share/man/man5
 "
+
 TERMUX_PKG_SERVICE_SCRIPT=("atd" "mkdir -p $TERMUX_PREFIX/var/run && exec atd")
+
 termux_step_create_debscripts() {
 	cat <<- EOF > ./postinst
 	#!${TERMUX_PREFIX}/bin/sh
