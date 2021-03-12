b774f419bf63a83be964d350e1127dcd30e2a628
araczkowski <araczkowski@gmail.com>
2021-03-12 17:39:27 +0100

    build packages/ffmpeg/build.sh

--- packages/ffmpeg/build.sh
+++ packages/ffmpeg/build.sh
@@ -4,17 +4,18 @@ TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 # NOTE: mpv has to be rebuilt and version bumped after updating ffmpeg.
 TERMUX_PKG_VERSION=4.3.1
-TERMUX_PKG_REVISION=4
 TERMUX_PKG_SRCURL=https://www.ffmpeg.org/releases/ffmpeg-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=ad009240d46e307b4e03a213a0f49c11b650e445b1f8be0dda2a9212b34d2ffb
-TERMUX_PKG_DEPENDS="libass, libbz2, libdav1d, libiconv, libsoxr, libx264, libx265, xvidcore, libvorbis, libmp3lame, libopus, libvpx, libgnutls, libandroid-glob, freetype, zlib, liblzma, libwebp"
+TERMUX_PKG_DEPENDS="libass, libbz2, libdav1d, libiconv, libsoxr, libx264, libx265, xvidcore, libvorbis, libmp3lame, libopus, libvpx, libgnutls, libandroid-glob, freetype, zlib, liblzma, libvidstab, libwebp"
 TERMUX_PKG_CONFLICTS="libav"
 TERMUX_PKG_BREAKS="ffmpeg-dev"
 TERMUX_PKG_REPLACES="ffmpeg-dev"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-fopenmp -static-openmp"
+
 termux_step_configure() {
 	cd $TERMUX_PKG_BUILDDIR
+
 	export ASFLAGS="-no-integrated-as"
+
 	local _EXTRA_CONFIGURE_FLAGS=""
 	if [ $TERMUX_ARCH = "arm" ]; then
 		_ARCH="armeabi-v7a"
@@ -32,6 +33,7 @@ termux_step_configure() {
 	else
 		termux_error_exit "Unsupported arch: $TERMUX_ARCH"
 	fi
+
 	$TERMUX_PKG_SRCDIR/configure \
 		--arch=${_ARCH} \
 		--as=$AS \
@@ -58,9 +60,11 @@ termux_step_configure() {
 		--enable-libvpx \
 		--enable-shared \
 		--enable-libsoxr \
+		--enable-libvidstab \
 		--enable-libwebp \
 		--prefix=$TERMUX_PREFIX \
 		--target-os=android \
 		--extra-libs="-landroid-glob" \
 		$_EXTRA_CONFIGURE_FLAGS
 }
+
