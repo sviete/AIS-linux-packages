f69e2c18407e4945be7c2bfc8970e9fbcbe40691
araczkowski <araczkowski@gmail.com>
2021-03-13 00:23:29 +0100

    build packages/inetutils/build.sh

--- packages/inetutils/build.sh
+++ packages/inetutils/build.sh
@@ -24,6 +24,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 --disable-uucpd
 ac_cv_lib_crypt_crypt=no
 "
+
 termux_step_pre_configure() {
 	CPPFLAGS+=" -DLOGIN_PROCESS=6 -DDEAD_PROCESS=8 -DLOG_NFACILITIES=24"
 }
