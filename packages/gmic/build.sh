febae70f476fa717ed0dd623815a68da48710cac
araczkowski <araczkowski@gmail.com>
2021-03-12 20:51:34 +0100

    build packages/gmic/build.sh

--- packages/gmic/build.sh
+++ packages/gmic/build.sh
@@ -8,17 +8,21 @@ TERMUX_PKG_SHA256=d22fe8515af16dcd050d09a0bc7127ec29f4795ce732a0c6c3eaf839356bc1
 TERMUX_PKG_DEPENDS="libc++, libcurl, fftw, libpng, libjpeg-turbo, libtiff, zlib"
 TERMUX_PKG_BUILD_DEPENDS="graphicsmagick"
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_configure() {
 	return 0;
 }
+
 termux_step_make() {
 	cd src/
 	make USR="$TERMUX_PREFIX" STRIP="$STRIP" \
 	     CFLAGS="$CXXFLAGS" LIBS="$LDFLAGS" cli
 }
+
 termux_step_make_install() {
 	cp src/gmic $TERMUX_PREFIX/bin/
 	cp src/gmic-gm $TERMUX_PREFIX/bin/
 	cp man/gmic.1.gz $TERMUX_PREFIX/share/man/man1/
 	cp man/gmic.1.gz $TERMUX_PREFIX/share/man/man1/gmic-gm.1.gz
 }
+
