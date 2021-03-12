c2b2e30ca7ab8c4462dc5635821d4c8522981abb
araczkowski <araczkowski@gmail.com>
2021-03-12 12:26:27 +0100

    build packages/cmatrix/build.sh

--- packages/cmatrix/build.sh
+++ packages/cmatrix/build.sh
@@ -7,8 +7,10 @@ TERMUX_PKG_SRCURL=https://github.com/abishekvashok/cmatrix/archive/v${TERMUX_PKG
 TERMUX_PKG_SHA256=ad93ba39acd383696ab6a9ebbed1259ecf2d3cf9f49d6b97038c66f80749e99a
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--mandir=$TERMUX_PREFIX/share/man"
 TERMUX_PKG_DEPENDS="ncurses"
+
 termux_step_pre_configure() {
 	autoreconf -i
+
 	export ac_cv_file__usr_lib_kbd_consolefonts=no
 	export ac_cv_file__usr_share_consolefonts=no
 	export ac_cv_file__usr_lib_X11_fonts_misc=no
