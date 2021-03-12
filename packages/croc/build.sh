55ef2b93ff57b489629132500e45bb2b04ca4315
araczkowski <araczkowski@gmail.com>
2021-03-12 13:11:50 +0100

    build packages/croc/build.sh

--- packages/croc/build.sh
+++ packages/croc/build.sh
@@ -6,11 +6,15 @@ TERMUX_PKG_VERSION=8.6.7
 TERMUX_PKG_SRCURL=https://github.com/schollz/croc/releases/download/v${TERMUX_PKG_VERSION}/croc_${TERMUX_PKG_VERSION}_src.tar.gz
 TERMUX_PKG_SHA256=9be0a9e11160d3b3293cefa14fa2b99283c4d0dcb753dbc31502c28f10ca6b82
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_make() {
 	cd $TERMUX_PKG_SRCDIR
+
 	termux_setup_golang
+
 	go build -o croc -trimpath
 }
+
 termux_step_make_install() {
 	install -m755 croc $TERMUX_PREFIX/bin/croc
 }
