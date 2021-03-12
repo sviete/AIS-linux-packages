5a37b93f78622fd8b8bf7e1fe11fcac53a03ee17
araczkowski <araczkowski@gmail.com>
2021-03-12 12:46:38 +0100

    build packages/colordiff/build.sh

--- packages/colordiff/build.sh
+++ packages/colordiff/build.sh
@@ -9,11 +9,13 @@ TERMUX_PKG_SHA256=46e8c14d87f6c4b77a273cdd97020fda88d5b2be42cf015d5d84aca3dfff3b
 TERMUX_PKG_DEPENDS="perl"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+
 termux_step_make() {
 	# Skip the 'make' invocation as it only tries to
 	# rebuild the documentation.
 	continue
 }
+
 termux_step_post_configure() {
 	export INSTALL_DIR=${PREFIX}/bin
 	export MAN_DIR=${PREFIX}/share/man/man1
