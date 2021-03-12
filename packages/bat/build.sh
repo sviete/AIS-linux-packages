6c976ccb99bf906448247f97c1713ca8e69ccd63
araczkowski <araczkowski@gmail.com>
2021-03-12 10:10:16 +0100

    build packages/bat/build.sh

--- packages/bat/build.sh
+++ packages/bat/build.sh
@@ -11,10 +11,12 @@ TERMUX_PKG_DEPENDS="less, libgit2"
 TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_pre_configure() {
 	CFLAGS="$CFLAGS $CPPFLAGS"
+
 	# See https://github.com/nagisa/rust_libloading/issues/54
 	export CC_x86_64_unknown_linux_gnu=gcc
 	export CFLAGS_x86_64_unknown_linux_gnu=""
 }
+
 termux_step_post_make_install() {
 	mkdir -p $TERMUX_PREFIX/share/man/man1
 	cp $(find . -name bat.1) $TERMUX_PREFIX/share/man/man1/
