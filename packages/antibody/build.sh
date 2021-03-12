fb1cb9e614fb5871ea232bd60344b2dab5938f5a
araczkowski <araczkowski@gmail.com>
2021-03-12 08:00:46 +0100

    build packages/antibody/build.sh

--- packages/antibody/build.sh
+++ packages/antibody/build.sh
@@ -5,14 +5,18 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=6.1.1
 TERMUX_PKG_SRCURL=https://github.com/getantibody/antibody/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=87bced5fba8cf5d587ea803d33dda72e8bcbd4e4c9991a9b40b2de4babbfc24f
+
 termux_step_make() {
 	termux_setup_golang
+
 	export GOPATH=$TERMUX_PKG_BUILDDIR
 	mkdir -p "$GOPATH"/src/github.com/getantibody
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/getantibody/antibody
+
 	cd "$GOPATH"/src/github.com/getantibody/antibody
 	go build
 }
+
 termux_step_make_install() {
 	install -Dm700 \
 		"$GOPATH"/src/github.com/getantibody/antibody/antibody \
