a015eb56d9b8e97f0db1df043c33d9dcf6577677
araczkowski <araczkowski@gmail.com>
2021-03-12 14:52:43 +0100

    build packages/direnv/build.sh

--- packages/direnv/build.sh
+++ packages/direnv/build.sh
@@ -6,10 +6,12 @@ TERMUX_PKG_VERSION=2.27.0
 TERMUX_PKG_SRCURL=https://github.com/direnv/direnv/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9dc5ce43c63d9d9ff510c6bcd6ae06f3f2f907347e7cbb2bb6513bfb0f151621
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_make() {
  	termux_setup_golang
 	make
 }
+
 termux_step_make_install() {
 	make install DESTDIR=$TERMUX_PREFIX
 }
