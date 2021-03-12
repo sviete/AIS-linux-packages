1b8e946fc9cbb929835c5a6ebb38daed6385776c
araczkowski <araczkowski@gmail.com>
2021-03-12 13:37:03 +0100

    build packages/ctypes-sh/build.sh

--- packages/ctypes-sh/build.sh
+++ packages/ctypes-sh/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_SRCURL=https://github.com/taviso/ctypes.sh/releases/download/v${TERMU
 TERMUX_PKG_SHA256=8896334f5fa88f656057bff807ec6921c8f76fc6de801d996d2057fcb18b3a68
 TERMUX_PKG_DEPENDS="bash, libelf, libdw, libffi, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_pre_configure() {
 	autoreconf -vif
 }
