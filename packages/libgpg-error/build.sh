c7d99e42d7b6661582f9e669a348ba2b8f7e6a8a
araczkowski <araczkowski@gmail.com>
2021-03-13 05:56:28 +0100

    build packages/libgpg-error/build.sh

--- packages/libgpg-error/build.sh
+++ packages/libgpg-error/build.sh
@@ -8,12 +8,15 @@ TERMUX_PKG_SHA256=64b078b45ac3c3003d7e352a5e05318880a5778c42331ce1ef33d1a0d99227
 TERMUX_PKG_BREAKS="libgpg-error-dev"
 TERMUX_PKG_REPLACES="libgpg-error-dev"
 TERMUX_PKG_RM_AFTER_INSTALL="share/common-lisp"
+
 termux_step_post_get_source() {
 	# Upstream only has Android definitions for platform-specific lock objects.
 	# See https://lists.gnupg.org/pipermail/gnupg-devel/2014-January/028203.html
 	# for how to generate a lock-obj header file on devices.
+
 	# For aarch64 this was generated on a device:
 	cp $TERMUX_PKG_BUILDER_DIR/lock-obj-pub.aarch64-unknown-linux-android.h $TERMUX_PKG_SRCDIR/src/syscfg/
+
 	if [ $TERMUX_ARCH = i686 ]; then
 		# Android i686 has same config as arm (verified by generating a file on a i686 device):
 		cp $TERMUX_PKG_SRCDIR/src/syscfg/lock-obj-pub.arm-unknown-linux-androideabi.h \
@@ -24,6 +27,7 @@ termux_step_post_get_source() {
 			$TERMUX_PKG_SRCDIR/src/syscfg/lock-obj-pub.linux-android.h
 	fi
 }
+
 termux_step_pre_configure() {
 	autoreconf -fi
 	# USE_POSIX_THREADS_WEAK is being enabled for on-device build and causes
