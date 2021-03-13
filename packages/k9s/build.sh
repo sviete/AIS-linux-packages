e7f595e80085a3c8365269bae323a9b0a19c8610
araczkowski <araczkowski@gmail.com>
2021-03-13 01:49:17 +0100

    build packages/k9s/build.sh

--- packages/k9s/build.sh
+++ packages/k9s/build.sh
@@ -5,15 +5,18 @@ TERMUX_PKG_MAINTAINER="Krishna Kanhaiya @kcubeterm"
 TERMUX_PKG_VERSION=0.24.2
 TERMUX_PKG_SRCURL=https://github.com/derailed/k9s/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0f4e51a202c1221b8a6f18790a9e3e18c33a3807136d07bd40b02deac7d91f23
+
 termux_step_make() {
         termux_setup_golang
         cd "$TERMUX_PKG_SRCDIR"
         mkdir -p "${TERMUX_PKG_BUILDDIR}/src/github.com/derailed"
         cp -a "${TERMUX_PKG_SRCDIR}" "${TERMUX_PKG_BUILDDIR}/src/github.com/derailed/k9s"
         cd "${TERMUX_PKG_BUILDDIR}/src/github.com/derailed/k9s"
+
         go get -d -v
         go build
 }
+
 termux_step_make_install() {
         install -Dm700 ${TERMUX_PKG_BUILDDIR}/src/github.com/derailed/k9s/k9s \
                  $TERMUX_PREFIX/bin/k9s
