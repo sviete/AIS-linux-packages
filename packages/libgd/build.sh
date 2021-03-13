9ac8ea6299d84eebc6177ecfb1dc9ff334922baa
araczkowski <araczkowski@gmail.com>
2021-03-13 05:31:16 +0100

    build packages/libgd/build.sh

--- packages/libgd/build.sh
+++ packages/libgd/build.sh
@@ -8,6 +8,7 @@ TERMUX_PKG_SHA256=e904a35fd3379ddb2d7c64f929b7cbdf0422863646dae252be0029b9e47c9f
 TERMUX_PKG_DEPENDS="freetype, fontconfig, libiconv, libjpeg-turbo, libpng, libtiff, libwebp, zlib"
 TERMUX_PKG_BREAKS="libgd-dev"
 TERMUX_PKG_REPLACES="libgd-dev"
+
 # Disable vpx support for now, look at https://github.com/gagern/libgd/commit/d41eb72cd4545c394578332e5c102dee69e02ee8
 # for enabling:
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--without-vpx --without-x"
