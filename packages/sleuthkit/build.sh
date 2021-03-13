c42c4043edd380d29168289e7abafeb73545575b
araczkowski <araczkowski@gmail.com>
2021-03-14 00:38:24 +0100

    build packages/sleuthkit/build.sh

--- packages/sleuthkit/build.sh
+++ packages/sleuthkit/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://sleuthkit.org/
 TERMUX_PKG_DESCRIPTION="The Sleuth Kit® (TSK) is a library for digital forensics tools. "
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="licenses/mit.txt, licenses/GNU-COPYING, licenses/bsd.txt, licenses/cpl1.0.txt, licenses/IBM-LICENSE"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=4.10.1
@@ -10,6 +10,7 @@ TERMUX_PKG_SHA256=18e587f5f1869f9d87edc4e77e7d5ddcacfd0966d15e5fb45ce520b0fe9ff0
 TERMUX_PKG_DEPENDS="libsqlite, ncurses"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-java"
+
 termux_step_pre_configure() {
 	./bootstrap
 }
