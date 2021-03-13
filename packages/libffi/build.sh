1e02e36d6973480fc87bc577dda573275fb168e6
araczkowski <araczkowski@gmail.com>
2021-03-13 05:16:08 +0100

    build packages/libffi/build.sh

--- packages/libffi/build.sh
+++ packages/libffi/build.sh
@@ -10,6 +10,7 @@ TERMUX_PKG_BREAKS="libffi-dev"
 TERMUX_PKG_REPLACES="libffi-dev"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-multi-os-directory"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/libffi-${TERMUX_PKG_VERSION}/include"
+
 termux_step_pre_configure() {
 	if [ $TERMUX_ARCH = arm ]; then
 		CFLAGS+=" -fno-integrated-as"
