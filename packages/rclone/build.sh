06b18af10e32bbdcec240198e95791269d4c15e9
araczkowski <araczkowski@gmail.com>
2021-03-13 21:36:50 +0100

    build packages/rclone/build.sh

--- packages/rclone/build.sh
+++ packages/rclone/build.sh
@@ -8,13 +8,18 @@ TERMUX_PKG_SHA256=95f952dc059b842bd40338458b77657f7b5a1680c4ca837a3adcf83b63c8fd
 
 termux_step_make_install() {
 	cd $TERMUX_PKG_SRCDIR
+
 	termux_setup_golang
+
 	mkdir -p .gopath/src/github.com/rclone
 	ln -sf "$PWD" .gopath/src/github.com/rclone/rclone
 	export GOPATH="$PWD/.gopath"
+
 	go build -v -o rclone
+
 	# XXX: Fix read-only files which prevents removal of src dir.
 	chmod u+w -R .
+
 	cp rclone $TERMUX_PREFIX/bin/rclone
 	mkdir -p $TERMUX_PREFIX/share/man/man1/
 	cp rclone.1 $TERMUX_PREFIX/share/man/man1/
