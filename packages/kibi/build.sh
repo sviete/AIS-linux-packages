50e87b14a916b228b348d39bd7ca542dd8f27fe0
araczkowski <araczkowski@gmail.com>
2021-03-13 02:14:29 +0100

    build packages/kibi/build.sh

--- packages/kibi/build.sh
+++ packages/kibi/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION=0.2.2
 TERMUX_PKG_SRCURL=https://github.com/ilai-deutel/kibi/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=df0e2945d9d08fed3a0adbe73c73405641615eb55835675e06e91411fd541e91
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_post_make_install() {
     install -Dm644 "config_example.ini" "$TERMUX_PREFIX/etc/kibi/config.ini"
     install -Dm644 syntax.d/* -t "$TERMUX_PREFIX/share/kibi/syntax.d"
