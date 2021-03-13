c46ed5119469c064498b846c334dbce9630da716
araczkowski <araczkowski@gmail.com>
2021-03-13 17:16:56 +0100

    build packages/par2/build.sh

--- packages/par2/build.sh
+++ packages/par2/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/Parchive/par2cmdline/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=529f85857ec44e501cd8d95b0c8caf47477d7daa5bfb989e422c800bb71b689a
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_pre_configure() {
 	if [ $TERMUX_ARCH = "i686" ]; then
 		# Avoid undefined reference to __atomic_* functions:
@@ -16,4 +17,3 @@ termux_step_pre_configure() {
 	automake --add-missing
 	autoconf
 }
-
