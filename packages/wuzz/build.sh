6889c18cd3682d28b3c5706e75765ca239889b93
araczkowski <araczkowski@gmail.com>
2021-03-14 08:22:09 +0100

    build packages/wuzz/build.sh

--- packages/wuzz/build.sh
+++ packages/wuzz/build.sh
@@ -8,12 +8,15 @@ TERMUX_PKG_SHA256=721ea7343698e9f3c69e09eab86b9b1fef828057655f7cebc1de728c2f7515
 
 termux_step_make() {
 	termux_setup_golang
+
 	export GOPATH=$TERMUX_PKG_BUILDDIR
 	mkdir -p "$GOPATH"/src/github.com/asciimoo
 	ln -sf "$TERMUX_PKG_SRCDIR" "$GOPATH"/src/github.com/asciimoo/wuzz
+
 	cd "$GOPATH"/src/github.com/asciimoo/wuzz
 	go build
 }
+
 termux_step_make_install() {
 	install -Dm700 \
 		"$GOPATH"/src/github.com/asciimoo/wuzz/wuzz \
