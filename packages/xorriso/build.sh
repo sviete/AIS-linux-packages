7afd4e153712bee50cd1548aaeb48729c406deef
araczkowski <araczkowski@gmail.com>
2021-03-14 09:02:31 +0100

    build packages/xorriso/build.sh

--- packages/xorriso/build.sh
+++ packages/xorriso/build.sh
@@ -9,6 +9,7 @@ TERMUX_PKG_SHA256=3ac155f0ca53e8dbeefacc7f32205a98f4f27d2d348de39ee0183ba8a4c9e3
 TERMUX_PKG_DEPENDS="libiconv, libandroid-support, readline, libbz2, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-jtethreads"
+
 termux_step_pre_configure() {
 	./bootstrap
 }
