ad15bcbd9903160d374fbe0ea05a5f05b08e63e2
araczkowski <araczkowski@gmail.com>
2021-03-14 06:36:18 +0100

    build packages/usbmuxd/build.sh

--- packages/usbmuxd/build.sh
+++ packages/usbmuxd/build.sh
@@ -1,6 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://libimobiledevice.org
 TERMUX_PKG_DESCRIPTION="A socket daemon to multiplex connections from and to iOS devices."
-TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="COPYING.GPLv2, COPYING.GPLv3"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.1.1
@@ -8,6 +8,7 @@ TERMUX_PKG_SRCURL=https://github.com/libimobiledevice/usbmuxd/archive/$TERMUX_PK
 TERMUX_PKG_SHA256=e7ce30143e69d77fc5aa6fb0cb5f0cfcdbeff47eb1ac7fd90ac259a90de9fadd
 TERMUX_PKG_DEPENDS="libusb, libplist"
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_configure() {
         ./autogen.sh --prefix=$TERMUX_PREFIX \
 		     --without-preflight \
