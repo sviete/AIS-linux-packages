6a954eca6e0a85b3d56fc7d9a8cad520298a7ade
araczkowski <araczkowski@gmail.com>
2021-03-14 05:20:42 +0100

    build packages/toxic/build.sh

--- packages/toxic/build.sh
+++ packages/toxic/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_SRCURL=https://github.com/JFreegman/toxic/archive/v${TERMUX_PKG_VERSI
 TERMUX_PKG_SHA256=b3cfbea28135fbc48c083276160cef02de755b60f6cfe1bd6efc1b5fdd4b92fe
 TERMUX_PKG_DEPENDS="c-toxcore, libconfig, libcurl, libqrencode, ncurses, zlib"
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_make() {
 	make \
 		PREFIX="${TERMUX_PREFIX}" \
@@ -15,6 +16,7 @@ termux_step_make() {
 		USER_CFLAGS="${CFLAGS}" \
 		USER_LDFLAGS="${LDFLAGS}"
 }
+
 termux_step_make_install() {
 	make PREFIX="${TERMUX_PREFIX}" install
 }
