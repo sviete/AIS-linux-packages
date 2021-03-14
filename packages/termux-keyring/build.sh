b08107f0bb4213031f9ef38ed66c66e2875e52cf
araczkowski <araczkowski@gmail.com>
2021-03-14 03:09:40 +0100

    build packages/termux-keyring/build.sh

--- packages/termux-keyring/build.sh
+++ packages/termux-keyring/build.sh
@@ -5,11 +5,14 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.4
 TERMUX_PKG_SKIP_SRC_EXTRACT=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+
 termux_step_make_install() {
 	mkdir -p $TERMUX_PREFIX/etc/apt/trusted.gpg.d
+
 	# Maintainer-specific keys.
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/grimler.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/xeffyr.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
+
 	# Key for automatic builds (via CI).
 	install -Dm600 $TERMUX_PKG_BUILDER_DIR/termux-autobuilds.gpg $TERMUX_PREFIX/etc/apt/trusted.gpg.d/
 }
