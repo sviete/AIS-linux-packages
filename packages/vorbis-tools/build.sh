453db25b51d2cf2d50df7d9b51f3e9ad82e58998
araczkowski <araczkowski@gmail.com>
2021-03-14 07:31:45 +0100

    build packages/vorbis-tools/build.sh

--- packages/vorbis-tools/build.sh
+++ packages/vorbis-tools/build.sh
@@ -7,6 +7,7 @@ TERMUX_PKG_SRCURL=http://downloads.xiph.org/releases/vorbis/vorbis-tools-${TERMU
 TERMUX_PKG_SHA256=db7774ec2bf2c939b139452183669be84fda5774d6400fc57fde37f77624f0b0
 # libflac for flac support in oggenc:
 TERMUX_PKG_DEPENDS="libiconv, libvorbis, libflac, libogg"
+
 termux_step_pre_configure() {
 	LDFLAGS+=" -lm"
 }
