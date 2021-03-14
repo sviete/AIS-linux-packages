9c7d0d92a7845cb798ec7f2e14c39709032dfb22
araczkowski <araczkowski@gmail.com>
2021-03-14 07:06:34 +0100

    build packages/vifm/build.sh

--- packages/vifm/build.sh
+++ packages/vifm/build.sh
@@ -6,6 +6,7 @@ TERMUX_PKG_VERSION=0.11
 TERMUX_PKG_SRCURL=https://github.com/vifm/vifm/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=7b76f39b2225db2960ee125da204ce6264fe992ec2d9f2954ab74ba3c28ce0ec
 TERMUX_PKG_DEPENDS="ncurses, file"
+
 termux_step_pre_configure() {
 	autoreconf -if
 }
