31b867dd0aef663feeec635f4b055629a25522f6
araczkowski <araczkowski@gmail.com>
2021-03-14 01:13:42 +0100

    build packages/ssss/build.sh

--- packages/ssss/build.sh
+++ packages/ssss/build.sh
@@ -7,9 +7,11 @@ TERMUX_PKG_SRCURL=https://github.com/MrJoy/ssss/archive/releases/v${TERMUX_PKG_V
 TERMUX_PKG_SHA256=dbb1f03797cb3fa69594530f9b2c36010f66705b9d5fbbc27293dce72b9c9473
 TERMUX_PKG_DEPENDS="libgmp"
 TERMUX_PKG_BUILD_IN_SRC=true
+
 termux_step_make_install() {
 	install -Dm700 ssss-split "$TERMUX_PREFIX"/bin/
 	ln -sfr "$TERMUX_PREFIX"/bin/ssss-split $TERMUX_PREFIX/bin/ssss-combine
+
 	install -Dm600 ssss.1 "$TERMUX_PREFIX"/share/man/man1/ssss.1
 	ln -sfr \
 		"$TERMUX_PREFIX"/share/man/man1/ssss.1 \
