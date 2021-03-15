8398fd127d6173dc0b14b0985a315b7d707858ee
araczkowski <araczkowski@gmail.com>
2021-03-15 04:40:06 +0100

    packages/ccache/build.sh

--- packages/ccache/build.sh
+++ packages/ccache/build.sh
@@ -6,3 +6,4 @@ TERMUX_PKG_VERSION=4.2
 TERMUX_PKG_SRCURL=https://github.com/ccache/ccache/releases/download/v$TERMUX_PKG_VERSION/ccache-$TERMUX_PKG_VERSION.tar.xz
 TERMUX_PKG_SHA256=2f14b11888c39778c93814fc6843fc25ad60ff6ba4eeee3dff29a1bad67ba94f
 TERMUX_PKG_DEPENDS="zlib, zstd"
+
