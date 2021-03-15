f229c9e62c76a134ec6151ab069068b7c487f2d4
araczkowski <araczkowski@gmail.com>
2021-03-15 02:28:53 +0100

    packages/binaryen/build.sh

--- packages/binaryen/build.sh
+++ packages/binaryen/build.sh
@@ -5,3 +5,4 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=100
 TERMUX_PKG_SRCURL=https://github.com/WebAssembly/binaryen/archive/version_${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8a416b61ab9031240f8ba51a2a422c6ae99d4db1966a7bc7a6e515fa33e7a21c
+
