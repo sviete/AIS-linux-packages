da3b1b92bb790355dc4ae31bc94ec4eaab0c9781
araczkowski <araczkowski@gmail.com>
2021-03-15 10:58:26 +0100

    packages/findomain/build.sh

--- packages/findomain/build.sh
+++ packages/findomain/build.sh
@@ -8,3 +8,4 @@ TERMUX_PKG_SHA256=7bbedd088a64557876ccd7e7da92f1f8a5b67cfed1aaf335cd5d2b2a5f1824
 TERMUX_PKG_BUILD_IN_SRC=true
 # Compilation error related to assembly code.
 TERMUX_PKG_BLACKLISTED_ARCHES="arm"
+
