c2bde6b7bddf0e42897da5a3fe5cc466b4a226ca
araczkowski <araczkowski@gmail.com>
2021-03-15 10:03:17 +0100

    packages/electric-fence/build.sh

--- packages/electric-fence/build.sh
+++ packages/electric-fence/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://deb.debian.org/debian/pool/main/e/electric-fence/electric-fence_${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=8aa8d1e5772a169098f0aba6e647295c12561a6851ef9b378a54020e8086bcc6
 TERMUX_PKG_BUILD_IN_SRC=true
+
