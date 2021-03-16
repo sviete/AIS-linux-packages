ff5fd60c510353a120ea21fe9f8c801666e38e7f
araczkowski <araczkowski@gmail.com>
2021-03-16 09:36:56 +0100

    packages/microsocks/build.sh

--- packages/microsocks/build.sh
+++ packages/microsocks/build.sh
@@ -8,3 +8,4 @@ TERMUX_PKG_SRCURL=https://github.com/rofl0r/microsocks/archive/${_COMMIT}.tar.gz
 TERMUX_PKG_SHA256=7c5442be114b514a1a33c5219a8746a8232455c4d46aa418534f3ef2694779bc
 TERMUX_PKG_BUILD_IN_SRC=true
 
+
