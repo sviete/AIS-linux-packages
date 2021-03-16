715b572818cf8915264b564fa9e393d73f3fdd66
araczkowski <araczkowski@gmail.com>
2021-03-17 00:22:09 +0100

    packages/termux-api/build.sh

--- packages/termux-api/build.sh
+++ packages/termux-api/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_SRCURL=https://github.com/termux/termux-api-package/archive/v${TERMUX
 TERMUX_PKG_SHA256=ae551fa81d679a6fc4ff2cf479f43bf5bd20d3b7d5c0fc6e81e5aca20c1643a9
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_DEPENDS="bash, util-linux"
+
