4335e0376482d36ae0bcfd81e9be1a1b104cf6ef
araczkowski <araczkowski@gmail.com>
2021-03-16 10:36:20 +0100

    packages/muchsync/build.sh

--- packages/muchsync/build.sh
+++ packages/muchsync/build.sh
@@ -6,3 +6,4 @@ TERMUX_PKG_VERSION=6
 TERMUX_PKG_SRCURL=http://www.muchsync.org/src/muchsync-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=0b3de3b4d885edb9f887a07dcbc8b596e7fef2038c61e682cfa55a13fa4ce9e8
 TERMUX_PKG_DEPENDS="libc++, libsqlite, libxapian, notmuch, openssl"
+
