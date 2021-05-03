8199715580a2bca7e6050303b28c08edbf25e855
tomasz75 <tomasz@sviete.pl>
2021-05-03 16:15:40 +0200

    rust

--- packages/rust/build.sh
+++ packages/rust/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Systems programming language focused on safety, speed an
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Kevin Cotugno @kcotugno"
 TERMUX_PKG_VERSION=1.51.0
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://static.rust-lang.org/dist/rustc-$TERMUX_PKG_VERSION-src.tar.xz
 TERMUX_PKG_SHA256=92c68a91fca33cbafb83442cde722d010cc387dc1ee8a2680e2fb33a575821a1
 TERMUX_PKG_DEPENDS="libc++, clang, openssl, lld, zlib, libllvm"
@@ -50,9 +50,9 @@ termux_step_configure() {
 
 	# rust checks libs in PREFIX/lib because both host and target are x86_64. It then can't find libc.so and libdl.so because rust program doesn't
 	# know where those are. Putting them temporarly in $PREFIX/lib prevents that failure
-	
+
 	if [ $TERMUX_ARCH = "x86_64" ]; then
-		mv $TERMUX_PREFIX/lib/libtinfo.so.6 $TERMUX_PREFIX/lib/libtinfo.so.6.tmp	
+		mv $TERMUX_PREFIX/lib/libtinfo.so.6 $TERMUX_PREFIX/lib/libtinfo.so.6.tmp
 		mv $TERMUX_PREFIX/lib/libz.so.1 $TERMUX_PREFIX/lib/libz.so.1.tmp
 		mv $TERMUX_PREFIX/lib/libz.so $TERMUX_PREFIX/lib/libz.so.tmp
 	fi
@@ -66,7 +66,7 @@ termux_step_make_install() {
 		 mv $TERMUX_PREFIX ${TERMUX_PREFIX}a
 		 $TERMUX_PKG_SRCDIR/x.py build --host x86_64-unknown-linux-gnu --stage 1 cargo || $TERMUX_PKG_SRCDIR/x.py build --host x86_64-unknown-linux-gnu  --stage 1 rls ||  $TERMUX_PKG_SRCDIR/x.py build --host x86_64-unknown-linux-gnu --stage 1 rustfmt || $TERMUX_PKG_SRCDIR/x.py --stage 1 --host x86_64-unknown-linux-gnu  build rustdoc || $TERMUX_PKG_SRCDIR/x.py --stage 1 --host x86_64-unknown-linux-gnu build error_index_generator  || true
 		 mv ${TERMUX_PREFIX}a ${TERMUX_PREFIX}
-	
+
 	fi
 
 	#$TERMUX_PKG_SRCDIR/x.py dist --stage 1 --host $CARGO_TARGET_NAME --target $CARGO_TARGET_NAME || bash
@@ -83,10 +83,10 @@ termux_step_make_install() {
 		mv $TERMUX_PREFIX/lib/libz.so.1.tmp $TERMUX_PREFIX/lib/libz.so.1
 		mv $TERMUX_PREFIX/lib/libz.so.tmp $TERMUX_PREFIX/lib/libz.so
 	fi
-	
+
 	ln -sf rustlib/$CARGO_TARGET_NAME/lib/*.so .
 	ln -sf $TERMUX_PREFIX/bin/lld $TERMUX_PREFIX/bin/rust-lld
-	
+
 	cd "$TERMUX_PREFIX/lib/rustlib"
 	rm -rf components \
 		install.log \
@@ -104,5 +104,3 @@ termux_step_post_massage() {
 		rm -f lib/libz.so.1
 	fi
 }
-
-
