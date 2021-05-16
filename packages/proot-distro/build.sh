0d2bd2d4f710be6f1dd13f7cf1af4bcb6fd3206c
asystentka-jolka <jolka@sviete.pl>
2021-05-16 13:36:24 +0200

    procps

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.6.2
+TERMUX_PKG_VERSION=1.7.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=87295b45ff95e00b12afcdfb310fcf746c5514aafe820cecf785e0197f7a144a
+TERMUX_PKG_SHA256=0912687d61097ad7dc5f416769c655db3db973794c4a0c9426684431361487fc
 TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, xz-utils"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
@@ -24,5 +25,3 @@ etc/proot-distro/ubuntu-20.04.sh
 termux_step_make_install() {
 	./install.sh
 }
-
-
