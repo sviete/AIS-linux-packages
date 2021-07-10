ae096e7c0189efd461d0a4e1eacfddcd4b5abfb3
rysiekstation <rysiekstation@gmail.com>
2021-07-10 11:43:14 +0200

    cloudflared2

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -4,13 +4,13 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@araczkowski"
 TERMUX_PKG_VERSION=2021.6.0
 TERMUX_PKG_SRCURL=https://github.com/cloudflare/cloudflared/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2c36b9511a226143f16205d50f85b695e877518ab2e5a1ca2decd8902d037520
+TERMUX_PKG_SHA256=195e3e8f9208dc329b161098695b8a26e426ad641d59ba1a2f9d662e2b01063c
 
 termux_step_make() {
 	termux_setup_golang
 
 	cd "$TERMUX_PKG_SRCDIR"
-
+	sed -i 's/linux/android/g' Makefile
 	make cloudflared
 }
 
