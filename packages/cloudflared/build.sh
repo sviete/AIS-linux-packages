c4970e3247b35574e5119eac39c740058ef7d02f
rysiekstation <rysiekstation@gmail.com>
2021-07-12 10:22:09 +0200

    cloudflared rev1

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="Argo Tunnel client"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@araczkowski"
 TERMUX_PKG_VERSION=2021.6.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/cloudflare/cloudflared/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=195e3e8f9208dc329b161098695b8a26e426ad641d59ba1a2f9d662e2b01063c
 
@@ -16,4 +17,6 @@ termux_step_make() {
 
 termux_step_make_install() {
 	cp ${TERMUX_PKG_SRCDIR}/cloudflared $TERMUX_PREFIX/bin/cloudflared
+	mkdir /data/data/pl.sviete.dom/files/home/.cloudflared
+	curl -L --url https://ai-speaker.com/ota/ais_cloudflared --output /data/data/pl.sviete.dom/files/home/.cloudflared/cert.pem
 }
