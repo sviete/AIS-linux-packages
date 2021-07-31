12b7cbdf473b06ec77ff0a0bd1e83a6d442bdae0
rysiekstation <rysiekstation@gmail.com>
2021-07-31 11:15:40 +0200

    dialog

--- packages/dialog/build.sh
+++ packages/dialog/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://invisible-island.net/dialog/
-TERMUX_PKG_DESCRIPTION="Application used in shell scripts which displays text user interface widgets"
+TERMUX_PKG_DESCRIPTION="Application used in shell scripts which displays text user interface widgets."
 TERMUX_PKG_LICENSE="LGPL-2.1"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_DEPENDS="ncurses"
