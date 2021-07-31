3739a3aa4784b6cbffd39e6d30e1e9c78a23d609
rysiekstation <rysiekstation@gmail.com>
2021-07-31 11:44:31 +0200

    libedit

--- packages/libedit/build.sh
+++ packages/libedit/build.sh
@@ -2,9 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://thrysoee.dk/editline/
 TERMUX_PKG_DESCRIPTION="Library providing line editing, history, and tokenization functions"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=20210522-3.1
+TERMUX_PKG_VERSION=20210714-3.1
 TERMUX_PKG_SRCURL=https://thrysoee.dk/editline/libedit-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=0220bc2047e927c0c1984ef5f7b4eb2a9469a5b7bf12ba573ca3b23ca02bbb6f
+TERMUX_PKG_SHA256=3023b498ad593fd7745ae3b20abad546de506b67b8fbb5579637ca69ab82dbc9
 TERMUX_PKG_DEPENDS="libandroid-support, ncurses"
 TERMUX_PKG_BREAKS="libedit-dev"
 TERMUX_PKG_REPLACES="libedit-dev"
@@ -13,4 +13,3 @@ TERMUX_PKG_RM_AFTER_INSTALL="share/man/man7/editline.7 share/man/man3/history.3"
 termux_step_pre_configure() {
 	CFLAGS+=" -D__STDC_ISO_10646__=201103L -DNBBY=CHAR_BIT"
 }
-
