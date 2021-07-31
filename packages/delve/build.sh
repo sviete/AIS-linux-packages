875f0694d1cbd3cf54eae570dff726798c10bc0d
rysiekstation <rysiekstation@gmail.com>
2021-07-31 10:58:05 +0200

    delve

--- packages/delve/build.sh
+++ packages/delve/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/go-delve/delve
-TERMUX_PKG_DESCRIPTION="A debugger for the Go programming language"
+TERMUX_PKG_DESCRIPTION="A debugger for the Go programming language."
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Krishna kanhaiya @kcubeterm"
 TERMUX_PKG_VERSION=1.7.0
@@ -12,7 +12,7 @@ termux_step_make() {
 	termux_setup_golang
 	cd $TERMUX_PKG_SRCDIR
 
-	mkdir -p "$TERMUX_PKG_BUILDDIR"/src/github.com/go-delve/    
+	mkdir -p "$TERMUX_PKG_BUILDDIR"/src/github.com/go-delve/
 	mkdir -p "$TERMUX_PREFIX"/share/doc/delve
 	cp -a "$TERMUX_PKG_SRCDIR" "$TERMUX_PKG_BUILDDIR"/src/github.com/go-delve/delve/
 	cd "$TERMUX_PKG_BUILDDIR"/src/github.com/go-delve/delve/cmd/dlv/
@@ -24,4 +24,3 @@ termux_step_make_install() {
 	install -Dm700 -t "$TERMUX_PREFIX"/bin "$TERMUX_PKG_BUILDDIR"/src/github.com/go-delve/delve/cmd/dlv/dlv
 	cp -a "$TERMUX_PKG_SRCDIR"/Documentation/* "$TERMUX_PREFIX"/share/doc/delve
 }
-
