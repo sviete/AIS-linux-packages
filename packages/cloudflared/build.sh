e0e053b564daef1841e47f47a838490f412fc39e
Andrzej Raczkowski <araczkowski@gmail.com>
2021-09-10 14:31:33 +0200

    2021.8.7

--- packages/cloudflared/build.sh
+++ packages/cloudflared/build.sh
@@ -2,7 +2,7 @@ TERMUX_PKG_HOMEPAGE=https://github.com/cloudflare/cloudflared/
 TERMUX_PKG_DESCRIPTION="Argo Tunnel client."
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@araczkowski"
-TERMUX_PKG_VERSION=2021.8.2
+TERMUX_PKG_VERSION=2021.8.7
 TERMUX_PKG_SRCURL=https://github.com/cloudflare/cloudflared/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=195e3e8f9208dc329b161098695b8a26e426ad641d59ba1a2f9d662e2b01063c
 
