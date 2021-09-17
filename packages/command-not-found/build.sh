d53fcdfa7e504110ae5672039a8cd126a2b83290
Tomasz Świetlicki <54634833+tomasz75@users.noreply.github.com>
2021-09-17 09:52:56 +0200

    PKG_REVISION=1

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -3,5 +3,6 @@ TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell se
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@tomasz75"
 TERMUX_PKG_VERSION=1.63
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/tomasz75/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=1542113d3e831070f4a0aad790a222379a5c4a5877acc4020d3f329435c930ab
