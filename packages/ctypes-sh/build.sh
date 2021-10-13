ab66db83bb2326a7df06affb8317808d90f4f437
rysiekstation <rysiekstation@gmail.com>
2021-10-13 19:28:49 +0200

    ctypes.sh

--- packages/ctypes-sh/build.sh
+++ packages/ctypes-sh/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/taviso/ctypes.sh
-TERMUX_PKG_DESCRIPTION="A foreign function interface for bash"
+TERMUX_PKG_DESCRIPTION="A foreign function interface for bash."
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.2
