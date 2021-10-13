d40f2e728a7597856a326ce2a26f1b2af5fbec03
rysiekstation <rysiekstation@gmail.com>
2021-10-13 19:29:55 +0200

    darkhttpd

--- packages/darkhttpd/build.sh
+++ packages/darkhttpd/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://unix4lyfe.org/darkhttpd
-TERMUX_PKG_DESCRIPTION="A simple webserver, implemented in a single .c file."
+TERMUX_PKG_DESCRIPTION="A simple webserver, implemented in a single .c file"
 TERMUX_PKG_LICENSE="BSD"
 TERMUX_PKG_MAINTAINER="David Paskevic @casept"
 TERMUX_PKG_VERSION=1.13
@@ -11,4 +11,3 @@ TERMUX_PKG_BUILD_IN_SRC=true
 termux_step_pre_configure() {
 	CFLAGS+=" $LDFLAGS"
 }
-
