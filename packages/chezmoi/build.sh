07396caa7e5945be8d0929dca172951974e53682
rysiekstation <rysiekstation@gmail.com>
2021-10-13 12:31:58 +0200

    chezmoi

--- packages/chezmoi/build.sh
+++ packages/chezmoi/build.sh
@@ -2,9 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://chezmoi.io
 TERMUX_PKG_DESCRIPTION="Manage your dotfiles across multiple machines"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Henrik Grimler @Grimler91"
-TERMUX_PKG_VERSION=2.4.0
+TERMUX_PKG_VERSION=2.7.0
 TERMUX_PKG_SRCURL=https://github.com/twpayne/chezmoi/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=1a183c2d3d0d46314ae3647a5c014367948639800bf1b64208f19c7defc8f674
+TERMUX_PKG_SHA256=93c838f450132cdc41f219cfc1eb764424c3b00e533fd9d6c217e4163529a9ee
+TERMUX_PKG_AUTO_UPDATE=true
 
 termux_step_make() {
 	termux_setup_golang
