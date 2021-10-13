f32ac48456b90015d182d3bdfc5bd71000653c6e
rysiekstation <rysiekstation@gmail.com>
2021-10-13 19:17:27 +0200

    check

--- packages/check/build.sh
+++ packages/check/build.sh
@@ -1,5 +1,5 @@
 TERMUX_PKG_HOMEPAGE=https://libcheck.github.io/check
-TERMUX_PKG_DESCRIPTION="A unit testing framework for C"
+TERMUX_PKG_DESCRIPTION="A unit testing framework for C."
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.15.2
@@ -9,4 +9,3 @@ TERMUX_PKG_SHA256=a8de4e0bacfb4d76dd1c618ded263523b53b85d92a146d8835eb1a52932fa2
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libtool, pkg-config, texinfo"
 TERMUX_PKG_BUILD_IN_SRC=true
-
