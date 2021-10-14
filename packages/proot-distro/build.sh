a16f3f1756f36c237dc73343c3e5b7c39a126b8c
rysiekstation <rysiekstation@gmail.com>
2021-10-14 14:24:22 +0200

    proot-distro

--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -2,12 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=2.6.0
+TERMUX_PKG_VERSION=2.6.6
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=ffdba85094cc7a56f48df3c5ce35e34d71b68ce7a832fd80015b2367cdbe526c
+TERMUX_PKG_SHA256=0cec903067376b389a4a433c2af7670d2bd236ced9f0b9573fb1d606ffcb5892
 TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, xz-utils"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+
 
 termux_step_make_install() {
 	TERMUX_PREFIX="$TERMUX_PREFIX" TERMUX_ANDROID_HOME="$TERMUX_ANDROID_HOME" ./install.sh
